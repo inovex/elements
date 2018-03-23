a75c0d20fc18dd9cc4e08f5c691a1235caff982b
Patrick Hillert <phillert@inovex.de>
2018-03-23 09:41:26 +0100

    chore(): add sass support

--- stencil.config.js
+++ stencil.config.js
@@ -1,7 +1,12 @@
+const sass = require('@stencil/sass');
+
 exports.config = {
   namespace: 'mycomponent',
   generateDistribution: true,
-  serviceWorker: false
+  serviceWorker: false,
+  plugins: [
+    sass()
+  ]
 };
 
 exports.devServer = {
