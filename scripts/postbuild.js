70d5064a2fa34eb649f73003038acf9079915a89
Johannes Reuter <johannes.reuter@inovex.de>
2018-08-24 10:25:34 +0000

    feat(*): Add react wrapper elements and improve attribute typings

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,22 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const typingfix = require('./typingfix');
+const reactwrapper = require('./reactwrapper');
 
-const typescriptDefinitionsPath = path.join(
-  __dirname,
-  '..',
-  'dist/types/components.d.ts'
-);
-
-// Fix typescript definitions
-const typescriptDefinitions = String(
-  fs.readFileSync(typescriptDefinitionsPath)
-);
-const fixedTypescriptDefinitions = typescriptDefinitions.replace(
-  'interface HTMLAttributes {}',
-  'interface HTMLAttributes { [key: string]: any; }'
-);
-fs.writeFileSync(typescriptDefinitionsPath, fixedTypescriptDefinitions);
-
-console.info(
-  'Fixed typescript definitions. // TODO remove this when stencil has a fix for that'
-);
+typingfix();
+reactwrapper();
