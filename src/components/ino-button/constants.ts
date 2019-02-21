cea66f34c4d7b7c50ecd8b7d9e8ee752239672fd
Jan-Niklas Voß <jvoss@inovex.de>
2019-02-21 08:42:47 +0000

    feat(ino-button): add constants

---
+++ src/components/ino-button/constants.ts
@@ -0,0 +1,13 @@
+export const SELECTORS = {
+  MDC_BUTTON: '.mdc-button'
+};
+
+export const CSS_CLASSES = {
+  MDC_BUTTON: 'mdc-button',
+  MDC_BUTTON_ICON: 'mdc-button__icon',
+  MDC_BUTTON_ICON_RIGHT: 'mdc-button__icon--right',
+  UNELEVATED: 'mdc-button--unelevated',
+  OUTLINED: 'mdc-button--outlined',
+  RAISED: 'mdc-button--raised',
+  DENSE: 'mdc-button--dense'
+};
