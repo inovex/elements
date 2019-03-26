9b0d2026fb698b49d87a384740af3203d8947695
Patrick Hillert <phillert@inovex.de>
2019-03-26 16:46:25 +0100

    fix(postinstall): remove postinstall from released package

--- scripts/postinstall-hints.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-
-console.log(
-  '\n **************************************************************\n',
-  chalk.whiteBright.bgBlack.bold("  => Notice: Make sure you've at least installed node 10+ <=  \n"),
-  '**************************************************************\n'
-);
