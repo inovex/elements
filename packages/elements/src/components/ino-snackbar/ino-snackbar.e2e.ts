e08833a8026f570a988e32560108357c445c24cd
agerlach <agerlach@inovex.de>
2020-04-14 16:47:29 +0200

    test(elements|ino-snackbar): add prop and event tests

---
+++ packages/elements/src/components/ino-snackbar/ino-snackbar.e2e.ts
@@ -0,0 +1,46 @@
+import { setupPageWithContent } from '../../util/e2etests-setup';
+
+const INO_SNACKBAR = `<ino-snackbar></ino-snackbar>`;
+const INO_SNACKBAR_SELECTOR = 'ino-snackbar';
+const INO_BTN_SELECTOR = 'ino-snackbar ino-button';
+const INO_ICON_BTN_SELECTOR = 'ino-snackbar ino-icon-button';
+
+describe('InoSnackbar', () => {
+
+  it('should render with default values', async () => {
+    const page = await setupPageWithContent(INO_SNACKBAR);
+    const inoSnackbar = await page.find(INO_SNACKBAR_SELECTOR);
+
+    expect(inoSnackbar).toBeDefined();
+  });
+
+  describe('Events', () => {
+    it('should emit an inoActionClick event upon clicking the button', async () => {
+      const page = await setupPageWithContent(INO_SNACKBAR);
+      const inoSnackbar = await page.find(INO_SNACKBAR_SELECTOR);
+      await inoSnackbar.setAttribute('ino-action-text', 'test');
+      await page.waitForChanges();
+
+      const inoBtn = await page.find(INO_BTN_SELECTOR);
+      const actionEvent = await page.spyOnEvent('inoActionClick');
+
+      await inoBtn.click();
+      await page.waitForChanges();
+
+      expect(actionEvent).toHaveReceivedEvent();
+    });
+
+    it('should emit a hideEl event upon closing the snackbar', async () => {
+      const page = await setupPageWithContent(INO_SNACKBAR);
+      const inoBtn = await page.find(INO_ICON_BTN_SELECTOR);
+      const hideEvent = await page.spyOnEvent('hideEl');
+
+      await inoBtn.click();
+      await page.waitForChanges();
+
+      expect(hideEvent).toHaveReceivedEvent();
+      expect(hideEvent).toHaveReceivedEventDetail(true);
+    });
+  });
+
+});
