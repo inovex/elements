b3c39a8cd8e0fd459864d8a512344361ce68691e
agerlach <agerlach@inovex.de>
2020-04-14 16:16:16 +0200

    test(elements|ino-radio): add prop and event tests

--- packages/elements/src/components/ino-radio/ino-radio.e2e.ts
+++ packages/elements/src/components/ino-radio/ino-radio.e2e.ts
@@ -3,6 +3,8 @@ import { setupPageWithContent } from '../../util/e2etests-setup';
 const INO_RADIO = `<ino-radio></ino-radio>`;
 const INO_RADIO_CHECKED = `<ino-radio checked></ino-radio>`;
 const INO_RADIO_DISABLED = `<ino-radio disabled></ino-radio>`;
+const INO_RADIO_SELECTOR = 'ino-radio';
+const INPUT_SELECTOR = 'ino-radio input';
 
 describe('InoRadio', () => {
   it('should render with default values', async () => {
@@ -11,55 +13,83 @@ describe('InoRadio', () => {
     expect(inoCheckBoxEl).not.toBeNull();
   });
 
-  it('should render unchecked', async () => {
-    const page = await setupPageWithContent(INO_RADIO);
-    const input = await page.find('ino-radio');
-    const isChecked = await input.getProperty('checked');
-    expect(isChecked).toBeFalsy();
+  describe('Properties', () => {
+
+    it('should render unchecked', async () => {
+      const page = await setupPageWithContent(INO_RADIO);
+      const inoRadio = await page.find(INO_RADIO_SELECTOR);
+      const input = await page.find(INPUT_SELECTOR);
+
+      const isChecked = await inoRadio.getProperty('checked');
+      expect(isChecked).toBeFalsy();
+      expect(input).not.toHaveAttribute('checked');
+    });
+
+    it('should render checked', async () => {
+      const page = await setupPageWithContent(INO_RADIO);
+      const inoRadio = await page.find(INO_RADIO_SELECTOR);
+
+      await inoRadio.setProperty('checked', true);
+      await page.waitForChanges();
+
+      const isChecked = await inoRadio.getProperty('checked');
+      expect(isChecked).toBeTruthy();
+    });
+
+    it('should render disabled', async () => {
+      const page = await setupPageWithContent(INO_RADIO_CHECKED);
+      const inoRadio = await page.find(INO_RADIO_SELECTOR);
+      const input = await page.find(INPUT_SELECTOR);
+
+      await inoRadio.setAttribute('disabled', true);
+      await page.waitForChanges();
+
+      const innerDiv = await inoRadio.shadowRoot.querySelector('.mdc-radio');
+      expect(input).toHaveAttribute('disabled');
+      expect(innerDiv).toHaveClass('mdc-radio--disabled');
+    });
+
   });
 
-  it('should render checked', async () => {
-    const page = await setupPageWithContent(INO_RADIO_CHECKED);
-    const input = await page.find('ino-radio');
-    const isChecked = await input.getProperty('checked');
-    expect(isChecked).toBeTruthy();
-  });
+  describe('Events', () => {
 
-  it('should fire checkedChange event on click', async () => {
-    const page = await setupPageWithContent(INO_RADIO);
+    it('should fire checkedChange event on click', async () => {
+      const page = await setupPageWithContent(INO_RADIO);
 
-    const checkedChange = await page.spyOnEvent('checkedChange');
-    expect(checkedChange).not.toHaveReceivedEvent();
-    await page.click('ino-radio');
-    await page.waitForChanges();
-    expect(checkedChange).toHaveReceivedEvent();
-  });
+      const checkedChange = await page.spyOnEvent('checkedChange');
+      expect(checkedChange).not.toHaveReceivedEvent();
+      await page.click(INO_RADIO_SELECTOR);
+      await page.waitForChanges();
+      expect(checkedChange).toHaveReceivedEvent();
+    });
 
-  it('should not fire checkedChange event on click when already checked', async () => {
-    const page = await setupPageWithContent(INO_RADIO_CHECKED);
+    it('should not fire checkedChange event on click when already checked', async () => {
+      const page = await setupPageWithContent(INO_RADIO_CHECKED);
 
-    const checkedChange = await page.spyOnEvent('checkedChange');
-    await page.click('ino-radio');
-    await page.waitForChanges();
-    expect(checkedChange).not.toHaveReceivedEvent();
-  });
+      const checkedChange = await page.spyOnEvent('checkedChange');
+      await page.click(INO_RADIO_SELECTOR);
+      await page.waitForChanges();
+      expect(checkedChange).not.toHaveReceivedEvent();
+    });
 
-  it('should not fire checkedChange event on click when disabled', async () => {
-    const page = await setupPageWithContent(INO_RADIO_DISABLED);
+    it('should not fire checkedChange event on click when disabled', async () => {
+      const page = await setupPageWithContent(INO_RADIO_DISABLED);
 
-    const checkedChange = await page.spyOnEvent('checkedChange');
-    await page.click('ino-radio');
-    await page.waitForChanges();
-    expect(checkedChange).not.toHaveReceivedEvent();
-  });
+      const checkedChange = await page.spyOnEvent('checkedChange');
+      await page.click(INO_RADIO_SELECTOR);
+      await page.waitForChanges();
+      expect(checkedChange).not.toHaveReceivedEvent();
+    });
 
-  it('should receive checkedChanged with true as detail', async () => {
-    const page = await setupPageWithContent(INO_RADIO);
+    it('should receive checkedChanged with true as detail', async () => {
+      const page = await setupPageWithContent(INO_RADIO);
 
-    const checkedChange = await page.spyOnEvent('checkedChange');
-    expect(checkedChange).not.toHaveReceivedEvent();
-    await page.click('ino-radio');
-    await page.waitForChanges();
-    expect(checkedChange).toHaveReceivedEventDetail(true);
+      const checkedChange = await page.spyOnEvent('checkedChange');
+      expect(checkedChange).not.toHaveReceivedEvent();
+      await page.click(INO_RADIO_SELECTOR);
+      await page.waitForChanges();
+      expect(checkedChange).toHaveReceivedEventDetail(true);
+    });
+
   });
 });
