0716501fb033c13de2bb4161876b9f204e4864e5
agerlach <agerlach@inovex.de>
2020-04-15 13:27:32 +0200

    test(elements|ino-tab): add prop and event tests

---
+++ packages/elements/src/components/ino-tab/ino-tab.e2e.ts
@@ -0,0 +1,57 @@
+import { setupPageWithContent } from '../../util/e2etests-setup';
+
+const INO_TAB = `<ino-tab/>`;
+const INO_TAB_SELECTOR = 'ino-tab';
+const BTN_SELECTOR = 'ino-tab > button';
+
+describe('InoTab', () => {
+
+  it('should render with default values', async () => {
+    const page = await setupPageWithContent(INO_TAB);
+    const inoTab = await page.find(INO_TAB_SELECTOR);
+
+    expect(inoTab).toBeDefined();
+  });
+
+  describe('Properties', () => {
+
+    it('should render with inoStacked set to true', async () => {
+      const page = await setupPageWithContent(INO_TAB);
+      const inoTab = await page.find(INO_TAB_SELECTOR);
+      const btn = await page.find(BTN_SELECTOR);
+
+      await inoTab.setAttribute('ino-stacked', true);
+      await page.waitForChanges();
+
+      expect(btn).toHaveClass('mdc-tab--stacked');
+    });
+
+  });
+
+  describe('Events', () => {
+
+    it('should emit an inoInteracted event upon receiving a MDCTab:interacted event', async () => {
+      const page = await setupPageWithContent(INO_TAB);
+      const inoTab = await page.find(INO_TAB_SELECTOR);
+      const inoInteractedEvent = await page.spyOnEvent('inoInteracted');
+
+      await inoTab.triggerEvent('MDCTab:interacted');
+      await page.waitForChanges();
+
+      expect(inoInteractedEvent).toHaveReceivedEvent();
+    });
+
+    it('should stop the propagation of the MDCTab:interacted event', async () => {
+      const page = await setupPageWithContent(INO_TAB);
+      const inoTab = await page.find(INO_TAB_SELECTOR);
+      const mdcInteractedEvent = await page.spyOnEvent('MDCTab:interacted');
+
+      await inoTab.triggerEvent('MDCTab:interacted');
+      await page.waitForChanges();
+
+      expect(mdcInteractedEvent).not.toHaveReceivedEvent();
+    });
+
+  });
+
+});
