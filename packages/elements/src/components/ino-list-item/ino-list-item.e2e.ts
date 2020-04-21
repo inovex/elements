9e05c4131ec18bd2cbfa6d36a5ea577a7c8f35e0
Alexander Gerlach <alexander.gerlach@inovex.de>
2020-04-21 12:17:57 +0000

    test(elements|ino-list-item) add prop and event tests

---
+++ packages/elements/src/components/ino-list-item/ino-list-item.e2e.ts
@@ -0,0 +1,68 @@
+import { setupPageWithContent } from '../../util/e2etests-setup';
+
+const LIST_ITEM = `<ino-list-item/>`;
+const LIST_ITEM_SELECTOR = 'ino-list-item';
+const LI_SELECTOR = 'ino-list-item li';
+
+describe('InoListItem', () => {
+
+  it('should render with default values', async () => {
+    const page = await setupPageWithContent(LIST_ITEM);
+    const inoListItem = await page.find(LIST_ITEM_SELECTOR);
+
+    expect(inoListItem).toBeDefined();
+  });
+
+  describe('Properties', () => {
+
+    it('should disable the list-item if disabled property is set to true', async () => {
+      const page = await setupPageWithContent(LIST_ITEM);
+      const inoListItem = await page.find(LIST_ITEM_SELECTOR);
+      const liElem = await page.find(LI_SELECTOR);
+
+      await inoListItem.setAttribute('ino-disabled', true);
+      await page.waitForChanges();
+
+      expect(liElem).toHaveClass('mdc-list-item--disabled');
+    });
+
+    it('should render with the selected property set to true', async () => {
+      const page = await setupPageWithContent(LIST_ITEM);
+      const inoListItem = await page.find(LIST_ITEM_SELECTOR);
+      const liElem = await page.find(LI_SELECTOR);
+
+      await inoListItem.setAttribute('ino-selected', true);
+      await page.waitForChanges();
+
+      expect(liElem).toHaveClass('mdc-list-item--selected');
+    });
+
+    it('should render with the activated property set to true', async () => {
+      const page = await setupPageWithContent(LIST_ITEM);
+      const inoListItem = await page.find(LIST_ITEM_SELECTOR);
+      const liElem = await page.find(LI_SELECTOR);
+
+      await inoListItem.setAttribute('ino-activated', true);
+      await page.waitForChanges();
+
+      expect(liElem).toHaveClass('mdc-list-item--activated');
+    });
+
+  });
+
+  describe('Events', () => {
+
+    it('should emit a clickEl event upon clicking a list item', async () => {
+      const page = await setupPageWithContent(LIST_ITEM);
+      const inoListItem = await page.find(LIST_ITEM_SELECTOR);
+      const clickElEvent = await page.spyOnEvent('clickEl');
+
+      await inoListItem.click();
+      await page.waitForChanges();
+
+      expect(clickElEvent).toHaveReceivedEvent();
+    });
+
+  });
+
+});
