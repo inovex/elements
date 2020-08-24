03f56b72eb2d0fb4a87b19446a808ae6b8a9fbb6
agerlach <agerlach@inovex.de>
2020-08-24 11:11:12 +0200

    docs(elements|ino-segment-button): add customizable button to story

--- packages/storybook/src/stories/ino-segment-button/ino-segment-button.stories.js
+++ packages/storybook/src/stories/ino-segment-button/ino-segment-button.stories.js
@@ -9,6 +9,24 @@ export default {
 
 export const DefaultUsage = () => /* html */ `
     <div class="story-segment-button">
+        <style>
+          ino-segment-button.customizable-segment-btn {
+              --segment-button-color: ${text('--segment-button-color', '#c1c1c1', 'Custom Properties')};
+              --segment-button-active-color: ${text('--segment-button-active-color', '#0d10f3', 'Custom Properties')};
+              --segment-button-hover-color: ${text('--segment-button-hover-color', '#5d60f7', 'Custom Properties')};
+              --segment-button-disabled-color: ${text('--segment-button-disabled-color', '#777777', 'Custom Properties')};
+          }
+        </style>
+        <h4>Customizable Segment Button</h4>
+        <ino-segment-button
+                value="1"
+                class="customizable-segment-btn"
+                checked="${boolean('checked', false, 'General')}"
+                disabled="${boolean('disabled', false, 'General')}"
+                ino-dense="${boolean('ino-dense', false, 'General')}"
+            >
+            Customizable Segment Button
+        </ino-segment-button>
         <h4>Enabled, not checked</h4>
         <ino-segment-button>Content</ino-segment-button>
 
