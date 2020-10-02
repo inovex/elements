99b593d0ee94b8637444a8aa036ebbd6e336490a
P1NHE4D <44554211+P1NHE4D@users.noreply.github.com>
2020-10-02 16:07:43 +0200

    chore(elements): update stencil to v2.0.3 (#85)

--- packages/elements/stencil.config.ts
+++ packages/elements/stencil.config.ts
@@ -1,5 +1,6 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   buildEs5: false, // see https://github.com/ionic-team/stencil/issues/253
@@ -9,8 +10,7 @@ export const config: Config = {
   outputTargets: [
     { type: 'dist' },
     { type: 'docs-readme' },
-    {
-      type: 'angular',
+    angularOutputTarget({
       componentCorePackage: '@inovex.de/elements',
       directivesProxyFile:
         '../elements-angular/elements/src/directives/proxies.ts',
@@ -18,7 +18,7 @@ export const config: Config = {
         '../elements-angular/elements/src/directives/proxies-utils.ts',
       directivesArrayFile:
         '../elements-angular/elements/src/directives/proxies-list.ts'
-    }
+    }),
   ],
   plugins: [
     sass({
