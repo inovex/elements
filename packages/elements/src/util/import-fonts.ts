c8b836de7074f19332e5be9fb35132b343d07720
Jan-Niklas Voß <jvoss@inovex.de>
2020-10-02 15:11:24 +0200

    feat(elements-angular): add function to add fonts to head element

--- packages/elements/src/util/import-fonts.ts
+++ packages/elements/src/util/import-fonts.ts
@@ -1,1 +1,15 @@
 require('typeface-lato');
+
+// For the angular wrapper
+export function importLatoFont() {
+  const cssFile = require('typeface-lato').default;
+  addCSSToHead(cssFile);
+}
+
+function addCSSToHead(css){
+  const head = document.getElementsByTagName('head')[0];
+  const style = document.createElement('style');
+  style.setAttribute('type', 'text/css');
+  style.appendChild(document.createTextNode(css));
+  head.appendChild(style);
+}
