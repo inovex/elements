4cabf4b3f4d9212b12961b360d9df9ab7de671f6
Jan-Niklas Voß <jvoss@inovex.de>
2021-01-06 12:19:11 +0100

    test: update test utils function to wait for render before resolving

--- packages/elements/src/util/e2etests-setup.ts
+++ packages/elements/src/util/e2etests-setup.ts
@@ -1,7 +1,14 @@
 import { newE2EPage } from '@stencil/core/testing';
 
+/**
+ * Helper function to create a new E2E page with the given content.
+ * Waits for the page to load before resolving.
+ * @param content HTML content to create the page from (e.g. `<ino-button>My Button</ino-button>`)
+ */
 export async function setupPageWithContent(content: string) {
   const page = await newE2EPage();
   await page.setContent(content);
+  await page.waitForChanges();
+
   return page;
 }
