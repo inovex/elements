02771110eb10203e156943644e5395bf1d2b153f
Jan-Niklas Voß <jvoss@inovex.de>
2021-01-11 12:26:29 +0100

    test: use variables instead of strings

--- packages/elements/src/components/ino-menu/ino-menu.e2e.ts
+++ packages/elements/src/components/ino-menu/ino-menu.e2e.ts
@@ -33,7 +33,7 @@ describe('InoMenu', () => {
       const page = await setupPageWithContent(`
         <div id="container">
             <button id="${btnId}">Open menu</button>
-            <ino-menu ino-for="id-btn" ino-open="true">
+            <ino-menu ino-for="${btnId}" ino-open="true">
                 <ino-list-item id="item-1" ino-text="item-1"></ino-list-item>
                 <ino-list-item id="item-1" ino-text="item-2"></ino-list-item>
             </ino-menu>
@@ -55,7 +55,7 @@ describe('InoMenu', () => {
       const page = await setupPageWithContent(`
         <div id="container">
             <button id="${btnId}">Open menu</button>
-            <ino-menu ino-for="id-btn" ino-open="false">
+            <ino-menu ino-for="${btnId}" ino-open="false">
                 <ino-list-item id="item-1" ino-text="item-1"></ino-list-item>
                 <ino-list-item id="item-1" ino-text="item-2"></ino-list-item>
             </ino-menu>
@@ -72,12 +72,13 @@ describe('InoMenu', () => {
 
     it('should emit \'menuClose\' event on inside item click', async () => {
 
+      const btnId = 'id-btn';
       const listItemId = 'item-1';
 
       const page = await setupPageWithContent(`
         <div id="container">
-            <button>Open menu</button>
-            <ino-menu ino-for="id-btn" ino-open="true">
+            <button id="${btnId}">Open menu</button>
+            <ino-menu ino-for="${btnId}" ino-open="true">
                 <ino-list-item id="${listItemId}" ino-text="item-1"></ino-list-item>
                 <ino-list-item id="item-1" ino-text="item-2"></ino-list-item>
             </ino-menu>
