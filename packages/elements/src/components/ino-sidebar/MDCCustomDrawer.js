d1b3261c8ff4299128a19d67075e67785ab03515
agerlach <agerlach@inovex.de>
2021-02-04 14:43:43 +0100

    chore(elements): edit eslint configurations

--- packages/elements/src/components/ino-sidebar/MDCCustomDrawer.js
+++ packages/elements/src/components/ino-sidebar/MDCCustomDrawer.js
@@ -1,8 +1,8 @@
-import { MDCDrawer } from '@material/drawer/component';
+import { MDCDrawer } from "@material/drawer/component";
 
 export class MDCCustomDrawer extends MDCDrawer {
-  constructor(mdcDrawer) {
-    super(mdcDrawer);
-    this.foundation.handleKeydown = (e) => {};
-  }
+	constructor(mdcDrawer) {
+		super(mdcDrawer);
+		this.foundation.handleKeydown = (e) => {};
+	}
 }
