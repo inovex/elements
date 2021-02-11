6116e259c2d981d3bf504e69a9037a8b3eb12d51
agerlach <agerlach@inovex.de>
2021-02-11 11:01:41 +0100

    fix(elements): fix pipeline, revert unintended changes

--- packages/elements-react/src/components/index.ts
+++ packages/elements-react/src/components/index.ts
@@ -1,4 +1,6 @@
 import { JSX as StencilJSX } from '@stencil/core';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { addIcons } from '@inovex.de/elements/dist/collection/util/icons';
 import { ICON_PATHS } from '@inovex.de/elements/dist/inovex-elements/ino-icon/index.esm.js';
 import { defineCustomElements } from '@inovex.de/elements/dist/loader';
