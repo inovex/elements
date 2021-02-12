3a1e439e1e40db1b3c33f0f1983ff21561d00c7b
agerlach <agerlach@inovex.de>
2021-02-12 10:52:27 +0100

    refactor(elements|ino-fab): revert unintended changes

--- packages/storybook/src/stories/ino-fab/ino-fab.stories.js
+++ packages/storybook/src/stories/ino-fab/ino-fab.stories.js
@@ -1,110 +1,110 @@
-import { boolean, select, text } from "@storybook/addon-knobs";
+import { boolean, select, text } from '@storybook/addon-knobs';
 
-import withStencilReadme from "_local-storybookcore/with-stencil-readme";
+import withStencilReadme from '_local-storybookcore/with-stencil-readme';
 
-import componentReadme from "_local-elements/src/components/ino-fab/readme.md";
+import componentReadme from '_local-elements/src/components/ino-fab/readme.md';
 
-import ICONS from "_local-elements/src/components/ino-icon/icons";
-import "./ino-fab.scss";
+import ICONS from '_local-elements/src/components/ino-icon/icons';
+import './ino-fab.scss';
 
 export default {
-  title: "Buttons/<ino-fab>",
+  title: 'Buttons/<ino-fab>',
   decorators: [withStencilReadme(componentReadme)]
 };
 
 export const DefaultUsage = () => /*html*/ `
-    <div class="story-fab">
+    <div class='story-fab'>
     <h4>Customizable Fab</h4>
      <style>
           ino-fab.customizable-fab {
             --ino-fab-color: ${text(
-  "--ino-fab-color",
-  "#ffffff",
-  "Custom Properties"
+  '--ino-fab-color',
+  '#ffffff',
+  'Custom Properties'
 )};
             --ino-fab-background-color: ${text(
-  "--ino-fab-background-color",
-  "#3d40f5",
-  "Custom Properties"
+  '--ino-fab-background-color',
+  '#3d40f5',
+  'Custom Properties'
 )};
             --ino-fab-background-color-hover: ${text(
-  "--ino-fab-background-color-hover",
-  "#5d60f7",
-  "Custom Properties"
+  '--ino-fab-background-color-hover',
+  '#5d60f7',
+  'Custom Properties'
 )};
             --ino-fab-background-color-active: ${text(
-  "--ino-fab-background-color-active",
-  "#0d10f3",
-  "Custom Properties"
+  '--ino-fab-background-color-active',
+  '#0d10f3',
+  'Custom Properties'
 )};
             --ino-fab-icon-color: ${text(
-  "--ino-fab-icon-color",
-  "#ffffff",
-  "Custom Properties"
+  '--ino-fab-icon-color',
+  '#ffffff',
+  'Custom Properties'
 )};
             --ino-fab-color-disabled: ${text(
-  "--ino-fab-color-disabled",
-  "#ffffff",
-  "Custom Properties"
+  '--ino-fab-color-disabled',
+  '#ffffff',
+  'Custom Properties'
 )};
             --ino-fab-background-color-disabled: ${text(
-  "--ino-fab-background-color-disabled",
-  "#9d9d9d",
-  "Custom Properties"
+  '--ino-fab-background-color-disabled',
+  '#9d9d9d',
+  'Custom Properties'
 )};
             --ino-fab-icon-color-disabled: ${text(
-  "--ino-fab-icon-color-disabled",
-  "#ffffff",
-  "Custom Properties"
+  '--ino-fab-icon-color-disabled',
+  '#ffffff',
+  'Custom Properties'
 )};
           }
       </style>
       <ino-fab
-        class="customizable-fab"
-      ino-edge-position="${select(
-  "ino-edge-position",
-  ["top-left", "top-right", "bottom-right", "bottom-left", "none"],
-  "top-left"
-)}"
-      ino-label="${text("ino-label", "Label")}"
-      ino-extended="${boolean("ino-extended", false)}"
-      ino-disabled="${boolean("ino-disabled", false)}"
-      ino-mini="${boolean("ino-mini", false)}">
-        <ino-icon slot="ino-icon-leading" ino-icon="${select("ino-leading-icon", ICONS, "add")}"/>
+        class='customizable-fab'
+      ino-edge-position='${select(
+  'ino-edge-position',
+  ['top-left', 'top-right', 'bottom-right', 'bottom-left', 'none'],
+  'top-left'
+)}'
+      ino-label='${text('ino-label', 'Label')}'
+      ino-extended='${boolean('ino-extended', false)}'
+      ino-disabled='${boolean('ino-disabled', false)}'
+      ino-mini='${boolean('ino-mini', false)}'>
+        <ino-icon slot='ino-icon-leading' ino-icon='${select('ino-leading-icon', ICONS, 'add')}'/>
       </ino-fab>
 
     <h4>Variants</h4>
-      <div class="ino-fab-variants">
-        <ino-fab ino-tooltip-placement="none" ino-label="Default Fab">
-          <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+      <div class='ino-fab-variants'>
+        <ino-fab ino-tooltip-placement='none' ino-label='Default Fab'>
+          <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
         </ino-fab>
-        <ino-fab ino-mini ino-label="Mini Fab">
-            <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+        <ino-fab ino-mini ino-label='Mini Fab'>
+            <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
         </ino-fab>
-        <ino-fab ino-extended ino-label="Extended Button">
-            <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+        <ino-fab ino-extended ino-label='Extended Button'>
+            <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
         </ino-fab>
-        <ino-fab ino-disabled ino-label="Disabled Button">
-            <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+        <ino-fab ino-disabled ino-label='Disabled Button'>
+            <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
         </ino-fab>
       </div>
     </div>
     <h4>Different Edge Positions</h4>
-    <div class="ino-fab-variants">
-       <ino-fab ino-label="top-left" ino-edge-position="top-left">
-           <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+    <div class='ino-fab-variants'>
+       <ino-fab ino-label='top-left' ino-edge-position='top-left'>
+           <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
        </ino-fab>
-       <ino-fab ino-label="top-right" ino-edge-position="top-right">
-           <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+       <ino-fab ino-label='top-right' ino-edge-position='top-right'>
+           <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
        </ino-fab>
-       <ino-fab ino-label="bottom-right" ino-edge-position="bottom-right">
-           <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+       <ino-fab ino-label='bottom-right' ino-edge-position='bottom-right'>
+           <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
        </ino-fab>
-       <ino-fab ino-label="bottom-left" ino-edge-position="bottom-left">
-           <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+       <ino-fab ino-label='bottom-left' ino-edge-position='bottom-left'>
+           <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
        </ino-fab>
-       <ino-fab ino-label="none" ino-edge-position="none">
-           <ino-icon slot="ino-icon-leading" ino-icon="add"></ino-icon>
+       <ino-fab ino-label='none' ino-edge-position='none'>
+           <ino-icon slot='ino-icon-leading' ino-icon='add'></ino-icon>
        </ino-fab>
     </div>
   `;
