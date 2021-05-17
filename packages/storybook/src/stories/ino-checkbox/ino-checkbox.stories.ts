11c9e577c524bdb25ed7a355e5ecd2ec0adb1ed6
Jan-Niklas Voß <jvoss@inovex.de>
2021-05-17 16:39:14 +0200

    refactor(storybook|ino-checkbox): update story to new storybook

--- packages/storybook/src/stories/ino-checkbox/ino-checkbox.stories.ts
+++ packages/storybook/src/stories/ino-checkbox/ino-checkbox.stories.ts
@@ -1,50 +1,62 @@
+import { Components } from '@inovex.de/elements';
 import { useEffect } from '@storybook/client-api';
+import { Story } from '@storybook/web-components';
 import { html } from 'lit-html';
+
 import { defaultDecorator } from '../utils';
 import './ino-checkbox.scss';
 
 export default {
   title: 'Components (WIP)/ino-checkbox',
   component: 'ino-checkbox',
-  decorators: [defaultDecorator, story => {
-    useEffect(() => {
-      const handleCheckedChange = e => {
-        const checkbox = e.target;
-        checkbox.setAttribute('checked', e.detail);
-        checkbox.indeterminate && checkbox.setAttribute('indeterminate', 'false');
-      };
+  decorators: [
+    defaultDecorator,
+    (story) => {
+      useEffect(() => {
+        const handleCheckedChange = (e) => {
+          const checkbox: HTMLInoCheckboxElement = e.target;
+          checkbox.checked = e.detail;
+          if (checkbox.indeterminate) {
+            checkbox.indeterminate = false;
+          }
+        };
 
-      const checkboxes = document.querySelectorAll('ino-checkbox');
-      checkboxes.forEach(c => c.addEventListener('checkedChange', handleCheckedChange));
-      return () => checkboxes.forEach(
-        c => c.removeEventListener('checkedChange', handleCheckedChange)
-      );
-    });
-    return story();
-  }]
+        const checkboxes = document.querySelectorAll('ino-checkbox');
+        checkboxes.forEach((c) =>
+          c.addEventListener('checkedChange', handleCheckedChange)
+        );
+        return () =>
+          checkboxes.forEach((c) =>
+            c.removeEventListener('checkedChange', handleCheckedChange)
+          );
+      });
+      return story();
+    },
+  ],
 };
 
-export const Playground = args => html`
+export const Playground: Story<Components.InoCheckbox> = (args) => html`
   <ino-checkbox
     class="customizable-checkbox"
     checked="${args.checked}"
     disabled="${args.disabled}"
     indeterminate="${args.indeterminate}"
     name="${args.name}"
-    ino-selection="${args.inoSelection}"
+    selection="${args.selection}"
     value="${args.value}"
   >
     Label
   </ino-checkbox>
 `;
+
 Playground.args = {
   checked: false,
   disabled: false,
   indeterminate: false,
   name: '',
-  inoSelection: false,
-  value: ''
-}
+  selection: false,
+  value: '',
+};
 
 export const Variations = () => html`
   <div class="story-states">
@@ -55,19 +67,25 @@ export const Variations = () => html`
   <div class="story-states">
     <ino-checkbox disabled checked>Checked and Disabled</ino-checkbox>
     <ino-checkbox disabled>Unchecked and Disabled</ino-checkbox>
-    <ino-checkbox disabled indeterminate>Indeterminated and Disabled</ino-checkbox>
+    <ino-checkbox disabled indeterminate
+      >Indeterminate and Disabled
+    </ino-checkbox>
   </div>
 `;
 
 export const Selection = () => html`
   <div class="story-states">
-    <ino-checkbox ino-selection checked>Checked</ino-checkbox>
-    <ino-checkbox ino-selection>Unchecked</ino-checkbox>
-    <ino-checkbox ino-selection indeterminate>Indeterminate</ino-checkbox>
+    <ino-checkbox selection checked>Checked</ino-checkbox>
+    <ino-checkbox selection>Unchecked</ino-checkbox>
+    <ino-checkbox selection indeterminate>Indeterminate</ino-checkbox>
   </div>
   <div class="story-states">
-    <ino-checkbox checked ino-selection disabled>Checked and disabled</ino-checkbox>
-    <ino-checkbox ino-selection disabled>Unchecked and disabled</ino-checkbox>
-    <ino-checkbox ino-selection indeterminate disabled>Indeterminate Disabled</ino-checkbox>
+    <ino-checkbox checked selection disabled
+      >Checked and disabled
+    </ino-checkbox>
+    <ino-checkbox selection disabled>Unchecked and disabled</ino-checkbox>
+    <ino-checkbox selection indeterminate disabled>
+      Indeterminate Disabled
+    </ino-checkbox>
   </div>
 `;
