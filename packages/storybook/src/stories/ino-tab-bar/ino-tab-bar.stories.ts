ca0c59260a10d9e00695c24eaf9a7b8b5ba18b66
Pascal Fecht <pfecht@inovex.de>
2021-06-01 15:02:06 +0200

    refactor: migrate tab-bar and tab

--- packages/storybook/src/stories/ino-tab-bar/ino-tab-bar.stories.ts
+++ packages/storybook/src/stories/ino-tab-bar/ino-tab-bar.stories.ts
@@ -1,107 +1,59 @@
 import { html } from 'lit-html';
-import { defaultDecorator, withColorScheme, withIconControl } from '../utils';
+import { defaultDecorator } from '../utils';
 import { useEffect } from '@storybook/client-api';
 import './ino-tab-bar.scss';
+import { Story } from '@storybook/web-components';
+import { Components } from '@inovex.de/elements';
 
+const eventHandler = e => e.target.setAttribute('active-tab', e.detail);
+      
 export default {
-  title: `Components (WIP)/ino-tab-bar`,
+  title: `Structure/ino-tab-bar`,
   component: 'ino-tab-bar',
   decorators: [defaultDecorator, story => {
     useEffect(() => {
-      const tabBar = document.getElementById('customizable-tabbar');
-      const eventHandler = (e) => e.target.setAttribute('active-tab', e.detail);
-      tabBar.addEventListener('activeTabChange', eventHandler);
-      return () => tabBar.removeEventListener('activeTabChange', eventHandler);
+      const tabBars = document.querySelectorAll('ino-tab-bar');
+      tabBars.forEach(t => t.addEventListener('activeTabChange', eventHandler));
+      return () => tabBars.forEach(t => t.removeEventListener('activeTabChange', eventHandler));
     });
     return story();
   }],
 };
 
-export const Playground = (args) => html`
+export const Playground: Story<Components.InoTabBar> = args => html`
   <ino-tab-bar id="customizable-tabbar" active-tab="${args.activeTab}">
-    <ino-tab
-      label="User"
-      icon="user"
-      stacked="${args.stacked}"
-      indicator-content-width="${args.indicatorContentWidth}"
-    ></ino-tab>
-    <ino-tab
-      label="Delivery"
-      icon="delivery"
-      stacked="${args.stacked}"
-      indicator-content-width="${args.indicatorContentWidth}"
-    ></ino-tab>
-    <ino-tab
-      label="Settings"
-      icon="settings"
-      stacked="${args.stacked}"
-      indicator-content-width="${args.indicatorContentWidth}"
-    ></ino-tab>
-    <ino-tab
-      label="Download"
-      icon="download"
-      stacked="${args.stacked}"
-      indicator-content-width="${args.indicatorContentWidth}"
-    ></ino-tab>
+    <ino-tab label="User" icon="user"></ino-tab>
+    <ino-tab label="Delivery" icon="delivery"></ino-tab>
+    <ino-tab label="Settings" icon="settings"></ino-tab>
+    <ino-tab label="Download" icon="download"></ino-tab>
   </ino-tab-bar>
 `;
 Playground.args = {
-  stacked: false,
-  indicatorContentWidth: false,
   activeTab: 0
 }
-Playground.parameters = {
-  actions: {
-    handles: ['activeTabChange ino-tab-bar']
+Playground.argTypes = {
+  activeTab: {
+    control: {
+      type: 'select',
+      options: [0,1,2,3]
+    }
   }
-};
+}
 
 export const Stacked = () => html`
   <ino-tab-bar>
-    <ino-tab
-      label="Label 1"
-      icon="service"
-      stacked="true">
-    </ino-tab>
-    <ino-tab
-      label="Label 2"
-      icon="info"
-      stacked="true">
-    </ino-tab>
-    <ino-tab
-      label="Label 3"
-      icon="info"
-      stacked="true">
-    </ino-tab>
-    <ino-tab
-      label="Label 4"
-      icon="info"
-      stacked="true">
-    </ino-tab>
+    <ino-tab label="Label 1" icon="info" stacked></ino-tab>
+    <ino-tab label="Label 2" icon="info" stacked></ino-tab>
+    <ino-tab label="Label 3" icon="info" stacked></ino-tab>
+    <ino-tab label="Label 4" icon="info" stacked></ino-tab>
   </ino-tab-bar>
 `;
 
 export const IndicatorContentWidth = () => html`
   <ino-tab-bar>
-    <ino-tab
-      label="Label 1"
-      icon="info"
-      indicator-content-width="true">
-    </ino-tab>
-    <ino-tab
-      label="Label 2"
-      icon="info"
-      indicator-content-width="true">
-    </ino-tab>
-    <ino-tab
-      label="Label 3"
-      icon="info"
-      indicator-content-width="true">
-    </ino-tab>
-    <ino-tab
-      label="Label 4"
-      icon="info"
-      indicator-content-width="true">
-    </ino-tab>
+    <ino-tab label="Label 1" icon="info" indicator-content-width></ino-tab>
+    <ino-tab label="Label 2" icon="info" indicator-content-width></ino-tab>
+    <ino-tab label="Label 3" icon="info" indicator-content-width></ino-tab>
+    <ino-tab label="Label 4" icon="info" indicator-content-width></ino-tab>
   </ino-tab-bar>
 `;
--- packages/storybook/src/stories/ino-tab/ino-tab.stories.ts
+++ packages/storybook/src/stories/ino-tab/ino-tab.stories.ts
@@ -1,51 +1,42 @@
 import { html } from 'lit-html';
 import { defaultDecorator, withIconControl } from '../utils';
+import { Story } from '@storybook/web-components';
+import { Components } from '@inovex.de/elements';
 
 export default {
-  title: `Components (WIP)/ino-tab`,
+  title: `Structure/ino-tab`,
   component: 'ino-tab',
   decorators: [defaultDecorator],
 };
 
-export const Playground = (args) => html`
+export const Playground: Story<Components.InoTab> = args => html`
   <ino-tab
-    label="${args.label}"
     icon="${args.icon}"
+    indicator-content-width="${args.indicatorContentWidth}"
+    label="${args.label}"
     stacked="${args.stacked}">
   </ino-tab>
 `;
 Playground.args = {
+  indicatorContentWidth: false,
   label: 'Customizable Tab',
   stacked: false,
 }
-
 withIconControl(Playground, 'icon', 'info');
 
 export const Variants = () => html`
   <div class="story-tab-variants">
-    <ino-tab
-        label="Default"
-        icon="info"
-        stacked="false">
-    </ino-tab>
-    <ino-tab
-          label="Stacked"
-          icon="info"
-          stacked="true">
-    </ino-tab>
+    <ino-tab label="Default" icon="info"></ino-tab>
+    <ino-tab label="Stacked" icon="info" stacked></ino-tab>
     <ino-tab-bar active-tab="0">
-      <ino-tab
-            label="Default active Tab"
-            icon="info"
-            >
-      </ino-tab>
+      <ino-tab label="Default active Tab" icon="info"></ino-tab>
     </ino-tab-bar>
     <ino-tab-bar active-tab="0">
       <ino-tab
-            label="Active content width indicator"
-            icon="info"
-            indicator-content-width="true"
-            >
+        label="Active content width indicator"
+        icon="info"
+        indicator-content-width
+      >
       </ino-tab>
     </ino-tab-bar>
   </div>
