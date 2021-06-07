0b4b2fbc355fe7b0b6a08a2ebaf71fedb5d94111
Benjamin Pagelsdorf <bpagelsdorf@outlook.de>
2021-06-07 16:22:19 +0200

    fix(elements|ino-select): fix initialization error (#385)
    Co-authored-by: Benjamin Pagelsdorf <bpagelsdorf@inovex.de>

--- packages/elements/src/components/ino-select/ino-select.tsx
+++ packages/elements/src/components/ino-select/ino-select.tsx
@@ -20,7 +20,7 @@ import classNames from 'classnames';
 })
 export class Select implements ComponentInterface {
   // An internal instance of the material design form field.
-  private mdcSelectInstance: MDCSelect;
+  private mdcSelectInstance?: MDCSelect;
   private nativeInputElement?: HTMLInputElement;
 
   @Element() el!: HTMLElement;
@@ -78,7 +78,7 @@ export class Select implements ComponentInterface {
 
     if (this.value) {
       this.setSelectValue(this.value);
-    } else if (this.mdcSelectInstance.value) {
+    } else if (this.mdcSelectInstance?.value) {
       this.value = this.mdcSelectInstance.value;
     }
   }
@@ -91,7 +91,9 @@ export class Select implements ComponentInterface {
     if (this.nativeInputElement) {
       this.nativeInputElement.value = value;
     }
-    this.mdcSelectInstance.value = value;
+    if (this.mdcSelectInstance) {
+      this.mdcSelectInstance.value = value;
+    }
   }
 
   @Listen('MDCSelect:change')
