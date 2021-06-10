8af5801b9b4a433dee352634f442393f5f158303
Jan-Niklas Voß <jvoss@inovex.de>
2021-06-10 15:42:35 +0200

    feat: implement first MWP

--- packages/elements-angular/elements/schematics/ng-add/index.ts
+++ packages/elements-angular/elements/schematics/ng-add/index.ts
@@ -4,6 +4,7 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 // Just return the tree
 export function ngAdd(options: any): Rule {
   console.log('#### ADDING ELEMENTS');
+  console.log(options);
   return (tree: Tree, context: SchematicContext) => {
     context.addTask(new NodePackageInstallTask());
     return tree;
