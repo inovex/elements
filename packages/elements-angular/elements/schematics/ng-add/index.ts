b642d9cf36fbfec598c129227dcad59382db9687
Jan-Niklas Voß <jvoss@inovex.de>
2021-06-10 17:38:45 +0200

    feat: add icons to `angular.json` on `ng add` command

--- packages/elements-angular/elements/schematics/ng-add/index.ts
+++ packages/elements-angular/elements/schematics/ng-add/index.ts
@@ -1,12 +1,54 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  chain,
+  Rule,
+  SchematicContext,
+  Tree,
+} from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  getAngularAppConfig,
+  getProjectName,
+  readConfig,
+  writeConfig,
+} from '../utils';
 
-// Just return the tree
-export function ngAdd(options: any): Rule {
-  console.log('#### ADDING ELEMENTS');
-  console.log(options);
-  return (tree: Tree, context: SchematicContext) => {
+function addAsset(
+  host: Tree,
+  projectName: string,
+  architect: string,
+  asset: string | { glob: string; input: string; output: string }
+) {
+  const config = readConfig(host);
+  const appConfig = getAngularAppConfig(config, projectName);
+  appConfig.architect[architect].options.assets.push(asset);
+  writeConfig(host, config);
+}
+
+function installNodeDeps() {
+  return (_host: Tree, context: SchematicContext) => {
     context.addTask(new NodePackageInstallTask());
+  };
+}
+
+function addIcons() {
+  return async (tree: Tree) => {
+    const projectName = await getProjectName(tree);
+
+    const inoIconsGlob = {
+      glob: '**/*.svg',
+      input: 'node_modules/@inovex.de/elements/dist/inovex-elements/ino-icon',
+      output: 'ino-icon',
+    };
+    addAsset(tree, projectName, 'build', inoIconsGlob);
+    addAsset(tree, projectName, 'test', inoIconsGlob);
     return tree;
   };
+}
+
+export function ngAdd(): Rule {
+  return chain([
+    addIcons(),
+    // install freshly added dependencies
+    installNodeDeps(),
+  ]);
 }
---
+++ packages/elements-angular/elements/schematics/utils.ts
@@ -0,0 +1,51 @@
+import { virtualFs, workspaces } from '@angular-devkit/core';
+import { SchematicsException, Tree } from '@angular-devkit/schematics';
+
+const CONFIG_PATH = 'angular.json';
+
+function createHost(tree: Tree): workspaces.WorkspaceHost {
+  return {
+    async readFile(path: string): Promise<string> {
+      const data = tree.read(path);
+      if (!data) {
+        throw new SchematicsException('File not found.');
+      }
+      return virtualFs.fileBufferToString(data);
+    },
+    async writeFile(path: string, data: string): Promise<void> {
+      return tree.overwrite(path, data);
+    },
+    async isDirectory(path: string): Promise<boolean> {
+      return !tree.exists(path) && tree.getDir(path).subfiles.length > 0;
+    },
+    async isFile(path: string): Promise<boolean> {
+      return tree.exists(path);
+    },
+  };
+}
+
+export function readConfig(host: Tree) {
+  const sourceText = host.read(CONFIG_PATH)?.toString('utf-8') || '';
+  return JSON.parse(sourceText);
+}
+
+export function writeConfig(host: Tree, config: JSON) {
+  host.overwrite(CONFIG_PATH, JSON.stringify(config, null, 2));
+}
+
+export function getAngularAppConfig(
+  config: any,
+  projectName: string
+): any | never {
+  if (!config.projects.hasOwnProperty(projectName)) {
+    throw new SchematicsException(`Could not find project: ${projectName}`);
+  }
+
+  return config.projects[projectName];
+}
+
+export async function getProjectName(tree: Tree) {
+  const host = createHost(tree);
+  const { workspace } = await workspaces.readWorkspace('/', host);
+  return workspace.extensions.defaultProject as string;
+}
