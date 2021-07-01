15373baee368f78b4afe9598616dc0cb1aa8ce23
agerlach <agerlach@inovex.de>
2021-07-01 15:28:00 +0200

    refactor(elements): remove reversed property

--- packages/elements/src/components.d.ts
+++ packages/elements/src/components.d.ts
@@ -902,10 +902,6 @@ export namespace Components {
           * Sets the progress of the progress bar. Should always be between 0 and 1
          */
         "progress"?: number;
-        /**
-          * Reverses the progress bar
-         */
-        "reversed"?: boolean;
     }
     interface InoRadio {
         /**
@@ -2565,10 +2561,6 @@ declare namespace LocalJSX {
           * Sets the progress of the progress bar. Should always be between 0 and 1
          */
         "progress"?: number;
-        /**
-          * Reverses the progress bar
-         */
-        "reversed"?: boolean;
     }
     interface InoRadio {
         /**
