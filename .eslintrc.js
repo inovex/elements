0a6abbce21e98c99c71f858ca530d97dccfdf48b
Jan-Niklas Voß <jvoss@inovex.de>
2021-08-02 14:19:55 +0200

    refactor: add common eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint'],
+};
--- packages/elements-react/.eslintrc.js
+++ packages/elements-react/.eslintrc.js
@@ -1,62 +1,49 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "prettier",
-    "prettier/@typescript-eslint"
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['../../.eslintrc.js', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "tsconfig.json",
+    project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    sourceType: "module"
+    sourceType: 'module',
   },
   plugins: [
-    "eslint-plugin-no-null",
-    "eslint-plugin-prefer-arrow",
-    "eslint-plugin-react",
-    "@typescript-eslint"
+    'eslint-plugin-no-null',
+    'eslint-plugin-prefer-arrow',
+    'eslint-plugin-react',
   ],
   rules: {
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-floating-promises": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unnecessary-type-assertion": "off",
-    "@typescript-eslint/unbound-method": "error",
-    "comma-dangle": "off",
-    "import/no-unassigned-import": "off",
-    "no-cond-assign": "off",
-    "no-null/no-null": "off",
-    "no-template-curly-in-string": "error",
-    "prefer-arrow/prefer-arrow-functions": "error",
-    "react/jsx-boolean-value": [
-      "error",
-      "never"
-    ],
-    "react/jsx-curly-spacing": [
-      "error",
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/unbound-method': 'error',
+    'comma-dangle': 'off',
+    'import/no-unassigned-import': 'off',
+    'no-cond-assign': 'off',
+    'no-null/no-null': 'off',
+    'no-template-curly-in-string': 'error',
+    'prefer-arrow/prefer-arrow-functions': 'error',
+    'react/jsx-boolean-value': ['error', 'never'],
+    'react/jsx-curly-spacing': [
+      'error',
       {
-        "when": "never"
-      }
+        when: 'never',
+      },
     ],
-    "react/jsx-equals-spacing": "off",
-    "react/jsx-key": "off",
-    "react/jsx-no-bind": "off",
-    "react/no-find-dom-node": "off",
-    "react/jsx-tag-spacing": [
-      "off",
+    'react/jsx-equals-spacing': 'off',
+    'react/jsx-key': 'off',
+    'react/jsx-no-bind': 'off',
+    'react/no-find-dom-node': 'off',
+    'react/jsx-tag-spacing': [
+      'off',
       {
-        "afterOpening": "allow",
-        "closingSlash": "allow"
-      }
+        afterOpening: 'allow',
+        closingSlash: 'allow',
+      },
     ],
-    "react/jsx-wrap-multilines": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/self-closing-comp": "off"
-  }
-}
+    'react/jsx-wrap-multilines': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'off',
+  },
+};
