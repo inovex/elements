a513a10f8a9693a70dc68459765830f517f45680
Jan-Niklas Voß <jvoss@inovex.de>
2021-08-02 17:57:04 +0200

    feat: improve printing messages

--- scripts/newComponent.js
+++ scripts/newComponent.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 const camelCase = require('camelcase');
 
-const customElementsJson = require(path.join(
+const CUSTOM_ELEMENTS_JSON = require(path.join(
   __dirname,
   '../packages/storybook/custom-elements.json'
 ));
@@ -22,22 +22,24 @@ const REACT_INDEX_DIR = path.join(
   '../packages/elements-react/src/components/index.ts'
 );
 
-if (!customElementsJson) {
+if (!CUSTOM_ELEMENTS_JSON) {
   console.log(
     chalk.yellow(
       `No 'custom-elements.json' found in packages/storybook. Building first.`
     )
   );
-  shell.exec('lerna exec --scope=@inovex.de/elements -- yarn install');
+  shell.exec('lerna exec --scope=@inovex.de/elements');
 }
 
-const questions = [
-  {
-    type: 'text',
-    name: 'name',
-    message: 'Name of the new component:',
-  },
-];
+function printTask(action, infos) {
+  console.log(chalk.green`{bold ${action} ${infos}.`);
+}
+
+function createAndNotify(path) {
+  printTask('CREATE', path);
+  fs.openSync(path, 'w');
+  return path;
+}
 
 /**
  * Create files in:
@@ -54,7 +56,11 @@ const questions = [
  * @return {Promise<void>}
  */
 async function main() {
-  const { name } = await prompts(questions);
+  const { name } = await prompts({
+    type: 'text',
+    name: 'name',
+    message: 'Name of the new component:',
+  });
 
   if (!name.startsWith('ino')) {
     console.log(chalk.yellow(`Component name should start with "ino-".`));
@@ -62,7 +68,7 @@ async function main() {
     return;
   }
 
-  if (customElementsJson.tags.find((component) => component.name === name)) {
+  if (CUSTOM_ELEMENTS_JSON.tags.find((component) => component.name === name)) {
     console.log(
       chalk.yellow(
         `Component with the name "${name}" already exists. Please choose another name.`
@@ -84,9 +90,7 @@ async function main() {
 
   fs.mkdirSync(componentDir);
   Object.keys(elementsPaths).forEach((path) => {
-    const newFile = elementsPaths[path];
-    console.log(chalk.green(`Creating ${newFile}`));
-    fs.openSync(newFile, 'w');
+    const newFile = createAndNotify(elementsPaths[path]);
     newFiles.push(newFile);
   });
 
@@ -99,15 +103,11 @@ async function main() {
 
   fs.mkdirSync(storybookComponentDir);
   Object.keys(storybookPaths).forEach((path) => {
-    const newFile = storybookPaths[path];
-    console.log(chalk.green(`Creating ${newFile}`));
-    fs.openSync(newFile, 'w');
+    const newFile = createAndNotify(storybookPaths[path]);
     newFiles.push(newFile);
   });
 
-  console.log(
-    chalk.green(`Adding new component to React Wrapper in ${REACT_INDEX_DIR}`)
-  );
+  printTask('ADD', `to React Wrapper in ${REACT_INDEX_DIR}.`);
 
   const nameInPascalCase = camelCase(name, { pascalCase: true });
 
@@ -119,8 +119,10 @@ async function main() {
 >('${name}');`
   );
 
-  console.log(chalk.green(`Adding new files to git ...`));
+  printTask('ADD', `files to git.`);
   shell.exec(`git add ${newFiles.join(' ')}`);
+
+  // TODO: add to scopes
 }
 
 main();
