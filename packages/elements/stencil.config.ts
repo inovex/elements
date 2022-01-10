f1d1f3f8203eb85d32f8a190dd547ceffa537ef9
Patrick Hillert <silentHoo@users.noreply.github.com>
2022-01-10 08:17:25 +0100

    chore(deps): bump storybook from 6.3.2 to 6.4.9 (#475)
    * chore(deps): bump storybook from 6.3.2 to 6.4.9
    
    * chore: remove depracted static dir param
    
    * chore: add plugin to meet new manifest and add argtypes
    
    * chore: simplify usage
    
    Co-authored-by: Pascal Fecht <pfecht@inovex.de>

--- packages/elements/stencil.config.ts
+++ packages/elements/stencil.config.ts
@@ -4,68 +4,6 @@ import { sass } from '@stencil/sass';
 import { angularOutputTarget } from '@stencil/angular-output-target';
 import { promises as fs } from 'fs';
 import { angularOutputTargetFix } from './angular-target-fix';
-
-async function generateCustomElementsJson(docsData: JsonDocs) {
-  const jsonData = {
-    version: 1.2,
-    tags: docsData.components.map((component) => ({
-      name: component.tag,
-      path: component.filePath,
-      description: component.docs,
-
-      attributes: component.props
-        .filter((prop) => prop.attr)
-        .map((prop) => ({
-          name: prop.attr,
-          type: prop.type,
-          description: prop.docs,
-          defaultValue: prop.default,
-          required: prop.required,
-        })),
-
-      properties: component.props.map((prop) => ({
-        name: prop.name,
-        type: prop.type,
-        description: prop.docs,
-        defaultValue: prop.default,
-        required: prop.required,
-      })),
-
-      events: component.events.map((event) => ({
-        name: event.event,
-        description: event.docs,
-      })),
-
-      methods: component.methods.map((method) => ({
-        name: method.name,
-        description: method.docs,
-        signature: method.signature,
-      })),
-
-      slots: component.slots.map((slot) => ({
-        name: slot.name,
-        description: slot.docs,
-      })),
-
-      cssProperties: component.styles
-        .filter((style) => style.annotation === 'prop')
-        .map((style) => ({
-          name: style.name,
-          description: style.docs,
-        })),
-
-      cssParts: component.parts.map((part) => ({
-        name: part.name,
-        description: part.docs,
-      })),
-    })),
-  };
-  await fs.writeFile(
-    '../storybook/custom-elements.json',
-    JSON.stringify(jsonData, null, 2)
-  );
-}
-
 export const config: Config = {
   buildEs5: 'prod',
   extras: {
@@ -88,8 +26,8 @@ export const config: Config = {
     },
     { type: 'docs-readme' },
     {
-      type: 'docs-custom',
-      generator: generateCustomElementsJson,
+      type: 'docs-json',
+      file: '../storybook/elements-stencil-docs.json',
     },
     angularOutputTarget({
       componentCorePackage: '@inovex.de/elements',
