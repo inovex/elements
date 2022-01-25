ab7077be6815db4d3109b94046238006a9c15328
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2022-01-25 11:19:15 +0100

    feat(elements|ino-markdown-editor): implement custom angular value-accessor

---
+++ packages/elements-angular/elements/src/directives/control-value-accesors/ino-markdown-editor-value-accessor.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import {TextValueAccessorDirective} from "./text-value-accessor.directive";
+
+@Directive({
+  selector: 'ino-markdown-editor',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: InoMarkdownEditorValueAccessorDirective,
+      multi: true,
+    },
+  ],
+})
+export class InoMarkdownEditorValueAccessorDirective extends TextValueAccessorDirective {
+  constructor(el: ElementRef) {
+    super(el);
+  }
+
+  writeValue(value: any): void {
+    super.writeValue(value);
+    this.el.nativeElement.initialValue = this.lastValue;
+  }
+}
