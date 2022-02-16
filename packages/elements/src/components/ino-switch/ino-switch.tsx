13711358c5bfd723837cc6a3c5b68bcadaeedf1d
Sl1nd <s_lindauer@t-online.de>
2022-02-16 16:02:36 +0100

    fix(ino-switch): query unique switch elements in page

--- packages/elements/src/components/ino-switch/ino-switch.tsx
+++ packages/elements/src/components/ino-switch/ino-switch.tsx
@@ -62,7 +62,10 @@ export class Switch implements ComponentInterface {
   @Prop() colorScheme: ColorScheme = 'primary';
 
   componentDidLoad() {
-    this.switch = new MDCSwitch(document.querySelector('.mdc-switch'));
+    const switchRoot = document
+      .getElementById(this.switchId)
+      .closest('.mdc-switch');
+    this.switch = new MDCSwitch(switchRoot);
   }
 
   componentDidUnLoad() {
