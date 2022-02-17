5dd9a17a16199b385d33f87d6e483e0de02ca249
Jacob Cofman <jcofman@inovex.de>
2022-02-17 13:28:39 +0100

    chore: add react-output-target stencil script

--- packages/elements-react/src/components/index.ts
+++ packages/elements-react/src/components/index.ts
@@ -1,19 +1,13 @@
-import { JSX as StencilJSX } from '@stencil/core';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { addIcons } from '@inovex.de/elements/dist/collection/util/icons';
-import { ICON_PATHS } from '@inovex.de/elements/dist/inovex-elements/ino-icon/index.esm.js';
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@inovex.de/elements';
+
 import { defineCustomElements } from '@inovex.de/elements/dist/loader';
 
-import { createReactComponent } from './createComponent';
-
-defineCustomElements(window);
-addIcons(ICON_PATHS);
-
-//
-// Export all the elements we have. This is THE react wrapper.
-//
-
+<<<<<<< Updated upstream
 export const InoAutocomplete = createReactComponent<
   StencilJSX.IntrinsicElements['ino-autocomplete'],
   HTMLInoAutocompleteElement
@@ -214,3 +208,56 @@ export const InoCurrencyInput = createReactComponent<
   StencilJSX.IntrinsicElements['ino-currency-input'],
   HTMLInoCurrencyInputElement
 >('ino-currency-input');
+=======
+defineCustomElements();
+export const InoAutocomplete = /*@__PURE__*/createReactComponent<JSX.InoAutocomplete, HTMLInoAutocompleteElement>('ino-autocomplete');
+export const InoButton = /*@__PURE__*/createReactComponent<JSX.InoButton, HTMLInoButtonElement>('ino-button');
+export const InoCard = /*@__PURE__*/createReactComponent<JSX.InoCard, HTMLInoCardElement>('ino-card');
+export const InoCarousel = /*@__PURE__*/createReactComponent<JSX.InoCarousel, HTMLInoCarouselElement>('ino-carousel');
+export const InoCarouselSlide = /*@__PURE__*/createReactComponent<JSX.InoCarouselSlide, HTMLInoCarouselSlideElement>('ino-carousel-slide');
+export const InoCheckbox = /*@__PURE__*/createReactComponent<JSX.InoCheckbox, HTMLInoCheckboxElement>('ino-checkbox');
+export const InoChip = /*@__PURE__*/createReactComponent<JSX.InoChip, HTMLInoChipElement>('ino-chip');
+export const InoChipSet = /*@__PURE__*/createReactComponent<JSX.InoChipSet, HTMLInoChipSetElement>('ino-chip-set');
+export const InoControlItem = /*@__PURE__*/createReactComponent<JSX.InoControlItem, HTMLInoControlItemElement>('ino-control-item');
+export const InoDatepicker = /*@__PURE__*/createReactComponent<JSX.InoDatepicker, HTMLInoDatepickerElement>('ino-datepicker');
+export const InoDialog = /*@__PURE__*/createReactComponent<JSX.InoDialog, HTMLInoDialogElement>('ino-dialog');
+export const InoFab = /*@__PURE__*/createReactComponent<JSX.InoFab, HTMLInoFabElement>('ino-fab');
+export const InoFabSet = /*@__PURE__*/createReactComponent<JSX.InoFabSet, HTMLInoFabSetElement>('ino-fab-set');
+export const InoFormRow = /*@__PURE__*/createReactComponent<JSX.InoFormRow, HTMLInoFormRowElement>('ino-form-row');
+export const InoHeader = /*@__PURE__*/createReactComponent<JSX.InoHeader, HTMLInoHeaderElement>('ino-header');
+export const InoIcon = /*@__PURE__*/createReactComponent<JSX.InoIcon, HTMLInoIconElement>('ino-icon');
+export const InoIconButton = /*@__PURE__*/createReactComponent<JSX.InoIconButton, HTMLInoIconButtonElement>('ino-icon-button');
+export const InoImg = /*@__PURE__*/createReactComponent<JSX.InoImg, HTMLInoImgElement>('ino-img');
+export const InoImgList = /*@__PURE__*/createReactComponent<JSX.InoImgList, HTMLInoImgListElement>('ino-img-list');
+export const InoInput = /*@__PURE__*/createReactComponent<JSX.InoInput, HTMLInoInputElement>('ino-input');
+export const InoInputFile = /*@__PURE__*/createReactComponent<JSX.InoInputFile, HTMLInoInputFileElement>('ino-input-file');
+export const InoLabel = /*@__PURE__*/createReactComponent<JSX.InoLabel, HTMLInoLabelElement>('ino-label');
+export const InoList = /*@__PURE__*/createReactComponent<JSX.InoList, HTMLInoListElement>('ino-list');
+export const InoListDivider = /*@__PURE__*/createReactComponent<JSX.InoListDivider, HTMLInoListDividerElement>('ino-list-divider');
+export const InoListItem = /*@__PURE__*/createReactComponent<JSX.InoListItem, HTMLInoListItemElement>('ino-list-item');
+export const InoMarkdownEditor = /*@__PURE__*/createReactComponent<JSX.InoMarkdownEditor, HTMLInoMarkdownEditorElement>('ino-markdown-editor');
+export const InoMenu = /*@__PURE__*/createReactComponent<JSX.InoMenu, HTMLInoMenuElement>('ino-menu');
+export const InoNavDrawer = /*@__PURE__*/createReactComponent<JSX.InoNavDrawer, HTMLInoNavDrawerElement>('ino-nav-drawer');
+export const InoNavItem = /*@__PURE__*/createReactComponent<JSX.InoNavItem, HTMLInoNavItemElement>('ino-nav-item');
+export const InoOption = /*@__PURE__*/createReactComponent<JSX.InoOption, HTMLInoOptionElement>('ino-option');
+export const InoOptionGroup = /*@__PURE__*/createReactComponent<JSX.InoOptionGroup, HTMLInoOptionGroupElement>('ino-option-group');
+export const InoPopover = /*@__PURE__*/createReactComponent<JSX.InoPopover, HTMLInoPopoverElement>('ino-popover');
+export const InoProgressBar = /*@__PURE__*/createReactComponent<JSX.InoProgressBar, HTMLInoProgressBarElement>('ino-progress-bar');
+export const InoRadio = /*@__PURE__*/createReactComponent<JSX.InoRadio, HTMLInoRadioElement>('ino-radio');
+export const InoRadioGroup = /*@__PURE__*/createReactComponent<JSX.InoRadioGroup, HTMLInoRadioGroupElement>('ino-radio-group');
+export const InoRange = /*@__PURE__*/createReactComponent<JSX.InoRange, HTMLInoRangeElement>('ino-range');
+export const InoSegmentButton = /*@__PURE__*/createReactComponent<JSX.InoSegmentButton, HTMLInoSegmentButtonElement>('ino-segment-button');
+export const InoSegmentGroup = /*@__PURE__*/createReactComponent<JSX.InoSegmentGroup, HTMLInoSegmentGroupElement>('ino-segment-group');
+export const InoSelect = /*@__PURE__*/createReactComponent<JSX.InoSelect, HTMLInoSelectElement>('ino-select');
+export const InoSidebar = /*@__PURE__*/createReactComponent<JSX.InoSidebar, HTMLInoSidebarElement>('ino-sidebar');
+export const InoSnackbar = /*@__PURE__*/createReactComponent<JSX.InoSnackbar, HTMLInoSnackbarElement>('ino-snackbar');
+export const InoSpinner = /*@__PURE__*/createReactComponent<JSX.InoSpinner, HTMLInoSpinnerElement>('ino-spinner');
+export const InoSwitch = /*@__PURE__*/createReactComponent<JSX.InoSwitch, HTMLInoSwitchElement>('ino-switch');
+export const InoTab = /*@__PURE__*/createReactComponent<JSX.InoTab, HTMLInoTabElement>('ino-tab');
+export const InoTabBar = /*@__PURE__*/createReactComponent<JSX.InoTabBar, HTMLInoTabBarElement>('ino-tab-bar');
+export const InoTable = /*@__PURE__*/createReactComponent<JSX.InoTable, HTMLInoTableElement>('ino-table');
+export const InoTableCell = /*@__PURE__*/createReactComponent<JSX.InoTableCell, HTMLInoTableCellElement>('ino-table-cell');
+export const InoTableRow = /*@__PURE__*/createReactComponent<JSX.InoTableRow, HTMLInoTableRowElement>('ino-table-row');
+export const InoTextarea = /*@__PURE__*/createReactComponent<JSX.InoTextarea, HTMLInoTextareaElement>('ino-textarea');
+export const InoTooltip = /*@__PURE__*/createReactComponent<JSX.InoTooltip, HTMLInoTooltipElement>('ino-tooltip');
+>>>>>>> Stashed changes
