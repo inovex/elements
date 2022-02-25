f96b01af5b2ffc03f99c888c7b132e224be3c1b8
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-02-25 15:34:23 +0100

    docs(storybook): add comments

--- packages/storybook/src/stories/template-generator.ts
+++ packages/storybook/src/stories/template-generator.ts
@@ -1,6 +1,9 @@
 import { Story } from '@storybook/web-components';
 import docsJson from '../../elements-stencil-docs';
 
+/**
+ * A utility class that helps with creating type-safe stories
+ */
 export class TemplateGenerator<Component> {
   private readonly tagName: string;
   private readonly templateFn: Story<Component>;
@@ -10,6 +13,14 @@ export class TemplateGenerator<Component> {
     this.templateFn = templateFn;
   }
 
+  /**
+   * Generates a new story based of the provided args.
+   * Additionally, adds a description if the parameter `prop` is used.
+   * The `prop` is used to search for the JSDoc comment of the specified property in the `elements-stencil-docs.json`.
+   *
+   * @param args Properties to assign to the template
+   * @param prop The name of the "main" property the stories is written for
+   */
   public generateStory(args: Component, prop?: string) {
     const BoundTemplate: Story<Component> = this.templateFn.bind({});
     BoundTemplate.args = { ...args };
