a3b23efc715698dd81a557452c14850632a7874a
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-02-28 16:04:17 +0100

    test(elements|ino-table-header-cell): add test cases

--- packages/elements/src/components/ino-table-header-cell/ino-table-header-cell.e2e.ts
+++ packages/elements/src/components/ino-table-header-cell/ino-table-header-cell.e2e.ts
@@ -1,23 +1,82 @@
 import { setupPageWithContent } from '../../util/e2etests-setup';
+import { E2EElement, E2EPage } from '@stencil/core/testing';
+import { SortDirection, SortDirectionChangeDetails } from '../../interface';
+import { setPropertyOfEl } from '../../util/test-utils';
+
+const columnId = 'my-id';
 
 describe('ino-table', () => {
+  let page: E2EPage;
+  let headerCell: E2EElement;
+  let sortBtn: E2EElement;
 
-  describe('Properties', () => {
-    it('should render with numerical values', async () => {
-      const page = await setupPageWithContent(`
-        <ino-table>
-          <tr slot="header-row">
-            <ino-table-header-cell>Test</ino-table-header-cell>
-          </tr>
-        </ino-table>
-      `);
-      const cell = await page.find('ino-table-header-cell');
+  beforeEach(async () => {
+    page = await setupPageWithContent(`<ino-table>
+      <tr slot="header-row">
+        <ino-table-header-cell column-id="${columnId}" label="Simple Text field">
+          <ino-input placeholder="Search for XY..." data-ino-focus>
+            <ino-icon slot="icon-trailing" icon="close"></ino-icon>
+          </ino-input>
+        </ino-table-header-cell>
+      </tr>
+    </ino-table>`);
 
-      await cell.classList.add('ino-table__cell--numeric');
-      await page.waitForChanges();
-
-      expect(cell).toHaveClass('ino-table__cell--numeric');
-    });
+    headerCell = await page.find('ino-table-header-cell');
+    sortBtn = await page.find('ino-icon-button');
   });
 
+  function getDummySortDirectionDetails(
+    sortDirection: SortDirection
+  ): SortDirectionChangeDetails {
+    return {
+      columnId,
+      sortDirection,
+    };
+  }
+
+  it('should render with sort button', async () => {
+    expect(sortBtn).toBeTruthy();
+  });
+
+  it(`should emit sort event with 'desc' as detail when clicking the sort button`, async () => {
+    const sortDirectionChangeSpy = await page.spyOnEvent('sortDirectionChange');
+    await sortBtn.click();
+    expect(sortDirectionChangeSpy).toHaveReceivedEventDetail(
+      getDummySortDirectionDetails('desc')
+    );
+  });
+
+  it(`should emit sort event with 'asc' as detail when clicking the sort button`, async () => {
+    headerCell.setProperty('sortDirection', 'desc');
+    await page.waitForChanges();
+    const sortDirectionChangeSpy = await page.spyOnEvent('sortDirectionChange');
+    await sortBtn.click();
+    expect(sortDirectionChangeSpy).toHaveReceivedEventDetail(
+      getDummySortDirectionDetails('asc')
+    );
+  });
+
+  it(`should emit sort event with 'desc' as detail when clicking the sort button`, async () => {
+    headerCell.setProperty('sortStart', 'desc');
+    const sortDirectionChangeSpy = await page.spyOnEvent('sortDirectionChange');
+    await sortBtn.click();
+    expect(sortDirectionChangeSpy).toHaveReceivedEventDetail(
+      getDummySortDirectionDetails('desc')
+    );
+  });
+
+  it(`should emit sort event with 'desc' as detail when clicking the sort button`, async () => {
+
+    await setPropertyOfEl<HTMLInoTableHeaderCellElement>(
+      page,
+      'ino-table-header-cell',
+      { sortStart: 'asc' }
+    );
+
+    const sortDirectionChangeSpy = await page.spyOnEvent('sortDirectionChange');
+    await sortBtn.click();
+    expect(sortDirectionChangeSpy).toHaveReceivedEventDetail(
+      getDummySortDirectionDetails('asc')
+    );
+  });
 });
