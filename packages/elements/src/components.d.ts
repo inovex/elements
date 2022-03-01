50fa38ec8800c8999da7ba9792364c41a0f193de
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2022-03-01 15:26:40 +0100

    feat(elements|ino-tab): use default slot instead of label property

--- packages/elements/src/components.d.ts
+++ packages/elements/src/components.d.ts
@@ -1054,6 +1054,7 @@ export namespace Components {
         "actionText"?: string;
         /**
           * [DEPRECATED] Please use the default slot instead The text message to display.
+          * @deprecated
          */
         "message"?: string;
         /**
@@ -1119,7 +1120,8 @@ export namespace Components {
          */
         "indicatorContentWidth": boolean;
         /**
-          * Indicates a label text in the tab.
+          * [DEPRECATED] Please use the default slot instead. Indicates a label text in the tab.
+          * @deprecated
          */
         "label"?: string;
         /**
@@ -2723,6 +2725,7 @@ declare namespace LocalJSX {
         "actionText"?: string;
         /**
           * [DEPRECATED] Please use the default slot instead The text message to display.
+          * @deprecated
          */
         "message"?: string;
         /**
@@ -2800,7 +2803,8 @@ declare namespace LocalJSX {
          */
         "indicatorContentWidth"?: boolean;
         /**
-          * Indicates a label text in the tab.
+          * [DEPRECATED] Please use the default slot instead. Indicates a label text in the tab.
+          * @deprecated
          */
         "label"?: string;
         /**
