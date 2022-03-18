c53fac4ecd6952c237669308edc2e12d1af4d654
Jacob Cofman <jcofman@inovex.de>
2022-03-18 17:51:40 +0100

    chore: add svgo to minimize icons and autoadd currentColor

---
+++ packages/elements/src/util/svgo.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  multipass: true, // boolean. false by default
+  js2svg: {
+    indent: 2, // string with spaces or number of spaces. 4 by default
+    pretty: false, // minize all svgs
+  },
+  plugins: [
+    // set of built-in plugins enabled by default
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeViewBox: false, // ⚠️ we need to keep the viewbox for correct sizing
+        },
+      },
+    },
+    // enable built-in plugins by name
+    {
+      name: 'sortAttrs',
+      params: {
+        xmlnsOrder: 'alphabetical',
+      },
+    },
+    {
+      name: 'convertColors',
+      params: {
+        currentColor: true, // ⚠️ set all fill icons to currentColor
+      },
+    },
+  ],
+};
