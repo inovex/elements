0ead83a40a83d7a225438348830c3b12f55424ee
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-05-02 18:21:24 +0200

    refactor: storybook cleanup

--- packages/storybook/src/stories/ino-button/ino-button.stories.ts
+++ packages/storybook/src/stories/ino-button/ino-button.stories.ts
@@ -1,26 +1,16 @@
 import { Components } from '@inovex.de/elements';
-import { Meta, Story } from '@storybook/web-components';
-import { html, TemplateResult } from 'lit-html';
+import { Meta } from '@storybook/web-components';
+import { html } from 'lit-html';
 import { TemplateGenerator } from '../template-generator';
 import { decorateStoryWithClass } from '../utils';
-
 import './ino-button.scss';
 
 export default {
   title: 'Buttons/<ino-button>',
   component: 'ino-button',
   decorators: [(story) => decorateStoryWithClass(story, 'story-button')],
-  parameters: {
-    actions: {
-      handles: [
-        'click .customizable-button',
-        'reset .reset-form',
-        'submit .submit-form',
-      ],
-    },
-  },
-   // will be used as default props for all stories
-   args: {
+  // will be used as default props for all stories
+  args: {
     variant: 'filled',
     dense: false,
     disabled: false,
@@ -43,31 +33,27 @@ const template = new TemplateGenerator<Components.InoButton>(
   `
 );
 
-
 export const Playground = template.generatePlaygroundStory();
 
-export const Filled = template.generateStoryForProp('variant', 'filled')
-export const Outlined = template.generateStoryForProp('variant', 'outlined')
-export const Text = template.generateStoryForProp('variant', 'text')
-export const Disabled = template.generateStoryForProp('disabled', true )
-export const Dense = template.generateStoryForProp('dense', true )
-export const Loading = template.generateStoryForProp('loading', true )
+export const Filled = template.generateStoryForProp('variant', 'filled');
+export const Outlined = template.generateStoryForProp('variant', 'outlined');
+export const Text = template.generateStoryForProp('variant', 'text');
+export const Disabled = template.generateStoryForProp('disabled', true);
+export const Dense = template.generateStoryForProp('dense', true);
+export const Loading = template.generateStoryForProp('loading', true);
 
 /*
- In order to include icons use the icon-leading˙ or ˙icon-trailing˙ slot
+ In order to include icons, use the `icon-leading` or `icon-trailing` slot
  */
 export const LeadingAndTrailingIcon = () => html`
-    <ino-button
-      class="storybook-button"
-    >
+  <div class="container">
+    <ino-button>
       <ino-icon icon="info" slot="icon-leading"></ino-icon>
       Label
     </ino-button>
-
-    <ino-button
-      class="storybook-button"
-    >
+    <ino-button>
       <ino-icon icon="info" slot="icon-trailing"></ino-icon>
       Label
     </ino-button>
-  `;
+  </div>
+`;
