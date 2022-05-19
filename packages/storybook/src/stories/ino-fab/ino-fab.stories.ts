99ba19fe518a3e148ead4d96cd9c9282bf6257be
MariaLStefan <maria-luisa.stefan@inovex.de>
2022-05-19 20:09:04 +0200

    refactor(storybook|ino-fab): reformat stories

--- packages/storybook/src/stories/ino-fab/ino-fab.stories.ts
+++ packages/storybook/src/stories/ino-fab/ino-fab.stories.ts
@@ -1,6 +1,7 @@
 import { Components } from '@inovex.de/elements';
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
+import { TemplateGenerator } from '../template-generator';
 import { decorateStoryWithClass, withIconControl } from '../utils';
 
 import './ino-fab.scss';
@@ -9,31 +10,33 @@ export default {
   title: 'Buttons/<ino-fab>',
   component: 'ino-fab',
   decorators: [(story) => decorateStoryWithClass(story, 'story-fab')],
-} as Meta;
+  args: {
+    disabled: false,
+    edgePosition: 'top-left',
+    extended: false,
+    label: 'Label',
+    mini: false,
+    tooltipPlacement: 'right',
+  }
+} as Meta<Components.InoFab>;
 
-export const Playground: Story<Components.InoFab> = (args) => html`
+const template = new TemplateGenerator<Components.InoFab>(
+  'ino-fab',
+  args => html`
   <ino-fab
-    class="customizable-fab"
-    disabled="${args.disabled}"
-    edge-position="${args.edgePosition}"
-    extended="${args.extended}"
-    label="${args.label}"
-    mini="${args.mini}"
-    tooltip-placement="${args.tooltipPlacement}"
+  class="customizable-fab"
+  disabled="${args.disabled}"
+  edge-position="${args.edgePosition}"
+  extended="${args.extended}"
+  label="${args.label}"
+  mini="${args.mini}"
+  tooltip-placement="${args.tooltipPlacement}"
   >
-    <ino-icon slot="icon-leading" icon="${args.icon}" />
+  <ino-icon slot="icon-leading" icon="${args.icon}" />
   </ino-fab>
-`;
+`);
 
-Playground.args = {
-  disabled: false,
-  edgePosition: 'top-left',
-  extended: false,
-  label: 'Label',
-  mini: false,
-  tooltipPlacement: 'right',
-};
-
+export const Playground = template.generatePlaygroundStory();
 withIconControl(Playground, 'icon', 'add');
 
 Playground.argTypes.edgePosition = {
@@ -67,7 +70,10 @@ Playground.argTypes.tooltipPlacement = {
   ],
 };
 
-export const Variants = () => html`
+
+const templateVariants = new TemplateGenerator<Components.InoFab>(
+  'ino-fab',
+  args => html`
   <div class='ino-fab-variants'>
     <ino-fab tooltip-placement='none' label='Default Fab'>
       <ino-icon slot='icon-leading' icon='add'></ino-icon>
@@ -82,10 +88,19 @@ export const Variants = () => html`
       <ino-icon slot='icon-leading' icon='add'></ino-icon>
     </ino-fab>
   </div>
-  </div>
-`;
+`);
+/**
+ * In order to achive diffrent variations, use this properties:
+ * 
+ * - `mini`: Optional, modifies the FAB to a smaller size
+ * - `extended`: Optional, modifies the FAB to wider size which includes a text label.
+ * - `disabled`: Disables the button
+ */
+export const Variants = templateVariants.generatePlaygroundStory();
 
-export const EdgePositions = () => html`
+const templateEdgePositions = new TemplateGenerator<Components.InoFab>(
+  'ino-fab',
+  args => html`
   <div class="ino-fab-variants">
     <ino-fab label="top-left" edge-position="top-left">
       <ino-icon slot="icon-leading" icon="add"></ino-icon>
@@ -103,4 +118,15 @@ export const EdgePositions = () => html`
       <ino-icon slot="icon-leading" icon="add"></ino-icon>
     </ino-fab>
   </div>
-`;
+`);
+/**
+ * In order to achive edges on the fab-button, add `edge-position`.
+ * 
+ * Possible values are:
+ *  'top-right'
+ *  'top-left'
+ *  'bottom-right'
+ *  'bottom-left'
+ *  'none' = 'top-left';
+ */
+export const EdgePositions = templateEdgePositions.generatePlaygroundStory();
