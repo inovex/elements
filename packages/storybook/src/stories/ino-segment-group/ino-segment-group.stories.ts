2ff8dfe771cdd6d08010fc3a754226f7c0df45ea
MariaLStefan <maria-luisa.stefan@inovex.de>
2022-05-24 13:28:50 +0200

    refactor(storybook|ino-segment-group): reformat stories

--- packages/storybook/src/stories/ino-segment-group/ino-segment-group.stories.ts
+++ packages/storybook/src/stories/ino-segment-group/ino-segment-group.stories.ts
@@ -2,6 +2,7 @@ import { Components } from '@inovex.de/elements';
 import { useEffect } from '@storybook/client-api';
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit-html';
+import { TemplateGenerator } from '../template-generator';
 import { decorateStoryWithClass } from '../utils';
 import './ino-segment-group.scss';
 
@@ -33,85 +34,53 @@ export default {
       handles: ['checkedChange'],
     },
   },
-} as Meta;
-export const Playground: Story<Components.InoSegmentGroup> = (args) => html`
-  <ino-segment-group id="segment-grp" name="${args.name}" value="${args.value}">
-    <ino-segment-button value="opt-1">Option 1</ino-segment-button>
-    <ino-segment-button value="opt-2">Option 2</ino-segment-button>
-    <ino-segment-button value="opt-3">Option 3</ino-segment-button>
-    <ino-segment-button value="opt-4">Option 4</ino-segment-button>
-  </ino-segment-group>
-`;
-Playground.args = {
-  name: '',
-  value: 'opt-2',
-};
-Playground.argTypes = {
-  value: {
-    control: {
-      type: 'select',
+  argTypes: {
+    value: {
+      control: {
+        type: 'select',
+      },
+      options: ['opt-1', 'opt-2', 'opt-3', 'opt-4'],
     },
-    options: ['opt-1', 'opt-2', 'opt-3', 'opt-4'],
   },
-};
+  args: {
+    name: '',
+    value: 'opt-2',
+    dense: false,
+  }
+} as Meta<Components.InoSegmentGroup>;
 
-export const DenseGroupWithCheckedOption = () => html`
-  <ino-segment-group value="3" id="dense-segment-grp">
-    <ino-segment-button dense value="1">Option 1</ino-segment-button>
-    <ino-segment-button dense value="2">Option 2</ino-segment-button>
-    <ino-segment-button dense value="3">Option 3</ino-segment-button>
-    <ino-segment-button dense value="4">Option 4</ino-segment-button>
+type InoSegmentGroupExtended = Components.InoSegmentGroup & {
+  dense: boolean;
+}
+
+const template = new TemplateGenerator<InoSegmentGroupExtended>(
+  'ino-segment-group',
+  args => html`
+  <ino-segment-group id="segment-grp" name="${args.name}" value="${args.value}">
+    <ino-segment-button dense="${(args.dense)}" value="opt-1">Option 1</ino-segment-button>
+    <ino-segment-button dense="${(args.dense)}" value="opt-2">Option 2</ino-segment-button>
+    <ino-segment-button dense="${(args.dense)}" value="opt-3">Option 3</ino-segment-button>
+    <ino-segment-button dense="${(args.dense)}" value="opt-4">Option 4</ino-segment-button>
   </ino-segment-group>
-`;
+`);
+export const Playground = template.generatePlaygroundStory();
 
-export const GroupWithNoCheckedOption = () => html`
-  <ino-segment-group id="segment-grp">
-    <ino-segment-button value="1">Option 1</ino-segment-button>
-    <ino-segment-button value="2">Option 2</ino-segment-button>
-    <ino-segment-button value="3">Option 3</ino-segment-button>
-    <ino-segment-button value="4">Option 4</ino-segment-button>
-  </ino-segment-group>
-`;
+export const DenseGroupWithCheckedOption = template.generateStoryForProp('value', 'opt-3');
+DenseGroupWithCheckedOption.args ={
+  dense: true,
+  value: 'opt-3',
+}
 
-export const SingleButtonGroup = () => html`
+export const GroupWithNoCheckedOption = template.generatePlaygroundStory();
+GroupWithNoCheckedOption.args = {
+  value: '',
+}
+
+const templateSingleButtonGroup = new TemplateGenerator<Components.InoSegmentGroup>(
+  'ino-segment-group',
+  args => html`
   <ino-segment-group id="single-segment-grp">
     <ino-segment-button value="1">Option 1</ino-segment-button>
   </ino-segment-group>
-`;
-
-/*
-<style>
-ino-segment-button.customizable-segment-btn {
-  --ino-segment-button-color: ${text(
-    '--ino-segment-button-color',
-    '#c1c1c1',
-    'Custom Properties'
-  )};
-  --ino-segment-button-checked-color: ${text(
-    '--ino-segment-button-checked-color',
-    '#0d10f3',
-    'Custom Properties'
-  )};
-  --ino-segment-button-hover-color: ${text(
-    '--ino-segment-button-hover-color',
-    '#5d60f7',
-    'Custom Properties'
-  )};
-  --ino-segment-button-disabled-color: ${text(
-    '--ino-segment-button-disabled-color',
-    '#777777',
-    'Custom Properties'
-  )};
-  --ino-segment-button-hover-shadow: ${text(
-    '--ino-segment-button-hover-shadow',
-    '0 4px 8px 0 rgba(61, 64, 245, 0.3), 0 12px 28px 0 rgba(61, 214, 245, 0.14)',
-    'Custom Properties'
-  )};
-  --ino-segment-button-checked-shadow: ${text(
-    '--ino-segment-button-checked-shadow',
-    '0 8px 16px 0 rgba(61, 64, 245, 0.3),0 16px 32px 0 rgba(61, 214, 245, 0.14)',
-    'Custom Properties'
-  )};
-}
-</style>
-*/
+`);
+export const SingleButtonGroup = templateSingleButtonGroup.generatePlaygroundStory();
