46a664fef55b35b9da5f75c58c344c94957f404f
MariaLStefan <maria-luisa.stefan@inovex.de>
2022-05-31 21:25:30 +0200

    refactor(storybook|ino-list): reformat stories

--- packages/storybook/src/stories/ino-list/ino-list.stories.ts
+++ packages/storybook/src/stories/ino-list/ino-list.stories.ts
@@ -1,99 +1,50 @@
 import { Meta } from '@storybook/web-components';
 import { html } from 'lit-html';
+import { TemplateGenerator } from '../template-generator';
+import { Components } from '@inovex.de/elements';
 
 export default {
   title: 'Structure/ino-list ',
   component: 'ino-list',
-} as Meta;
+  args: {
+    dense: false,
+    twoLines: false,
+    avatar: false,
+    secondaryText: "",
+  }
+} as Meta<Components.InoList>;
 
-export const Playground = (args) => html`
+type InoListExtended = Components.InoList & {
+  secondaryText: string,
+}
+
+const exampleImg = html
+`
+      <ino-img
+      slot="leading"
+      src="https://cdn-images-1.medium.com/max/1600/1*HP8l7LMMt7Sh5UoO1T-yLQ.png"
+      ratio-width="1"
+      ratio-height="1"
+    ></ino-img>
+    `;
+
+const template = new TemplateGenerator<InoListExtended>(
+  'ino-list',
+  args => html`
   <ino-list
     dense="${args.dense}"
     two-lines="${args.twoLines}"
     avatar="${args.avatar}"
   >
-    <ino-list-item text="First text item"></ino-list-item>
-    <ino-list-item text="Second text item"></ino-list-item>
-    <ino-list-item text="Third text item"></ino-list-item>
-  </ino-list>
-`;
-Playground.args = {
-  dense: false,
-  twoLines: false,
-  avatar: false,
-};
+    <ino-list-item text="First text item" secondary-text="${args.secondaryText}">${args.avatar? exampleImg : ""}</ino-list-item>
+    <ino-list-item text="Second text item" secondary-text="${args.secondaryText}">${args.avatar? exampleImg : ""}</ino-list-item>
+    <ino-list-item text="Third text item" secondary-text="${args.secondaryText}">${args.avatar? exampleImg : ""}</ino-list-item>
+</ino-list>
+`);
 
-const exampleImg = html`
-  <ino-img
-    slot="leading"
-    src="https://cdn-images-1.medium.com/max/1600/1*HP8l7LMMt7Sh5UoO1T-yLQ.png"
-    ratio-width="1"
-    ratio-height="1"
-  ></ino-img>
-`;
-
-export const TwoLines = () => html`
-  <ino-list two-lines>
-    <ino-list-item
-      text="First text item"
-      secondary-text="Secondary"
-    ></ino-list-item>
-    <ino-list-item
-      text="Second text item"
-      secondary-text="Secondary"
-    ></ino-list-item>
-    <ino-list-item
-      text="Third text item"
-      secondary-text="Secondary"
-    ></ino-list-item>
-  </ino-list>
-`;
-
-export const Dense = () => html`
-  <h4>Default</h4>
-  <ino-list dense class="first-list">
-    <ino-list-item text="First text item"></ino-list-item>
-    <ino-list-item text="Second text item"></ino-list-item>
-    <ino-list-item text="Third text item"></ino-list-item>
-  </ino-list>
-
-  <h4>Dense and Two Lines</h4>
-  <ino-list dense two-lines>
-    <ino-list-item
-      text="First text item"
-      secondary-text="Secondary"
-    ></ino-list-item>
-    <ino-list-item
-      text="Second text item"
-      secondary-text="Secondary"
-    ></ino-list-item>
-    <ino-list-item
-      text="Third text item"
-      secondary-text="Secondary"
-    ></ino-list-item>
-  </ino-list>
-`;
-
-export const Avatar = () => html`
-  <h4>Default</h4>
-  <ino-list avatar>
-    <ino-list-item text="First text item"> ${exampleImg} </ino-list-item>
-    <ino-list-item text="Second text item"> ${exampleImg} </ino-list-item>
-  </ino-list>
-
-  <h4>Avatar and Dense</h4>
-  <ino-list avatar dense>
-    <ino-list-item text="First text item"> ${exampleImg} </ino-list-item>
-    <ino-list-item text="Second text item"> ${exampleImg} </ino-list-item>
-  </ino-list>
-
-  <h4>Avatar and Two Lines</h4>
-  <ino-list avatar two-lines>
-    <ino-list-item text="First text item" secondary-text="Secondary">
-      ${exampleImg}
-    </ino-list-item>
-    <ino-list-item text="Second text item" secondary-text="Secondary">
-      ${exampleImg}
-    </ino-list-item>
-  </ino-list>
-`;
+export const Playground = template.generatePlaygroundStory();
+export const TwoLines = template.generateStoryForProp('twoLines', true, {
+  secondaryText: "Secondary",
+});
+export const Dense = template.generateStoryForProp('dense', true);
+export const Avatar = template.generateStoryForProp('avatar', true);
