9a9ca618587eeebf7e8f5b39c0354edba09ba4c3
MariaLStefan <maria-luisa.stefan@inovex.de>
2022-06-02 18:44:15 +0200

    refactor(storybook|ino-tab-bar): reformat stories

--- packages/storybook/src/stories/ino-tab-bar/ino-tab-bar.stories.ts
+++ packages/storybook/src/stories/ino-tab-bar/ino-tab-bar.stories.ts
@@ -1,6 +1,7 @@
 import { Components } from '@inovex.de/elements';
 import { useEffect } from '@storybook/client-api';
-import { Meta, Story } from '@storybook/web-components';
+import { Meta } from '@storybook/web-components';
+import { TemplateGenerator } from '../template-generator';
 import { html } from 'lit-html';
 import { decorateStoryWithClass } from '../utils';
 import './ino-tab-bar.scss';
@@ -26,43 +27,31 @@ export default {
       return story();
     },
   ],
-} as Meta;
+  args: {
+    activeTab: 0,
+    autoFocus: false,
+  },
+  argTypes: {
+    activeTab: {
+      control: {
+        type: 'select',
+      },
+      options: [0, 1, 2, 3],
+    },
+  },
+} as Meta<Components.InoTabBar>;
 
-export const Playground: Story<Components.InoTabBar> = (args) => html`
+const template = new TemplateGenerator<Components.InoTabBar>(
+  'ino-tab-bar',
+  args => html`
   <ino-tab-bar id="customizable-tabbar" active-tab="${args.activeTab}" auto-focus="${args.autoFocus}">
     <ino-tab label="User" icon="user"></ino-tab>
     <ino-tab label="Messages" icon="message"></ino-tab>
     <ino-tab label="Settings" icon="settings"></ino-tab>
     <ino-tab label="Download" icon="download"></ino-tab>
   </ino-tab-bar>
-`;
-Playground.args = {
-  activeTab: 0,
-  autoFocus: false,
-};
-Playground.argTypes = {
-  activeTab: {
-    control: {
-      type: 'select',
-    },
-    options: [0, 1, 2, 3],
-  },
-};
+`);
 
-export const Stacked = () => html`
-  <ino-tab-bar>
-    <ino-tab label="Label 1" icon="info" stacked></ino-tab>
-    <ino-tab label="Label 2" icon="info" stacked></ino-tab>
-    <ino-tab label="Label 3" icon="info" stacked></ino-tab>
-    <ino-tab label="Label 4" icon="info" stacked></ino-tab>
-  </ino-tab-bar>
-`;
-
-export const IndicatorContentWidth = () => html`
-  <ino-tab-bar>
-    <ino-tab label="Label 1" icon="info" indicator-content-width></ino-tab>
-    <ino-tab label="Label 2" icon="info" indicator-content-width></ino-tab>
-    <ino-tab label="Label 3" icon="info" indicator-content-width></ino-tab>
-    <ino-tab label="Label 4" icon="info" indicator-content-width></ino-tab>
-  </ino-tab-bar>
-`;
+export const Playground = template.generatePlaygroundStory();
+export const AutoFocus = template.generateStoryForProp('autoFocus', true);
+export const activeTab = template.generateStoryForProp('activeTab', 1);
