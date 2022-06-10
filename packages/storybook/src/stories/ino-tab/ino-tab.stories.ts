587389ae429292015d40388bb2a7f608c3065b3c
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-06-10 16:10:00 +0200

    refactorings

--- packages/storybook/src/stories/ino-tab/ino-tab.stories.ts
+++ packages/storybook/src/stories/ino-tab/ino-tab.stories.ts
@@ -1,5 +1,5 @@
 import { Components } from '@inovex.de/elements';
-import { Meta, Story } from '@storybook/web-components';
+import { Meta } from '@storybook/web-components';
 import { html } from 'lit-html';
 import { TemplateGenerator } from '../template-generator';
 import { decorateStoryWithClass, withIconControl } from '../utils';
@@ -10,38 +10,26 @@ export default {
   decorators: [(story) => decorateStoryWithClass(story)],
   args: {
     indicatorContentWidth: false,
-    defaultSlot: 'Customizable Tab',
-    label: '',
     stacked: false,
     icon: 'info',
   },
 } as Meta<Components.InoTab>;
 
-type InoTabExtended = Components.InoTab & {
-  defaultSlot: string,
-}
-
-const template = new TemplateGenerator<InoTabExtended>(
+const template = new TemplateGenerator<Components.InoTab>(
   'ino-tab',
   args => html`
   <ino-tab
     icon="${args.icon}"
     indicator-content-width="${args.indicatorContentWidth}"
-    label="${args.label}"
     stacked="${args.stacked}"
   >
-    ${args.defaultSlot}
+    Label
   </ino-tab>
 `);
 
 export const Playground = template.generatePlaygroundStory();
 withIconControl(Playground, 'icon', 'info');
 
-export const Icon = template.generateStoryForProp('icon', 'info');
-export const Label = template.generateStoryForProp('label', '');
-Label.args = {
-  defaultSlot: 'Label',
-}
 export const Stacked = template.generateStoryForProp('stacked', true);
 export const IndicatorContentWidth = template.generateStoryForProp('indicatorContentWidth', true);
 
