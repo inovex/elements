2f7d8fdb42fab55402adc8913502046a4e043453
Jan-Niklas <jan-niklas.voss@inovex.de>
2022-06-10 16:05:55 +0200

    feat(elements|ino-range): support input of intervals (#605)
    Co-authored-by: MariaLStefan <MariaLStefan@users.noreply.github.com>
    Co-authored-by: TobiasHeimGalindo <TobiasHeimGalindo@users.noreply.github.com>
    Co-authored-by: Tobias Heim Galindo <TobiasHeimGalindo@users.noreply.github.com>

--- packages/storybook/src/stories/ino-range/ino-range.stories.ts
+++ packages/storybook/src/stories/ino-range/ino-range.stories.ts
@@ -4,6 +4,7 @@ import { html } from 'lit-html';
 import { decorateStoryWithClass, withColorScheme } from '../utils';
 import './ino-range.scss';
 import { useEffect } from '@storybook/client-api';
+import { TemplateGenerator } from '../template-generator';
 
 export default {
   title: 'Input/<ino-range>',
@@ -12,116 +13,75 @@ export default {
     (story) => decorateStoryWithClass(story, 'story-range'),
     (story) => {
       useEffect(() => {
-        const eventHandler = (e: CustomEvent<number>) =>
+        const eventHandlerSingle = (e: CustomEvent<number>) =>
           ((e.target as HTMLInoRangeElement).value = e.detail);
+        const eventHandlerStart = (e: CustomEvent<number>) =>
+          ((e.target as HTMLInoRangeElement).valueStart = e.detail);
+        const eventHandlerEnd = (e: CustomEvent<number>) =>
+          ((e.target as HTMLInoRangeElement).valueEnd = e.detail);
         const inoRanges = document.querySelectorAll('ino-range');
-        inoRanges.forEach((r) =>
-          r.addEventListener('valueChange', eventHandler)
-        );
+        inoRanges.forEach((r) => {
+          r.addEventListener('valueChange', eventHandlerSingle);
+          r.addEventListener('valueStartChange', eventHandlerStart);
+          r.addEventListener('valueEndChange', eventHandlerEnd);
+        });
         return () =>
-          inoRanges.forEach((r) =>
-            r.removeEventListener('valueChange', eventHandler)
-          );
+          inoRanges.forEach((r) => {
+            r.removeEventListener('valueChange', eventHandlerSingle);
+            r.removeEventListener('valueStartChange', eventHandlerStart);
+            r.removeEventListener('valueEndChange', eventHandlerEnd);
+          });
       });
       return story();
     },
   ],
-} as Meta;
+  args: {
+    disabled: false,
+    discrete: false,
+    min: 0,
+    max: 100,
+    value: 50,
+    valueStart: 30,
+    valueEnd: 70,
+    name: '',
+    markers: false,
+    step: 1,
+    ranged: false,
+  },
+} as Meta<Components.InoRange>;
 
-export const Playground: Story<Components.InoRange> = (args) => html`
-  <ino-range
-    class="customizable-range"
-    disabled="${args.disabled}"
-    min="${args.min}"
-    max="${args.max}"
-    name="${args.name}"
-    discrete="${args.discrete}"
-    markers="${args.markers}"
-    value="${args.value}"
-    step="${args.step}"
-    color-scheme="${args.colorScheme}"
-  >
-  </ino-range>
-`;
+// the basic template for the checkbox component
+const template = new TemplateGenerator<Components.InoRange>(
+  'ino-range',
+  (args) => html`
+    <ino-range
+      class="customizable-range"
+      disabled="${args.disabled}"
+      min="${args.min}"
+      max="${args.max}"
+      name="${args.name}"
+      discrete="${args.discrete}"
+      markers="${args.markers}"
+      value="${args.value}"
+      step="${args.step}"
+      ranged="${args.ranged}"
+      value-start="${args.valueStart}"
+      value-end="${args.valueEnd}"
+      color-scheme="${args.colorScheme}"
+    >
+    </ino-range>
+  `
+);
 
-Playground.args = {
-  disabled: false,
-  discrete: false,
-  min: 0,
-  max: 100,
-  name: '',
-  markers: false,
-  value: 50,
-  step: 1,
-};
-withColorScheme(Playground, 'colorScheme');
+export const Playground = template.generatePlaygroundStory();
 
-export const Colors = () => html`
-  <div class="flex-parent">
-    <div class="flex-child">
-      <h5>primary</h5>
-      <ino-range color-schem="primary" min="0" max="100" value="50"></ino-range>
-    </div>
-    <div class="flex-child">
-      <h5>secondary</h5>
-      <ino-range
-        color-scheme="secondary"
-        min="0"
-        max="100"
-        value="50"
-      ></ino-range>
-    </div>
-    <div class="flex-child">
-      <h5>success</h5>
-      <ino-range
-        color-scheme="success"
-        min="0"
-        max="100"
-        value="50"
-      ></ino-range>
-    </div>
-    <div class="flex-child">
-      <h5>warning</h5>
-      <ino-range
-        color-scheme="warning"
-        min="0"
-        max="100"
-        value="50"
-      ></ino-range>
-    </div>
-    <div class="flex-child">
-      <h5>error</h5>
-      <ino-range color-scheme="error" min="0" max="100" value="50"></ino-range>
-    </div>
-    <div class="flex-child">
-      <h5>light</h5>
-      <ino-range color-scheme="light" min="0" max="100" value="50"></ino-range>
-    </div>
-    <div class="flex-child">
-      <h5>dark</h5>
-      <ino-range color-scheme="dark" min="0" max="100" value="50"></ino-range>
-    </div>
-  </div>
-`;
+export const Discrete = template.generateStoryForProp('discrete', true);
 
-export const DiscretRange = () => html`
-  <ino-range
-    color-scheme="primary"
-    min="0"
-    max="100"
-    value="50"
-    discrete
-  ></ino-range>
-`;
+export const Markers = template.generateStoryForProp('markers', true, {
+  discrete: true,
+});
 
-export const SteppedRange = () => html`
-  <ino-range
-    color-scheme="primary"
-    min="0"
-    max="100"
-    value="50"
-    step="25"
-    discrete
-    markers
-  ></ino-range>
-`;
+export const Ranged = template.generateStoryForProp('ranged', true, {
+  valueStart: 30,
+  valueEnd: 70,
+});
