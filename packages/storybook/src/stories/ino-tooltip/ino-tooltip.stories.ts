a4ead829d81234a0895eb12c1ae20180908d9594
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-06-10 15:20:27 +0200

    refactorings

--- packages/storybook/src/stories/ino-tooltip/ino-tooltip.stories.ts
+++ packages/storybook/src/stories/ino-tooltip/ino-tooltip.stories.ts
@@ -1,7 +1,7 @@
 import { Components } from '@inovex.de/elements';
 import { Meta } from '@storybook/web-components';
 import { html } from 'lit-html';
-import { decorateStoryWithClass, withColorScheme } from '../utils';
+import { decorateStoryWithClass } from '../utils';
 import { TemplateGenerator } from '../template-generator';
 import './ino-tooltip.scss';
 
@@ -9,66 +9,33 @@ export default {
   title: `Notification/ino-tooltip`,
   component: 'ino-tooltip',
   decorators: [(story) => decorateStoryWithClass(story, 'story-tooltip')],
-  argTypes: {
-    placement: {
-      control: {
-        type: 'select',
-      },
-      options: ['top', 'right', 'bottom', 'left'],
-    },
-    trigger: {
-      control: {
-        type: 'select',
-      },
-      options: [
-        'mouseenter',
-        'focus',
-        'click',
-        'mouseenter focus',
-        'mouseenter click',
-        'focus click',
-        'mouseenter focus click',
-      ],
-    },
-  },
   args: {
     for: 'tooltip-target',
-    defaultSlot: 'This is a tooltip text.',
-    label: '',
-    placement: 'bottom',
+    placement: 'auto',
     trigger: 'mouseenter focus',
     colorScheme: 'primary',
-    buttonLabel: 'Tooltip',
   },
 } as Meta;
 
-type InoTooltipExtended = Components.InoTooltip & {
-  defaultSlot: string,
-  buttonLabel: string,
-}
-
-const template = new TemplateGenerator<InoTooltipExtended>(
+const template = new TemplateGenerator<Components.InoTooltip>(
   'ino-tooltip',
   args => html`
-  <ino-button id="${args.for}">${args.buttonLabel}</ino-button>
+  <ino-button id="${args.for}">Trigger Element</ino-button>
   <ino-tooltip
     for="${args.for}"
-    label="${args.label}"
     placement="${args.placement}"
     trigger="${args.trigger}"
     color-scheme="${args.colorScheme}"
-  >${args.defaultSlot}</ino-tooltip>
+  >This is a tooltip</ino-tooltip>
 `);
 
 export const Playground = template.generatePlaygroundStory();
-withColorScheme(Playground, 'colorScheme', 'transparent');
 
 export const Placement = template.generateStoryForProp('placement', 'right', {
   for: 'tooltip-target-placement',
 });
 export const Trigger = template.generateStoryForProp('trigger', 'click', {
   for: 'tooltip-target-trigger',
-  buttonLabel: 'Click',
 });
 export const ColorScheme = template.generateStoryForProp('colorScheme', 'secondary', {
   for: 'tooltip-target-color-scheme',
