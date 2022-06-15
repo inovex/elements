3eec9800be0556de2281cdd3907d698b7d064c7f
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-06-15 16:54:04 +0200

    refactorings

--- packages/storybook/src/stories/ino-segment-group/ino-segment-group.stories.ts
+++ packages/storybook/src/stories/ino-segment-group/ino-segment-group.stories.ts
@@ -1,34 +1,14 @@
 import { Components } from '@inovex.de/elements';
-import { useEffect } from '@storybook/client-api';
-import { Meta, Story } from '@storybook/web-components';
+import { Meta } from '@storybook/web-components';
 import { html } from 'lit-html';
 import { TemplateGenerator } from '../template-generator';
 import { decorateStoryWithClass } from '../utils';
 import './ino-segment-group.scss';
 
-const eventHandler = (e: Event) => {
-  const el = e.target as HTMLElement;
-  if (el.tagName.toLowerCase() !== 'ino-segment-button') {
-    return;
-  }
-  const group = el.closest('ino-segment-group');
-  group.setAttribute('value', el.getAttribute('value'));
-};
-
 export default {
   title: 'Buttons/ino-segment-group',
   component: 'ino-segment-group',
-  decorators: [
-    (story) => decorateStoryWithClass(story, 'story-segment-group'),
-    (story) => {
-      useEffect(() => {
-        document.addEventListener('checkedChange', eventHandler);
-        return () =>
-          document.removeEventListener('checkedChange', eventHandler);
-      });
-      return story();
-    },
-  ],
+  decorators: [(story) => decorateStoryWithClass(story, 'story-segment-group')],
   parameters: {
     actions: {
       handles: ['checkedChange'],
@@ -45,42 +25,29 @@ export default {
   args: {
     name: '',
     value: 'opt-2',
-    dense: false,
-  }
+  },
 } as Meta<Components.InoSegmentGroup>;
 
-type InoSegmentGroupExtended = Components.InoSegmentGroup & {
-  dense: boolean;
-}
+const checkedChangeHandler = (e: CustomEvent<boolean>) => {
+  const pressedButton = e.target as HTMLInoSegmentButtonElement;
+  const segmentGroup = pressedButton.closest('ino-segment-group');
+  segmentGroup.value = pressedButton.value;
+};
 
-const template = new TemplateGenerator<InoSegmentGroupExtended>(
+const template = new TemplateGenerator<Components.InoSegmentGroup>(
   'ino-segment-group',
-  args => html`
-  <ino-segment-group id="segment-grp" name="${args.name}" value="${args.value}">
-    <ino-segment-button dense="${(args.dense)}" value="opt-1">Option 1</ino-segment-button>
-    <ino-segment-button dense="${(args.dense)}" value="opt-2">Option 2</ino-segment-button>
-    <ino-segment-button dense="${(args.dense)}" value="opt-3">Option 3</ino-segment-button>
-    <ino-segment-button dense="${(args.dense)}" value="opt-4">Option 4</ino-segment-button>
-  </ino-segment-group>
-`);
+  (args) => html`
+    <ino-segment-group
+      id="segment-grp"
+      name="${args.name}"
+      value="${args.value}"
+    >
+      <ino-segment-button value="opt-1" @checkedChange="${checkedChangeHandler}">Option 1</ino-segment-button>
+      <ino-segment-button value="opt-2" @checkedChange="${checkedChangeHandler}">Option 2</ino-segment-button>
+      <ino-segment-button value="opt-3" @checkedChange="${checkedChangeHandler}">Option 3</ino-segment-button>
+      <ino-segment-button value="opt-4" @checkedChange="${checkedChangeHandler}">Option 4</ino-segment-button>
+    </ino-segment-group>
+  `
+);
+
 export const Playground = template.generatePlaygroundStory();
-
-export const DenseGroupWithCheckedOption = template.generateStoryForProp('value', 'opt-3');
-DenseGroupWithCheckedOption.args ={
-  dense: true,
-  value: 'opt-3',
-}
-
-export const GroupWithNoCheckedOption = template.generatePlaygroundStory();
-GroupWithNoCheckedOption.args = {
-  value: '',
-}
-
-const templateSingleButtonGroup = new TemplateGenerator<Components.InoSegmentGroup>(
-  'ino-segment-group',
-  args => html`
-  <ino-segment-group id="single-segment-grp">
-    <ino-segment-button value="1">Option 1</ino-segment-button>
-  </ino-segment-group>
-`);
-export const SingleButtonGroup = templateSingleButtonGroup.generatePlaygroundStory();
