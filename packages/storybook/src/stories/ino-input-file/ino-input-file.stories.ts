8bf544dcfe74b3d1d8223c72ba064b1612bd447b
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2022-07-06 11:46:32 +0200

    refactor(storybook|ino-input-file): reformat stories (#589)
    Co-authored-by: Jan-Niklas Voß <jan-niklas.voss@inovex.de>

--- packages/storybook/src/stories/ino-input-file/ino-input-file.stories.ts
+++ packages/storybook/src/stories/ino-input-file/ino-input-file.stories.ts
@@ -1,14 +1,16 @@
 import { useEffect } from '@storybook/client-api';
 import { Meta } from '@storybook/web-components';
-import { html, TemplateResult } from 'lit-html';
+import { html } from 'lit-html';
+import { TemplateGenerator } from '../template-generator';
+import { Components } from '@inovex.de/elements';
 
 export default {
   title: 'Input/ino-input-file',
   component: 'ino-input-file',
   decorators: [
-    (story) => {
+    story => {
       useEffect(() => {
-        const eventHandler = function (e) {
+        const eventHandler = function(e) {
           e.stopImmediatePropagation();
           const el = e.target;
           if (el.tagName.toLowerCase() !== 'ino-input-file') {
@@ -16,7 +18,7 @@ export default {
           }
 
           const fileNames = e.detail.files
-            .map((f) => [f.name, f.type, f.size + ' bytes'].join(', '))
+            .map(f => [f.name, f.type, f.size + ' bytes'].join(', '))
             .join('\n');
           alert(fileNames);
         };
@@ -32,37 +34,34 @@ export default {
       handles: ['changeFile .customizable-input'],
     },
   },
-} as Meta;
+  args: {
+    accept: '',
+    autoFocus: false,
+    disabled: false,
+    multiple: false,
+    required: false,
+    label: 'Select a file to upload',
+    dragAndDrop: false,
+    dragAndDropText: 'Drag your files here',
+    dragAndDropSecondaryText: 'or',
+  }
+} as Meta<Components.InoInputFile>;
 
-export const Playground = (args): TemplateResult => html`
+
+const template = new TemplateGenerator<Components.InoInputFile>(
+  'ino-input-file',
+  args => html`
   <ino-input-file
-    class="customizable-input"
-    accept="${args.accept}"
-    autofocus="${args.autofocus}"
-    disabled="${args.disabled}"
-    multiple="${args.multiple}"
-    required="${args.required}"
-    label="${args.label}"
-    label-selected="${args.labelSelected}"
-    drag-and-drop="${args.dragAndDrop}"
-    drag-and-drop-text="${args.dragAndDropText}"
-    drag-and-drop-secondary-text="${args.dragAndDropSecondaryText}"
-  >
-  </ino-input-file>
-`;
-Playground.args = {
-  accept: '',
-  autoFocus: false,
-  disabled: false,
-  multiple: false,
-  required: false,
-  label: 'Select a file to upload',
-  labelSelected: 'selected',
-  dragAndDrop: false,
-  dragAndDropText: 'Drag your files here',
-  dragAndDropSecondaryText: 'or',
-};
-
-export const DragAndDrop = (): TemplateResult => html`
-  <ino-input-file multiple drag-and-drop></ino-input-file>
-`;
+  class="customizable-input"
+  accept="${args.accept}"
+  autoFocus="${args.autoFocus}"
+  disabled="${args.disabled}"
+  multiple="${args.multiple}"
+  required="${args.required}"
+  label="${args.label}"
+  drag-and-drop="${args.dragAndDrop}"
+  drag-and-drop-text="${args.dragAndDropText}"
+  drag-and-drop-secondary-text="${args.dragAndDropSecondaryText}"></ino-input-file>
+`);
+export const Playground = template.generatePlaygroundStory();
+export const DragAndDrop = template.generateStoryForProp('dragAndDrop', true);
