e6da3cba35dd1c7a3e16d921a1d17c1a310a8455
MariaLStefan <MariaLStefan@users.noreply.github.com>
2022-07-14 08:15:46 +0000

    Deploying elements-react-example

---
+++ example-projects/react/assets/ino-carousel-slide.entry.89aa922c.js
@@ -0,0 +1,3 @@
+import{r as i,h as e,H as o,g as t}from"./index.8a29c9ac.js";/*!
+ * Crafted with ❤ by inovex GmbH
+ */const l="ino-carousel-slide{display:block;position:absolute;width:100%;height:100%;opacity:0}ino-carousel-slide>div{background-size:cover;background-position:50%;width:100%;height:100%}ino-carousel-slide.ino-carousel-slide--selected{opacity:1}";let r=class{constructor(s){i(this,s)}render(){return e(o,null,e("div",{style:{"background-image":`url(${this.src})`}}))}get el(){return t(this)}};r.style=l;export{r as ino_carousel_slide};
