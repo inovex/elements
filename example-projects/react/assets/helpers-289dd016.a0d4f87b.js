02b2b07a57a75a8a3baeb97a87b56f2b76ed2593
MariaLStefan <MariaLStefan@users.noreply.github.com>
2022-07-14 14:34:03 +0000

    Deploying elements-angular-example

--- example-projects/react/assets/helpers-289dd016.a0d4f87b.js
+++
@@ -1,3 +0,0 @@
-/*!
- * Crafted with ❤ by inovex GmbH
- */const p=(t,n,u,d)=>{let e=t.querySelector("input.aux-input");return e||(e=t.ownerDocument.createElement("input"),e.type="hidden",e.classList.add("aux-input"),t.appendChild(e)),e.disabled=d,e.name=n,e.value=u||"",e};export{p as r};
