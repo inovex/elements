bcb5a77f80226c5ed55c90e66021ba4aad80e049
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2022-07-14 09:22:30 +0200

    delete example-project react folder

--- example-projects/react/assets/_commonjsHelpers-6ccee0aa.3580eb33.js
+++
@@ -1,3 +0,0 @@
-/*!
- * Crafted with ❤ by inovex GmbH
- */var s=typeof globalThis!="undefined"?globalThis:typeof window!="undefined"?window:typeof global!="undefined"?global:typeof self!="undefined"?self:{};function a(o,n,e){return e={path:n,exports:{},require:function(t,i){return r()}},o(e,e.exports),e.exports}function r(){throw new Error("Dynamic requires are not currently supported by @rollup/plugin-commonjs")}export{s as a,a as c};
