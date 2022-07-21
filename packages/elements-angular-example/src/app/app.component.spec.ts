8f837329a2d419b3651035a1cdc2b47963ee746c
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2022-07-21 10:19:23 +0200

    add missing module in test

--- packages/elements-angular-example/src/app/app.component.spec.ts
+++ packages/elements-angular-example/src/app/app.component.spec.ts
@@ -1,22 +1,24 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {TodoService} from "./shared/services/todo.service";
-import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import { TodoService } from "./shared/services/todo.service";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
       providers: [TodoService],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
-  }));
+  });
 
-  it('should create the app', async(() => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
+  });
 });
