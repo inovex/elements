2fe12bc95e1e6781f71a80733b65fb6bc96d5bfc
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2022-07-21 10:26:11 +0200

    ci: change base path for react example

--- packages/elements-react-example/vite.config.ts
+++ packages/elements-react-example/vite.config.ts
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  mode: "development",
   server: {
     open: '/'
   },
-  base: "./example-projects/react/",
+  base: ".elements.inovex.de/example-projects/react/",
   optimizeDeps: {
     include: ['@inovex.de/elements-react']
   },
@@ -16,6 +15,5 @@ export default defineConfig({
     commonjsOptions: {
       include: [/@inovex.de\/elements-react\//, /node_modules\//]
     },
-    minify: false,
   },
 })
