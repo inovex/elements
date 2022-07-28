0fd547b454b19473e7d2a2ec9b54c6a4d80d37a8
Tobias Heim Galindo <tobisellfy@gmail.com>
2022-07-28 12:00:04 +0200

    feat: finalize react starter guide

--- packages/elements-react-example/src/login-form-app/index.tsx
+++ packages/elements-react-example/src/login-form-app/index.tsx
@@ -4,12 +4,10 @@ import {
   InoButton,
   InoIcon,
   InoImg,
-  InoFab,
-  InoFabSet,
-  InoHeader,
   InoInput,
   InoSnackbar,
   InoPopover,
+  InoTab,
 } from '../shared/InovexElements';
 import './Guide.scss';
 import logo from '../../../landingpage/src/assets/logos/elements.svg';
@@ -18,6 +16,7 @@ export const LoginFormApp: React.FC = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [showSnackbar, setSnackbar] = React.useState(false);
+  const [hidePassword, setVisibility] = React.useState('password');
 
   const onValueChanged = (value: CustomEvent<string>, input: string) => {
     switch (input) {
@@ -30,11 +29,47 @@ export const LoginFormApp: React.FC = () => {
     }
   };
 
-  const resetData = () => {
-    setEmail('');
-    setPassword('');
+  const togglePasswordVisibility = () => {
+    if (hidePassword === 'password') {
+      setVisibility('text');
+    } else {
+      setVisibility('password');
+    }
   };
 
+  const submit = () => {
+    setSnackbar(true);
+
+    setTimeout(() => {
+      setSnackbar(false);
+    }, 5000);
+  };
+
+  const ValidateLogin = () => {
+    if (email.length === 0 || password.length < 8) {
+      return (
+        <InoSnackbar
+          action-text="email and/or password too short"
+          timeout={5000}
+          type="error"
+          stay-visible-on-hover="false"
+        >
+          Login failed
+        </InoSnackbar>
+      );
+    } else {
+      return (
+        <InoSnackbar
+          action-text="Welcome back!"
+          timeout={5000}
+          type="success"
+          stay-visible-on-hover="false"
+        >
+          Sucessfully logged in
+        </InoSnackbar>
+      );
+    }
+  };
   return (
     <div className="container">
       <div className="leftpage">
@@ -51,77 +86,57 @@ export const LoginFormApp: React.FC = () => {
         <div className="body">
           <h1>Login to your Account</h1>
           <p>Login using social networks</p>
-          <div>
-            <InoFabSet
-              open-dial="false"
-              dial-direction="right"
-              topBottomLocation="top"
-              leftRightLocation="left"
-            >
-              <InoFab label="First FAB">
-                <InoIcon slot="icon-leading" icon="star"></InoIcon>
-              </InoFab>
-              <InoFab label="Second FAB">
-                <InoIcon slot="icon-leading" icon="favorite"></InoIcon>
-              </InoFab>
-              <InoFab label="Third FAB">
-                <InoIcon slot="icon-leading" icon="info"></InoIcon>
-              </InoFab>
-            </InoFabSet>
+          <div className="login-alternatives">
+            <InoTab icon="mail" indicator-content-width="false" stacked>
+              Gmail
+            </InoTab>
+            <InoTab icon="mail" indicator-content-width="false" stacked>
+              Yahoo
+            </InoTab>
           </div>
-          <InoHeader>or</InoHeader>
-          <InoInput
-            type="text"
-            label="email"
-            outline={true}
-            value={email}
-            onValueChange={(value) => onValueChanged(value, 'email')}
-          ></InoInput>
-          <InoInput
-            type="text"
-            icon-trailing=""
-            label="password"
-            outline={true}
-            value={password}
-            onValueChange={(value) => onValueChanged(value, 'password')}
-          >
-            <InoIcon
-              slot="icon-trailing"
-              icon="password"
-              clickable={true}
-            ></InoIcon>{' '}
-            {/* TODO after ino-icon fix: clickEl reinpacken damit man das passwort verstecken kann oder nicht*/}
-          </InoInput>
+
+          <p>or</p>
+
+          <div className="login-inputs">
+            <InoInput
+              type="text"
+              label="email"
+              outline
+              value={email}
+              onValueChange={(value) => onValueChanged(value, 'email')}
+            ></InoInput>
+            <InoInput
+              type={hidePassword}
+              label="password"
+              outline
+              value={password}
+              onValueChange={(value) => onValueChanged(value, 'password')}
+            >
+              <InoIcon
+                slot="icon-trailing"
+                icon="password"
+                clickable
+                onClick={() => togglePasswordVisibility()}
+              ></InoIcon>
+            </InoInput>
+          </div>
           <InoButton
             type="submit"
             onClick={() => {
-              setSnackbar(!showSnackbar);
-              resetData();
+              submit();
             }}
           >
             Sign In
           </InoButton>
-          {showSnackbar ? (
-            <InoSnackbar
-              action-text="Welcome back!"
-              timeout={5000}
-              type="success"
-              stay-visible-on-hover="false"
-            >
-              Sucessfully logged in
-            </InoSnackbar>
-          ) : null}
         </div>
+        {showSnackbar ? <ValidateLogin /> : null}
       </div>
       <div className="rightpage">
         <h1>New Here?</h1>
         <p>Sign up here and discover the power of the Elements</p>
         <InoButton id="register">Sign Up</InoButton>
         <InoPopover for="register" placement="bottom">
-          <div className="styled-popover">
-            <InoIcon icon="user"></InoIcon>
-            to be continued.
-          </div>
+          <div className="styled-popover">to be continued.</div>
         </InoPopover>
       </div>
     </div>
