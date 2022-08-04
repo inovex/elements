31d6feb6372a51970becbb13865e4387241520ec
janivo <janivo@users.noreply.github.com>
2022-08-04 12:31:01 +0000

    Deploying elements-angular-example

--- example-projects/vue/js/8216.f4ceba73.js
+++
@@ -1,5 +0,0 @@
-"use strict";(self["webpackChunkelements_vue_example"]=self["webpackChunkelements_vue_example"]||[]).push([[8216],{8216:function(e,s,i){i.r(s),i.d(s,{ino_carousel_slide:function(){return o}});var t=i(7318);
-/*!
- * Crafted with ❤ by inovex GmbH
- */const l="ino-carousel-slide{display:block;position:absolute;width:100%;height:100%;opacity:0}ino-carousel-slide>div{background-size:cover;background-position:50%;width:100%;height:100%}ino-carousel-slide.ino-carousel-slide--selected{opacity:1}";let o=class{constructor(e){(0,t.r)(this,e)}render(){return(0,t.h)(t.H,null,(0,t.h)("div",{style:{"background-image":`url(${this.src})`}}))}get el(){return(0,t.g)(this)}};o.style=l}}]);
-//# sourceMappingURL=8216.f4ceba73.js.map
