88ce9bb4a864903386b7d00067babed068a57582
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-08-18 10:31:42 +0200

    test: wait for hydration before taking screenshot

--- packages/storybook/config/test-runner.js
+++ packages/storybook/config/test-runner.js
@@ -7,6 +7,18 @@ module.exports = {
     expect.extend({ toMatchImageSnapshot });
   },
   async postRender(page, context) {
+
+    await page.waitForFunction(
+      () => {
+        const inoElements = Array.from(
+          document.getElementsByTagName('*')
+        ).filter((el) => el.tagName.startsWith('INO'));
+
+        return inoElements.every((el) => el.classList.contains('hydrated'));
+      },
+      { polling: 50 }
+    );
+
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot({
       customSnapshotsDir,
