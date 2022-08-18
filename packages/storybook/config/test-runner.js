ee2550fd73f8d3abb2acba1ae0fa9dc2e193751b
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-08-18 12:36:06 +0200

    test: add timeout

--- packages/storybook/config/test-runner.js
+++ packages/storybook/config/test-runner.js
@@ -1,21 +1,24 @@
 module.exports = {
   async postRender(page, context) {
 
-    /* see: https://github.com/storybookjs/test-runner/issues/68#issuecomment-1214989696
-    await page.waitForFunction(
-      () => {
-        const inoElements = Array.from(
-          document.getElementsByTagName('*')
-        ).filter((el) => el.tagName.startsWith('INO'));
+    // see: https://github.com/storybookjs/test-runner/issues/68#issuecomment-1214989696
+    await Promise.all([
+      page.waitForFunction(
+        () => {
+          const inoElements = Array.from(
+            document.getElementsByTagName('*')
+          ).filter((el) => el.tagName.startsWith('INO'));
 
-        return inoElements.every((el) => el.classList.contains('hydrated'));
-      },
-      { polling: 50 }
-    );
+          return inoElements.every((el) => el.classList.contains('hydrated'));
+        },
+        { polling: 50 }
+      ),
+      page.waitForLoadState('load'),
+      page.waitForLoadState('domcontentloaded')
+    ]);
 
-     */
-
-    await page.$('#root');
+     await page.waitForTimeout(300);
+    jest.useFakeTimers();
 
     const elementHandler = await page.$('#root');
     const innerHTML = await elementHandler.innerHTML();
