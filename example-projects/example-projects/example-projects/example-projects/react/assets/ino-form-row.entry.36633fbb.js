e5b72de0b930a0a09d384d3e7ae6cac262355490
janivo <janivo@users.noreply.github.com>
2022-09-02 13:54:26 +0000

    Deploying example projects

--- example-projects/example-projects/example-projects/example-projects/react/assets/ino-form-row.entry.36633fbb.js
+++
@@ -1,3 +0,0 @@
-import{r as n,h as o,H as s}from"./index.c4fd3317.js";/*!
- * Crafted with ❤ by inovex GmbH
- */const i="ino-form-row{box-sizing:border-box}ino-form-row>.ino-form-row__composer{display:flex;align-items:center;box-sizing:border-box;width:100%;padding:2px}ino-form-row .ino-form-row__element{flex:1}ino-form-row .ino-form-row__label{width:120px}",e=class{constructor(r){n(this,r)}render(){return o(s,null,o("div",{class:"ino-form-row__composer"},o("div",{class:"ino-form-row__label"},this.label,this.mandatory&&o("span",null,"*")),o("div",{class:"ino-form-row__element"},o("slot",null))))}};e.style=i;export{e as ino_form_row};
