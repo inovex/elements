ca2a23c56d17b6bbdbe6bfb0af4219bbe0d4aed5
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-09-06 14:18:45 +0200

    refactor: add hints

--- packages/landingpage/src/workshop/components.js
+++ packages/landingpage/src/workshop/components.js
@@ -1,5 +1,9 @@
-import MyTest from "@/workshop/MyComponent.vue";
+import MyTest from "@/workshop/MyComponent.vue"; // <-- the .vue is important!
 
+/**
+ *  add your components/ui-pattern below, give it a label and some unique id
+ *  your component will be available under .../workshop/my-unique-id
+ */
 export const components = [
   {
     label: 'Meine Komponente',
