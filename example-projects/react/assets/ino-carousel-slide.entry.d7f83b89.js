1c054f5670d90a0ac0f975124165e7e8ba87874c
janivo <janivo@users.noreply.github.com>
2022-10-04 12:59:29 +0000

    Deploying example projects

---
+++ example-projects/react/assets/ino-carousel-slide.entry.d7f83b89.js
@@ -0,0 +1,3 @@
+import{r as o,h as e,H as i,g as t}from"./index.b8abd91e.js";/*!
+ * Crafted with ❤ by inovex GmbH
+ */const r="ino-carousel-slide{display:block;position:absolute;width:100%;height:100%;opacity:0}ino-carousel-slide>div{background-size:cover;background-position:50%;width:100%;height:100%}ino-carousel-slide.ino-carousel-slide--selected{opacity:1}",l=class{constructor(s){o(this,s)}render(){return e(i,null,e("div",{style:{"background-image":`url(${this.src})`}}))}get el(){return t(this)}};l.style=r;export{l as ino_carousel_slide};
