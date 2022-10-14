25b0bc8e4a884cbf910f34e858b50e7545253c14
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-10-14 16:38:44 +0200

    feat: add footer

--- packages/landingpage-new/components/layout/navbar.tsx
+++ packages/landingpage-new/components/layout/navbar.tsx
@@ -1,31 +1,20 @@
 import { useRouter } from 'next/router';
-import classNames from 'classnames';
 import styles from './navbar.module.scss';
-
-enum Routes {
-  HOME = '/',
-  EXPLORE = '/explore',
-  DOCS = '/docs',
-  ABOUT = '/about',
-  CONTACT = '/contact',
-}
+import { Routes } from './routes';
+import LinkItem from './linkItem';
 
 export default function Navbar() {
   const router = useRouter();
 
   return (
     <nav className={styles.navbar}>
-      {Object.entries(Routes).map(([name, url]) => (
-        <a key={url} href={url}>
-          <p
-            className={classNames(
-              styles.link,
-              router.pathname === url && styles.linkActive
-            )}
-          >
-            {name.toLowerCase()}
-          </p>
-        </a>
+      {Routes.map(({ name, url }) => (
+        <LinkItem
+          key={url}
+          url={url}
+          name={name}
+          isActive={router.pathname === url}
+        />
       ))}
     </nav>
   );
