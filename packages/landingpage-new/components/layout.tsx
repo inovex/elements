90d3c67155e42fef91313f5e5c8a622412cca8dd
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-10-14 14:41:49 +0200

    feat(header): style header according to design

--- packages/landingpage-new/components/layout.tsx
+++ packages/landingpage-new/components/layout.tsx
@@ -1,21 +1,41 @@
 import { ReactNode } from 'react';
 import { useRouter } from 'next/router';
 import { InoTab, InoTabBar } from '@elements';
+import Image from 'next/image';
+import styles from './layout.module.css';
 
-export default function Layout({children}: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
+  const router = useRouter();
 
-    const router = useRouter()
-
-    return (
-        <div>
-            <header>
-                <InoTabBar activeTab={router.pathname === '/' ? 0 : router.pathname === '/explore' ? 1 : 2}>
-                    <InoTab label={"Home"} onInteracted={() => router.push('/')}></InoTab>
-                    <InoTab label={"Explore"} onInteracted={() => router.push('explore')}></InoTab>
-                    <InoTab label={"Storybook"} onInteracted={() => router.push('storybook')}></InoTab>
-                </InoTabBar>
-            </header>
-            <main>{children}</main>
+  return (
+    <div className={styles.appContainer}>
+      <header className={styles.container}>
+        <div className={styles.logoContainer}>
+          <Image
+            src="/elements.svg"
+            alt="inovex-elements Logo"
+            width={48}
+            height={55}
+          />
+          <p>elements</p>
         </div>
-    );
+        <InoTabBar
+          activeTab={
+            router.pathname === '/' ? 0 : router.pathname === '/explore' ? 1 : 2
+          }
+        >
+          <InoTab label={'Home'} onInteracted={() => router.push('/')}></InoTab>
+          <InoTab
+            label={'Explore'}
+            onInteracted={() => router.push('explore')}
+          ></InoTab>
+          <InoTab
+            label={'Storybook'}
+            onInteracted={() => router.push('storybook')}
+          ></InoTab>
+        </InoTabBar>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
 }
