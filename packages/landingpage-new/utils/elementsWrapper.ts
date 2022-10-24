4687b029354ce9bbc007ccbdb13a99031aa322fd
Jan-Niklas <jan-niklas.voss@inovex.de>
2022-10-24 17:38:03 +0200

    chore: create basic setup for new landingpage (#765)
    * feat(landingpage-new): add basic landingpage
    
    * fix(landingpage-new): prevent mdx-js deps from being hoisted
    
    * feat(landingpage-new): take precautions so that the elements are imported correctly
    
    * feat(header): style header according to design
    
    * chore: install sass
    
    * feat: add header component
    
    * feat: add navbar
    
    * style: adjust header margins
    
    * chore: add sharp to optimize images
    
    * style: limit body width
    
    * feat: add footer
    
    * chore: remove package-lock
    
    * chore(storybook): add mdx-js to dependencies
    
    * chore(storybook): use lower eslint version explicitly
    
    * refactor: create sub folder for each main route
    
    * chore: add single quotation marks rule
    
    * style: update storybook iframe dimensions
    
    * refactor: indentations
    
    * feat: add link to main logo
    
    * feat: add shrinking header
    
    * fix: build errors

---
+++ packages/landingpage-new/utils/elementsWrapper.ts
@@ -0,0 +1,14 @@
+import dynamic from "next/dynamic";
+// eslint-disable-next-line no-restricted-imports
+import Components from "@inovex.de/elements-react/dist";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const importElement = <Component extends keyof typeof Components>(element: Component): typeof Components[Component]  => dynamic(import("@inovex.de/elements-react").then(m => m[element]), {
+  ssr: false,
+});
+
+export const InoButton = importElement('InoButton');
+export const InoChip = importElement('InoChip');
+export const InoTab = importElement('InoTab');
+export const InoTabBar = importElement('InoTabBar');
