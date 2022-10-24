05698c22b06b048a78f2cd971ff4a410ec1fabbe
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-10-24 16:11:55 +0200

    refactor: indentations

--- packages/landingpage-new/pages/_document.tsx
+++ packages/landingpage-new/pages/_document.tsx
@@ -1,13 +1,13 @@
-import {Head, Html, Main, NextScript} from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html>
-            <Head/>
-            <body>
-            <Main/>
-            <NextScript/>
-            </body>
-        </Html>
-    )
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
