d0386383e12cab3bff36de8d8ba751236d9e3128
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-10-24 17:16:48 +0200

    feat: add shrinking header

--- packages/landingpage-new/components/layout/header.tsx
+++ packages/landingpage-new/components/layout/header.tsx
@@ -1,10 +1,29 @@
 import Image from 'next/image';
 import styles from './header.module.scss';
 import Navbar from './navbar';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [yOffset, setYOffset] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+
+      const offset = window.pageYOffset;
+
+      if (offset !== 0 && Math.abs(yOffset - offset) < 5) return; // prevent jumping
+
+      setYOffset(offset);
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [yOffset]);
+
   return (
-    <header className={styles.header}>
+    <header
+      className={styles.header}
+      style={{ '--factor': yOffset } as React.CSSProperties}
+    >
       <div className={styles.logo}>
         <a
           target="_blank"
