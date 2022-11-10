4f4e8f2a411a7b14a84d4916f0cf1ed9598a7b25
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2022-11-10 17:54:13 +0100

    style: add simple animations

--- packages/landingpage-new/pages/home/header/install-chips.tsx
+++ packages/landingpage-new/pages/home/header/install-chips.tsx
@@ -1,6 +1,6 @@
-import { InoChip, InoPopover } from '@elements';
-import { useState } from 'react';
-import { useInterval, useCopyToClipboard, useTimeoutFn } from 'react-use';
+import { InoChip } from '@elements';
+import { useEffect, useState } from 'react';
+import { useCopyToClipboard, useInterval, useTimeoutFn } from 'react-use';
 import Image from 'next/image';
 import styles from './install-chips.module.scss';
 import classNames from 'classnames';
@@ -31,67 +31,90 @@ const IconByCommand: Record<Frameworks, string> = {
 const COMMAND_CHIP_ID = 'command-chip';
 
 export default function InstallChips() {
-  const [currentFramework, setCurrentFramework] = useState<Frameworks>(
+  const [
+    manuallySelectedFramework,
+    setManuallySelectedFramework,
+  ] = useState<Frameworks | null>(null);
+  const [carouselFramework, setCarouselFramework] = useState<Frameworks>(
     Frameworks.VUE
   );
-  const [clipboardState, copyToClipboard] = useCopyToClipboard();
+  const [currentFramework, setCurrentFramework] = useState<Frameworks>(
+    carouselFramework
+  );
+
+  const [, copyToClipboard] = useCopyToClipboard();
   const [showTooltip, setShowTooltip] = useState(false);
-  const [isReady, cancel, reset] = useTimeoutFn(
+  const [,, reset] = useTimeoutFn(
     () => setShowTooltip(false),
     3000
   );
 
-  useInterval(() => {
-    setCurrentFramework((currentCommand) => {
-      const currentIndex = FrameworksArr.indexOf(currentCommand);
-      const nextIndex = currentIndex + 1;
+  useInterval(
+    () => {
+      setCarouselFramework((currentCommand) => {
+        const currentIndex = FrameworksArr.indexOf(currentCommand);
+        const nextIndex = currentIndex + 1;
 
-      if (FrameworksArr.length <= nextIndex)
-        return FrameworksArr[0] as Frameworks;
+        if (FrameworksArr.length <= nextIndex)
+          return FrameworksArr[0] as Frameworks;
 
-      return FrameworksArr[nextIndex] as Frameworks;
-    });
-  }, 3000);
+        return FrameworksArr[nextIndex] as Frameworks;
+      });
+    },
+    manuallySelectedFramework ? null : 3000
+  );
+
+  useEffect(() => {
+    setCurrentFramework(manuallySelectedFramework ?? carouselFramework);
+  }, [manuallySelectedFramework, carouselFramework]);
 
   const currentCommand = CommandByFrameworks[currentFramework];
 
   return (
     <div className={styles.container}>
-        <div className={styles.chipContainer}>
-      <InoChip
-        id={COMMAND_CHIP_ID}
-        className={styles.chip}
-        fill="outline"
-        onClick={() => {
-          copyToClipboard(currentCommand);
-          setShowTooltip(true);
-          reset();
-        }}
-      >
-        <code>$ {currentCommand}</code>
-      </InoChip>
+      <div className={styles.chipContainer}>
+        <InoChip
+          id={COMMAND_CHIP_ID}
+          className={styles.chip}
+          fill="outline"
+          onClick={() => {
+            copyToClipboard(currentCommand);
+            setShowTooltip(true);
+            reset();
+          }}
+        >
+          <code className={styles.installCommand}>$ {currentCommand}</code>
+        </InoChip>
       </div>
-      {/* TODO ADD COPIED TO CLIPPBOARD NOTIFICATION <InoPopover attachToBody for={COMMAND_CHIP_ID} colorScheme="primary" placement="bottom" controlled visible={showTooltip} style={{backgroundColor: 'hotpink'}}>
-          Copied to clipboard!
-        </InoPopover> */}
-      {FrameworksArr.map((framework) => (
+      {FrameworksArr.map((framework: Frameworks) => (
         <div
-            key={framework}
-            className={classNames(
+          key={framework}
+          className={classNames(
             styles.icon,
             framework === currentFramework && styles.iconActive
           )}
         >
           <Image
-            src={`/${IconByCommand[framework as Frameworks]}`}
+            onClick={() => {
+              setManuallySelectedFramework(
+                currentFramework === framework ? null : framework
+              );
+              setCarouselFramework(framework);
+            }}
+            src={`/${IconByCommand[framework]}`}
             alt="Javascript Logo"
-            layout='fill'
-            width="100%"
-            height="100%"
+            layout="fill"
           />
-
         </div>
       ))}
+      <p
+        className={classNames(
+          styles.successMessage,
+          showTooltip && styles.successMessageShow
+        )}
+      >
+        Copied to your clipboard ✓
+      </p>
     </div>
   );
 }
