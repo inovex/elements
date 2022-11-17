0d74f930bd94be4d0bd2cc89aa9f5b1e3964869d
Jan-Niklas <jan-niklas.voss@inovex.de>
2022-11-17 11:00:10 +0100

    fix(landingpage): make icons available (#802)

--- packages/landingpage-new/next.config.js
+++ packages/landingpage-new/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const nextConfiguration = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+};
+
+const withImages = require('next-images');
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
@@ -8,8 +13,8 @@ const withMDX = require('@next/mdx')({
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
-})
-module.exports = withMDX({
-  // Append the default value with md extensions
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+});
+
+const plugins = [withImages, withMDX];
+
+module.exports = plugins.reduce((config, plugin) => plugin(config), nextConfiguration) // merge configs
--- packages/landingpage-new/pages/_app.tsx
+++ packages/landingpage-new/pages/_app.tsx
@@ -1,20 +1,24 @@
-import '../styles/globals.css'
-import type {AppProps} from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
-import {applyPolyfills, defineCustomElements,} from '@inovex.de/elements/dist/loader';
-import {useEffect} from 'react';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@inovex.de/elements/dist/loader';
+import { useEffect } from 'react';
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    void applyPolyfills().then(() => {
+      void defineCustomElements(window);
+    });
+  }, []);
 
-    useEffect(() => {
-        void applyPolyfills().then(() => {
-            void defineCustomElements(window);
-        });
-    }, [])
-
-    return <Layout>
-        <Component {...pageProps} />
-    </Layout>;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
