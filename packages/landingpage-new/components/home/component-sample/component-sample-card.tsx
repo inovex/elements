2126982d94383339ead9f2c10b1da799c24fc90c
Tobias Heim Galindo <tobisellfy@gmail.com>
2022-11-24 12:58:23 +0100

    refactor(landingpage-new): make card component universally usable

---
+++ packages/landingpage-new/components/home/component-sample/component-sample-card.tsx
@@ -0,0 +1,30 @@
+import { InoCard } from '@elements';
+import { FunctionComponent } from 'react';
+import styles from './component-sample-card.module.scss';
+import Card from '../../shared/card';
+
+interface Props {
+  children: JSX.Element;
+  componentName: string;
+  componentCategory: string | JSX.Element;
+}
+
+const ComponentSampleCard: FunctionComponent<Props> = ({
+  children,
+  componentName,
+  componentCategory,
+}) => (
+  <Card
+    cardClassName={styles.card}
+    content={children}
+    footerClassName={styles.footer}
+    footer={
+      <>
+        <div className={styles.component}>{componentName}</div>
+        <div className={styles.label}>{componentCategory}</div>
+      </>
+    }
+  />
+);
+
+export default ComponentSampleCard;
