4f53b3ad18293dd6d5cf1d2b778c66213be1e871
unknown <nschulz@inovex.de>
2022-12-08 14:36:13 +0100

    some style optimizations from magda

--- packages/landingpage-new/components/about/history.tsx
+++ packages/landingpage-new/components/about/history.tsx
@@ -10,37 +10,37 @@ function History() {
         <div className={styles.container}>
             <Milestone
                 title='Proof of concept 🔎'
-                subtitle='Feb 2018'
+                subtitle='Febuary 2018'
                 position='left'>
                 In 2018 we started our journey. We spent weeks evaluating which framework matches our needs and which decision may last for the next few years. It’s always a bet on time in front-end worlds and never easy to make the right pick. We started the elements as a side project and evaluated which library and tools would be the best choice for helping us writing standards based Web Components. We tried a lot and looked into the available libraries Polymer, SkateJS, SlimJS, Switzerland and Stencil. We opposed the features and decided to use Stencil
             </Milestone>
             <Milestone
                 title='Getting started 🛫'
-                subtitle='Jun 2018'
+                subtitle='June 2018'
                 position='right'>
                 With the help of packages like <a href='https://m3.material.io/develop/web' target='_blank'>Google Material Web</a>, <a href='https://flatpickr.js.org/' target='_blank'>flatpickr</a>, some other 3rd party dependencies and the product design from our designers, we create the core components we need to build our applications. With Storybook we are able to create the documentation and the playground for our consumers with minimal effort
             </Milestone>
             <Milestone
-                title='First internal consumers 🌻'
-                subtitle='Sep 2018'
+                title='First internal consumers 💙'
+                subtitle='September 2018'
                 position='left'>
                 Our motivation and enthusiasm brought quick success. Therefore it was possible that already in September one of our internal projects could use the elements and thereby gain an enormous added value
             </Milestone>
             <Milestone
-                title='Learning by doing 🤓'
-                subtitle='2019'
+                title='Learning by doing 💪🏻'
+                subtitle='in 2019'
                 position='right'>
                 During the journey we learned a lot about how we need to handle state, how we need to document the components as and provide a playground for the visual representation of the components. We understand what it means to create a library for a lot of people and being responsible for how to introduce breaking changes or how to handle bug fixes. It’s not easy, but it’s great to see the value we add to all the different projects
             </Milestone>
             <Milestone
-                title='First external consumers 🤝'
-                subtitle='2020'
+                title='First external consumers 🤝🏻'
+                subtitle='in 2020'
                 position='left'>
                 The elements become better known. Our customers were also convinced. This led to some of them also using elements in their own projects
             </Milestone>
             <Milestone
                 title='Open Source 🌍'
-                subtitle='Sep 2020'
+                subtitle='September 2020'
                 position='right'>
                 <mark>We’re public, we’re live!</mark> We are proud to announce our very first open source UI library for any framework, @inovex.de/elements, on <a href='https://www.npmjs.com/package/@inovex.de/elements' target='_blank'>npm</a> and <a href='https://github.com/inovex/elements' target='_blank'>GitHub</a> with a total of 46 core components
             </Milestone>
