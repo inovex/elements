9ee597d4d84c94f697e61c6f865473a0f4bee848
unknown <nschulz@inovex.de>
2022-12-08 10:41:26 +0100

    make history a timeline

--- packages/landingpage-new/components/about/milestone.tsx
+++ packages/landingpage-new/components/about/milestone.tsx
@@ -1,29 +1,22 @@
 import styles from './milestone.module.scss'
-import { InoIcon } from '@elements';
 
 type Props = {
-    icon: string;
     title: string;
     subtitle: string;
+    position: 'right' | 'left';
     children: any;
 }
 
 function Milestone(props: Props) {
     return(
-        <div className={styles.milestone}>
-            <div className={styles.milestoneHeader}>
-                <div className={styles.circle}>
-                    <InoIcon icon={props.icon} clickable={false} />
-                </div>
-                <div>
-                    <h2 className={styles.smallheader}>{props.title}</h2>
-                    <i>{props.subtitle}</i>
-                </div>
-            </div>
-            <div className={styles.textblock}>
-                {props.children} 
-            </div>
+    <div className={props.position === 'right' ? styles.milestoneRight : styles.milestoneLeft}>
+        <div className={styles.marker}></div>
+        <div className={styles.content}>
+            <h3>{props.title}</h3>
+            <span>{props.subtitle}</span>
+            <p>{props.children}</p>
         </div>
+    </div>
     );
 }
 
