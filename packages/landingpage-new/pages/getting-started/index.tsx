8879c13d9bdbd58c24d314bfef0719c153718d88
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-01-02 11:23:02 +0100

    feat: add page component to change title

--- packages/landingpage-new/pages/getting-started/index.tsx
+++ packages/landingpage-new/pages/getting-started/index.tsx
@@ -1,20 +1,24 @@
 import styles from './getting-started.module.scss';
 import ReactGuide from 'components/learn/react-guide.mdx';
+import Page from 'components/layout/page';
 
 const GettingStarted = () => {
   return (
-    <div className={styles.container}>
-      <ReactGuide/>
-      <h1>
-        Looking for more resources? Make sure to check out the example projects:
-      </h1>
-      <iframe
-        src="https://codesandbox.io/embed/github/inovex/elements-example-react/tree/master/?fontsize=14&hidenavigation=1&theme=dark"
-        className={styles.sandbox}
-        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-      ></iframe>
-    </div>
+    <Page title="Getting Started">
+      <div className={styles.container}>
+        <ReactGuide />
+        <h1>
+          Looking for more resources? Make sure to check out the example
+          projects:
+        </h1>
+        <iframe
+          src="https://codesandbox.io/embed/github/inovex/elements-example-react/tree/master/?fontsize=14&hidenavigation=1&theme=dark"
+          className={styles.sandbox}
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
+      </div>
+    </Page>
   );
 };
 
