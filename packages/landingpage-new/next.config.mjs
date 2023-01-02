5a0d002b8ceb13d85ef12ba6a064d9c686bd9420
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-01-02 11:01:15 +0100

    refactor: update config structure

--- packages/landingpage-new/next.config.mjs
+++ packages/landingpage-new/next.config.mjs
@@ -1,3 +1,11 @@
+import nextMdx from '@next/mdx';
+import withImages from 'next-images';
+import {remarkCodeHike} from '@code-hike/mdx';
+import remarkGfm from 'remark-gfm';
+import { readFile } from 'fs/promises';
+
+const theme = JSON.parse(await readFile('../../node_modules/shiki/themes/github-light.json'));
+
 const BASE_PATH = '/landingpage-new';
 
 /** @type {import('next').NextConfig} */
@@ -17,17 +25,11 @@ const nextConfiguration = {
   basePath: BASE_PATH, // TODO remove when old landingpage is replaced
 };
 
-const withImages = require('next-images');
-
-const { remarkCodeHike } = require('@code-hike/mdx');
-const remarkGfm = import('remark-gfm');
-const theme = require('shiki/themes/github-light.json');
-
-const withMDX = require('@next/mdx')({
+const withMDX = nextMdx({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [
-      [remarkCodeHike, { autoImport: false, theme, showCopyButton: true }],
+      [remarkCodeHike, {autoImport: false, theme, showCopyButton: true}],
       remarkGfm,
     ],
     rehypePlugins: [],
@@ -38,7 +40,7 @@ const withMDX = require('@next/mdx')({
 
 const plugins = [withImages, withMDX];
 
-module.exports = plugins.reduce(
+export default plugins.reduce(
   (config, plugin) => plugin(config),
   nextConfiguration
 ); // merge configs
