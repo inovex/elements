0e085cc01befa19dfae190876bc0e303679919f1
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-01-02 11:01:29 +0100

    refactor: finishing touches

--- packages/landingpage-new/pages/learn/index.tsx
+++ packages/landingpage-new/pages/learn/index.tsx
@@ -7,13 +7,17 @@ const Learn = () => {
     <>
       <div className={styles.container}>
         <ReactGuide />
+        <h1>
+          Looking for more resources? Make sure to check out the example
+          projects:
+        </h1>
+        <iframe
+          src="https://codesandbox.io/embed/github/inovex/elements-example-react/tree/master/?fontsize=14&hidenavigation=1&theme=dark"
+          className={styles.sandbox}
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
       </div>
-{/*      <iframe
-        src="https://codesandbox.io/embed/github/inovex/elements-example-react/tree/master/?fontsize=14&hidenavigation=1&theme=dark"
-        className={styles.sandbox}
-        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-      ></iframe>*/}
     </>
   );
 };
