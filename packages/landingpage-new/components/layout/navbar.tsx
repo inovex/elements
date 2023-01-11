653297759bc35ab35a751d18a02791653f3255e9
Nina Schulz <nschulz@inovex.de>
2023-01-11 16:50:05 +0100

    added popover submenu to header

--- packages/landingpage-new/components/layout/navbar.tsx
+++ packages/landingpage-new/components/layout/navbar.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import styles from './navbar.module.scss';
 import { MainRoutes, Routes, SubRoutes } from '../../utils/routes';
 import LinkItem from './linkItem';
-import { InoButton } from '@elements';
+import { InoButton, InoPopover } from '@elements';
 import Link from 'next/link';
 
 export default function Navbar() {
@@ -10,13 +10,40 @@ export default function Navbar() {
 
   return (
     <nav className={styles.navbar}>
-      {Routes.map(({ name, url }) => (
-        <LinkItem
-          key={url}
-          url={url}
-          name={name}
-          isActive={router.pathname === url}
-        />
+      {Routes.map(({ name: mainRouteName, url: mainRouteUrl, subRoutes }) => (
+        <div key={mainRouteUrl}>
+          <div id={mainRouteUrl}>
+            <LinkItem
+              url={mainRouteUrl}
+              name={mainRouteName}
+              isActive={router.pathname === mainRouteUrl}
+            />
+          </div>
+          <InoPopover
+            for={mainRouteUrl}
+            interactive
+            placement="bottom"
+            trigger="mouseenter"
+            controlled={false}
+            visible={false}
+            colorScheme="transparent"
+          >
+            <div className={styles.popover}>
+              {subRoutes.map(({ name: subRouteName, url: subRouteUrl }) => {
+                const subrouteDivider =
+                  mainRouteName === 'getting started' ? '/' : '#';
+                return (
+                  <LinkItem
+                    key={subRouteName + subRouteUrl}
+                    url={`${mainRouteUrl}${subrouteDivider}${subRouteUrl}`}
+                    name={subRouteName}
+                    isDense={true}
+                  />
+                );
+              })}
+            </div>
+          </InoPopover>
+        </div>
       ))}
       <Link href={MainRoutes.HOME + '#' + SubRoutes.HOME_CONTACT}>
         <InoButton>Contact us</InoButton>
