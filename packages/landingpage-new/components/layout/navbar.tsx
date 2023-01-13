a5c3eaae50a59b1cc2c0783005f4bb7c4c964c82
Nina Schulz <93990641+ninaschlz@users.noreply.github.com>
2023-01-13 09:22:24 +0100

    feat(landingpage): add sub navigation (#856)
    Co-authored-by: Jan-Niklas Voß <jan-niklas.voss@inovex.de>

--- packages/landingpage-new/components/layout/navbar.tsx
+++ packages/landingpage-new/components/layout/navbar.tsx
@@ -1,22 +1,57 @@
 import { useRouter } from 'next/router';
 import styles from './navbar.module.scss';
-import { MainRoutes, Routes, SubRoutes } from '../../utils/routes';
+import {getDividerByMainRoute, MainRoutes, Routes, SubRoutes} from '../../utils/routes';
 import LinkItem from './linkItem';
-import { InoButton } from '@elements';
+import { InoButton, InoPopover } from '@elements';
 import Link from 'next/link';
+
+const POPOVER_OFFSET = -46;
 
 export default function Navbar() {
   const router = useRouter();
 
   return (
     <nav className={styles.navbar}>
-      {Routes.map(({ name, url }) => (
-        <LinkItem
-          key={url}
-          url={url}
-          name={name}
-          isActive={router.pathname === url}
-        />
+      {Routes.map(({ name: mainRouteName, url: mainRouteUrl, subRoutes }) => (
+        <div key={mainRouteUrl}>
+          <div id={mainRouteUrl}>
+            <LinkItem
+              url={mainRouteUrl}
+              name={mainRouteName}
+              isActive={router.pathname === mainRouteUrl}
+            />
+          </div>
+          <InoPopover
+            for={mainRouteUrl}
+            interactive
+            placement="bottom"
+            trigger="mouseenter"
+            controlled={false}
+            visible={false}
+            distance={POPOVER_OFFSET}
+            colorScheme="transparent"
+          >
+            <div className={styles.popover}>
+              <LinkItem
+                url={mainRouteUrl}
+                name={mainRouteName}
+                isActive={router.pathname === mainRouteUrl}
+                noMargin={true}
+              />
+              {subRoutes.map(({ name: subRouteName, url: subRouteUrl }) => {
+                const subrouteDivider = getDividerByMainRoute(mainRouteUrl as MainRoutes);
+                return (
+                  <LinkItem
+                    key={subRouteName + subRouteUrl}
+                    url={`${mainRouteUrl}${subrouteDivider}${subRouteUrl}`}
+                    name={subRouteName}
+                    isDense={true}
+                  />
+                );
+              })}
+            </div>
+          </InoPopover>
+        </div>
       ))}
       <Link href={MainRoutes.HOME + '#' + SubRoutes.HOME_CONTACT}>
         <InoButton>Contact us</InoButton>
