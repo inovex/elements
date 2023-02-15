f90a5eec536d3a7eec85e6176d04fa1b104a30e2
janivo <janivo@users.noreply.github.com>
2023-02-15 09:02:31 +0000

    Deploying example projects

---
+++ example-projects/vue/js/8216.23ff5b29.js
@@ -0,0 +1,19 @@
+"use strict";(self["webpackChunkelements_vue_example"]=self["webpackChunkelements_vue_example"]||[]).push([[8216],{3662:function(e,n,r){r.d(n,{a:function(){return o},c:function(){return i},g:function(){return t}});
+/*!
+ * Crafted with ❤ by inovex GmbH
+ */
+var o="undefined"!==typeof globalThis?globalThis:"undefined"!==typeof window?window:"undefined"!==typeof r.g?r.g:"undefined"!==typeof self?self:{};function t(e){return e&&e.__esModule&&Object.prototype.hasOwnProperty.call(e,"default")?e["default"]:e}function i(e,n,r){return r={path:n,exports:{},require:function(e,n){return s()}},e(r,r.exports),r.exports}function s(){throw new Error("Dynamic requires are not currently supported by @rollup/plugin-commonjs")}},2183:function(e,n,r){r.d(n,{c:function(){return t}});var o=r(3662),t=(0,o.c)((function(e){
+/*!
+    Copyright (c) 2017 Jed Watson.
+    Licensed under the MIT License (MIT), see
+    http://jedwatson.github.io/classnames
+  */
+(function(){var n={}.hasOwnProperty;function r(){for(var e=[],o=0;o<arguments.length;o++){var t=arguments[o];if(t){var i=typeof t;if("string"===i||"number"===i)e.push(t);else if(Array.isArray(t)&&t.length){var s=r.apply(null,t);s&&e.push(s)}else if("object"===i)for(var u in t)n.call(t,u)&&t[u]&&e.push(u)}}return e.join(" ")}e.exports?(r.default=r,e.exports=r):window.classNames=r})()}));
+/*!
+ * Crafted with ❤ by inovex GmbH
+ */},8216:function(e,n,r){r.r(n),r.d(n,{ino_carousel_slide:function(){return s}});var o=r(1817),t=r(2183);r(3662);
+/*!
+ * Crafted with ❤ by inovex GmbH
+ */
+const i=".ino-carousel-slide{grid-row:1;grid-column:1;opacity:0;transition:opacity var(--carousel-animation-duration)}.ino-carousel-slide__image{background-size:cover;background-position:50%;width:100%;height:100%}.ino-carousel-slide.ino-carousel-slide--selected{opacity:1}",s=class{constructor(e){(0,o.r)(this,e)}render(){return(0,o.h)(o.H,{class:(0,t.c)({"ino-carousel-slide":!0,"ino-carousel-slide--selected":this.selected})},(0,o.h)("div",{class:"ino-carousel-slide__image",style:{"background-image":`url(${this.src})`}}))}get el(){return(0,o.g)(this)}};s.style=i}}]);
+//# sourceMappingURL=8216.23ff5b29.js.map
