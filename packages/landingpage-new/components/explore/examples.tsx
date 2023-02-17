2b0ffafd2c9f6a4f9978317284d01cbcd7651599
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-02-17 12:22:47 +0100

    :sparkles: use new carousel in new landingpage

--- packages/landingpage-new/components/explore/examples.tsx
+++ packages/landingpage-new/components/explore/examples.tsx
@@ -1,9 +1,9 @@
 import { InoCarousel, InoCarouselSlide, InoIconButton } from '@elements';
-import useBasePath from '../../utils/hooks/use-base-path';
 import styles from './examples.module.scss';
-import { useInterval, useMethods } from 'react-use';
+import { useMethods } from 'react-use';
 import classNames from 'classnames';
 import useTranslation from '../../utils/hooks/useTranslation';
+import useBasePath from '../../utils/hooks/useBasePath';
 
 enum Slides {
   Jarvis,
@@ -14,25 +14,27 @@ enum Slides {
 
 function createMethods(slide: Slides) {
   return {
-    increment() {
-      return slide === Slides.Inca ? Slides.Jarvis : slide + 1;
+    set(newSlide: Slides) {
+      return newSlide;
     },
-    decrement() {
-      return slide === Slides.Jarvis ? Slides.Inca : slide - 1;
+    increment(): Slides {
+      return slide === Slides.Inca ? Slides.Jarvis : Number(slide) + 1;
+    },
+    decrement(): Slides {
+      return slide === Slides.Jarvis ? Slides.Inca : Number(slide) - 1;
     },
   };
 }
 
 function Examples() {
   const basePath = useBasePath();
+  const carouselImgPathPrefix = `${basePath}/Carousel`;
   const { t } = useTranslation();
-  const [slideIndex, { increment, decrement }] = useMethods(
-    createMethods,
-    Slides.Jarvis
-  );
 
-  // implements buggy autoplay functionality until https://github.com/inovex/elements/issues/874 is resolved
-  useInterval(increment, 4000);
+  const [slide, { set, increment, decrement }] = useMethods(
+    createMethods,
+    Slides.Jarvis as Slides
+  );
 
   return (
     <>
@@ -42,36 +44,38 @@ function Examples() {
       <p className={classNames('body-l', styles.subtitle)}>
         {t('examples.sub_title')}
       </p>
-      <div className={styles.carouselContainer}>
+      <div className={styles.container}>
         <InoIconButton
           icon="arrow_left"
           activated
           onClick={() => decrement()}
         ></InoIconButton>
-        <InoCarousel
-          className={styles.slideshow}
-          hideButtons
-          animated
-          value={String(slideIndex)}
-          onValueChange={console.log}
-        >
-          <InoCarouselSlide
-            value={String(Slides.Jarvis)}
-            src={`${basePath}/JARVIS.png`}
-          ></InoCarouselSlide>
-          <InoCarouselSlide
-            value={String(Slides.SelfService)}
-            src={`${basePath}/SELFSERVICE.png`}
-          ></InoCarouselSlide>
-          <InoCarouselSlide
-            value={String(Slides.SelfOnboarding)}
-            src={`${basePath}/SELFONBOARDING.png`}
-          ></InoCarouselSlide>
-          <InoCarouselSlide
-            value={String(Slides.Inca)}
-            src={`${basePath}/INCA.png`}
-          ></InoCarouselSlide>
-        </InoCarousel>
+        <div className={styles.carouselContainer}>
+          <InoCarousel
+            className={styles.slideshow}
+            hideButtons
+            autoplay
+            value={slide}
+            onValueChange={(ev) => set(ev.detail)}
+          >
+            <InoCarouselSlide
+              value={Slides.Jarvis}
+              src={`${carouselImgPathPrefix}_JARVIS.png`}
+            ></InoCarouselSlide>
+            <InoCarouselSlide
+              value={Slides.SelfService}
+              src={`${carouselImgPathPrefix}_SELFSERVICE.png`}
+            ></InoCarouselSlide>
+            <InoCarouselSlide
+              value={Slides.SelfOnboarding}
+              src={`${carouselImgPathPrefix}_SELFONBOARDING.png`}
+            ></InoCarouselSlide>
+            <InoCarouselSlide
+              value={Slides.Inca}
+              src={`${carouselImgPathPrefix}_INCA.png`}
+            ></InoCarouselSlide>
+          </InoCarousel>
+        </div>
         <InoIconButton
           icon="arrow_right"
           activated
