ccbbebdeac93345520255eb3674678e4ded4c90d
Tobias Heim Galindo <81302108+TobiasHeimGalindo@users.noreply.github.com>
2023-03-02 10:34:33 +0100

    feat(landingpage): add header to explore page (#895)
    Co-authored-by: Jan-Niklas Voß <jan-niklas.voss@inovex.de>

--- packages/landingpage-new/components/explore/demos.tsx
+++ packages/landingpage-new/components/explore/demos.tsx
@@ -1,37 +1,53 @@
 import Image from 'next/image';
-import { Framework, IconByFramework } from 'utils/frameworks';
+import { Framework, IconByFramework, NameByFramework } from 'utils/frameworks';
 import Link from 'next/link';
 import styles from './demos.module.scss';
 import useTranslation from 'utils/hooks/useTranslation';
+import DemoCard from './demos-card';
 
 const DemoProjectByFrameworks: Partial<Record<Framework, string>> = {
+  [Framework.JS]:
+    'https://stackblitz.com/edit/ino-elements-js-example?embed=1&file=index.js',
   [Framework.REACT]: 'https://elements.inovex.de/example-projects/react/',
   [Framework.ANGULAR]: 'https://elements.inovex.de/example-projects/angular/',
   [Framework.VUE]: 'https://elements.inovex.de/example-projects/vue/',
 };
 
 function Demos() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <>
       <h1 className="header-d3">
         <b>{t('demos.title_1')}</b> {t('demos.title_2')}
       </h1>
-      {Object.keys(DemoProjectByFrameworks).map((key) => (
-        <Link
-          className={styles.link}
-          key={key}
-          href={DemoProjectByFrameworks[key as Framework] ?? ''}
-          target="_blank"
-        >
-          <Image
-            src={IconByFramework[key as Framework]}
-            alt={key + ' logo'}
-            fill
+      <p>{t('demos.subtitle')}</p>
+      <div className={styles.cardWrapper}>
+        {Object.keys(DemoProjectByFrameworks).map((key) => (
+          <DemoCard
+            key={key}
+            frameworkIcon={
+              <Image
+                height={40}
+                width={40}
+                src={IconByFramework[key as Framework]}
+                alt={key + ' logo'}
+                className={styles.framework}
+              />
+            }
+            frameworkName={NameByFramework[key as Framework]}
+            demoLink={
+              <Link
+                className={styles.footer}
+                href={DemoProjectByFrameworks[key as Framework] ?? ''}
+                target="_blank"
+              >
+                {t('demos.show_demo')}
+              </Link>
+            }
           />
-        </Link>
-      ))}
+        ))}
+      </div>
     </>
   );
 }
--- packages/landingpage-new/pages/[lang]/explore/index.tsx
+++ packages/landingpage-new/pages/[lang]/explore/index.tsx
@@ -11,13 +11,17 @@ import { Locale_File } from 'translations/types';
 import Page from 'components/layout/page';
 import useTranslation from 'utils/hooks/useTranslation';
 import Examples from 'components/explore/examples';
+import ExploreHeader from 'components/explore/explore-header';
+import styles from './index.module.scss';
+import classNames from 'classnames';
 
 const Explore = () => {
   const { t } = useTranslation();
 
   return (
     <Page title={[t('common.meta.explore')]}>
-      <div className="section-container">
+      <div className={classNames('section-container', styles.exploreContainer)}>
+        <ExploreHeader />
         <section id={SubRoutes.EXPLORE_EXAMPLES}>
           <Examples />
         </section>
@@ -25,7 +29,7 @@ const Explore = () => {
           <Demos />
         </section>
         <section id={SubRoutes.EXPLORE_RESSOURCES}>
-          <ResourceCard/>
+          <ResourceCard />
         </section>
       </div>
     </Page>
@@ -33,11 +37,15 @@ const Explore = () => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const {localization} = getStaticLanguageProps(ctx as LangContext, Locale_File.EXPLORE).props;
+  const { localization } = getStaticLanguageProps(
+    ctx as LangContext,
+    Locale_File.EXPLORE
+  ).props;
 
   return { props: { localization } };
 };
 
-export const getStaticPaths: GetStaticPaths = async () => getStaticLanguagePaths();
+export const getStaticPaths: GetStaticPaths = async () =>
+  getStaticLanguagePaths();
 
 export default Explore;
