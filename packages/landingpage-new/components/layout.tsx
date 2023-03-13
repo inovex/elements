65110a0a58f585c71c3efc9fdebb7f2675a56b5e
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-03-13 12:27:03 +0100

    fix(landingpage): fix responsive layout

--- packages/landingpage-new/components/layout.tsx
+++ packages/landingpage-new/components/layout.tsx
@@ -2,10 +2,14 @@ import { ReactNode } from 'react';
 import styles from './layout.module.scss';
 import Header from './layout/header';
 import Footer from './layout/footer';
+import Head from 'next/head';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className='inovex-elements-typo'>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </Head>
       <Header></Header>
       <main className={styles.main}>{children}</main>
       <Footer></Footer>
