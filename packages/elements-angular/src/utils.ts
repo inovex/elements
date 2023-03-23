1a9f4e504535975470ed3b41edf0288cc284cab0
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2023-03-23 13:03:18 +0100

    fix merge issues

--- packages/elements-angular/src/utils.ts
+++ packages/elements-angular/src/utils.ts
@@ -2,7 +2,7 @@
 declare const __zone_symbol__requestAnimationFrame: any;
 declare const requestAnimationFrame: any;
 
-export const raf = (h: any): number => {
+export const raf = (h: any): any => {
   if (typeof __zone_symbol__requestAnimationFrame === 'function') {
     return __zone_symbol__requestAnimationFrame(h);
   }
