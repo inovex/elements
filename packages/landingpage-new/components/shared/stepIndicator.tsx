748bd41ae2573bc3d7ad2d4ca6e20f6452375443
Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>
2023-04-14 14:12:43 +0200

    Finished react guide layout

--- packages/landingpage-new/components/shared/stepIndicator.tsx
+++ packages/landingpage-new/components/shared/stepIndicator.tsx
@@ -1,21 +1,51 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 
-export function StepIndicator({ step = '?' }) {
-  const style: CSSProperties = {
+interface StepIndicatorProps {
+  step?: string | null;
+  children?: ReactNode;
+}
+
+/**
+ * StepIndicator Component
+ * @param {string} [step='?'] - The step number or text to display inside the step indicator
+ * @param {ReactNode} [children=null] - The additional text or elements to display next to the step indicator
+ */
+export function StepIndicator({
+  step = '?',
+  children = null,
+}: StepIndicatorProps) {
+  const wrapperStyle: CSSProperties = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+
+  const stepStyle: CSSProperties = {
+    flex: 'none',
     height: '30px',
     width: '30px',
     backgroundColor: '#2D02FF',
     borderRadius: '50%',
     color: '#fff',
-    display: 'inline-block',
     fontSize: '20px',
     lineHeight: '30px',
     textAlign: 'center',
     marginRight: '16px',
   };
-  return (
-    <span className="step" style={style}>
-      {step}
-    </span>
-  );
+
+  if (children === null) {
+    return (
+      <div className="step" style={stepStyle}>
+        {step}
+      </div>
+    );
+  } else {
+    return (
+      <div className="step-indicator-wrapper" style={wrapperStyle}>
+        <div className="step" style={stepStyle}>
+          {step}
+        </div>
+        <div className="step-text">{children}</div>
+      </div>
+    );
+  }
 }
