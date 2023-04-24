9a03c4f343321cdc091e2c2d965d223e8bd8a1c7
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2023-04-24 15:08:10 +0200

    refactor(landingpage): add countup package

--- packages/landingpage-new/components/home/kpi-section/kpi-section.tsx
+++ packages/landingpage-new/components/home/kpi-section/kpi-section.tsx
@@ -1,6 +1,7 @@
 import useTranslation from 'utils/hooks/useTranslation';
 import styles from './kpi-section.module.scss';
-import CountUp from './CountUp';
+import classNames from 'classnames';
+import { useEffect, useRef } from 'react'
 
 interface KPIitem {
     id: number;
@@ -12,15 +13,41 @@ interface KPIitem {
 export default function KPIsection () {
     const { t } = useTranslation();
     const KPIs: KPIitem[] = t('kpi.kpis');
+
+    const countupRef = useRef(null);
+    let countUpAnim;
+    
+    // useEffect with empty dependency array runs once when component is mounted
+    useEffect(() => {
+        initCountUp();
+    }, []);
+
+    // dynamically import and initialize countUp, sets value of `countUpAnim`
+    // you don't have to import this way, but this works best for next.js
+    async function initCountUp() {
+        const countUpModule = await import('countup.js');
+        if(countupRef.current === null){
+            return
+        }
+        countUpAnim = new countUpModule.CountUp(countupRef.current, 1000);
+        if (!countUpAnim.error) {
+        countUpAnim.start();
+        } else {
+        console.error(countUpAnim.error);
+        }
+    }
+
     
     return (
         <>
-            <h1>
-                <b>{t('kpi.title_1')}</b>
-            </h1>
-            <h1>
-                {t('kpi.title_2')}
-            </h1>
+            <div className={styles.header_container}>
+                <h1 className={classNames(styles.header, 'header-d2')}>
+                    <b>{t('kpi.title_1')}</b>
+                </h1>
+                <h1 className={classNames(styles.header, 'header-d2')}>
+                    {t('kpi.title_2')}
+                </h1>
+            </div>
             <div className={styles.subheader}>
                 {t('kpi.subheader')}
             </div>
@@ -28,7 +55,10 @@ export default function KPIsection () {
                 {KPIs.map((kpi)=> {
                     return (
                         <div key={kpi.id} className={styles.kpi_container}>
-                            <CountUp count={kpi.target_value} duration={2000} />
+                            <b><h1 className={classNames('kpi_number', 'header-d2')} ref={countupRef}></h1></b>
+                            <div className={styles.kpi_title}>
+                                {kpi.title}
+                            </div>
                         </div>
                     )
                 })}
