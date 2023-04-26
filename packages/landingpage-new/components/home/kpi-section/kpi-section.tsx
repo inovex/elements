e79d5d7867ef1375a509eee602b5929a03dedd93
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2023-04-26 11:54:07 +0200

    refactor(landingpage): multiple refs for kpi

--- packages/landingpage-new/components/home/kpi-section/kpi-section.tsx
+++ packages/landingpage-new/components/home/kpi-section/kpi-section.tsx
@@ -1,7 +1,7 @@
 import useTranslation from 'utils/hooks/useTranslation';
 import styles from './kpi-section.module.scss';
 import classNames from 'classnames';
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, createRef } from 'react'
 
 interface KPIitem {
     id: number;
@@ -14,28 +14,40 @@ export default function KPIsection () {
     const { t } = useTranslation();
     const KPIs: KPIitem[] = t('kpi.kpis');
 
-    const countupRef = useRef(null);
+    const countupRefs = useRef<Array<HTMLDivElement | null>>([]);
+    // countupRefs.current = KPIs.map((countUpElement, i) => {
+    //     countupRefs.current[i] = countUpElement)
+    // };
     let countUpAnim;
     
-    // useEffect with empty dependency array runs once when component is mounted
     useEffect(() => {
         initCountUp();
     }, []);
 
+    useEffect(() => {
+        console.log('countupRefs updated?', countupRefs.current)
+    },[countupRefs.current])
+
+    
     // dynamically import and initialize countUp, sets value of `countUpAnim`
     // you don't have to import this way, but this works best for next.js
     async function initCountUp() {
         const countUpModule = await import('countup.js');
-        if(countupRef.current === null){
-            return
-        }
-        countUpAnim = new countUpModule.CountUp(countupRef.current, 1000);
-        if (!countUpAnim.error) {
-        countUpAnim.start();
-        } else {
-        console.error(countUpAnim.error);
-        }
+        countupRefs.current.forEach((countUpElement, i)=> {
+            if(countUpElement === null){
+                return
+            }
+            countUpAnim = new countUpModule.CountUp(countupRefs.current[i] as HTMLDivElement, 1000);
+            if (!countUpAnim.error) {
+            countUpAnim.start();
+            } else {
+            console.error(countUpAnim.error);
+            }
+        });
     }
+
+    // function, welche target value annimmt & ein ein neues countup object erzeugt
+    
 
     
     return (
@@ -52,10 +64,10 @@ export default function KPIsection () {
                 {t('kpi.subheader')}
             </div>
             <div className={styles.kpi_wrapper}>
-                {KPIs.map((kpi)=> {
+                {KPIs.map((kpi, i) => {
                     return (
                         <div key={kpi.id} className={styles.kpi_container}>
-                            <b><h1 className={classNames('kpi_number', 'header-d2')} ref={countupRef}></h1></b>
+                            <b><h1 className={classNames('kpi_number', 'header-d2')} ref={kpi => countupRefs.current[i] = kpi}></h1></b>
                             <div className={styles.kpi_title}>
                                 {kpi.title}
                             </div>
