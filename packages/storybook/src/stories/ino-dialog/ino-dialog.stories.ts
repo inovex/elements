6b5f4a4d64a6cb68be5f6ac33d9207ff7bbed4e0
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2023-05-16 16:39:29 +0200

    refactor(storybook|ino-dialog): add content story

--- packages/storybook/src/stories/ino-dialog/ino-dialog.stories.ts
+++ packages/storybook/src/stories/ino-dialog/ino-dialog.stories.ts
@@ -4,6 +4,9 @@ import { Meta } from '@storybook/web-components';
 import { TemplateGenerator } from '../template-generator';
 import { html } from 'lit-html';
 import { decorateStoryWithClass } from '../utils';
+import lightningImg from '../../assets/images/lightning.jpg';
+import mountainsImg from '../../assets/images/mountains.jpg';
+import nidarosImg from '../../assets/images/nidaros.jpg';
 
 import './ino-dialog.scss';
 
@@ -36,23 +39,9 @@ export default {
         };
         dialogs.forEach(dialog => dialog.addEventListener('close', handleClose));
 
-        const checkedChangeHandler = function (e) {
-          if (e.target.tagName.toLowerCase() !== 'ino-segment-button') {
-            return;
-          }
-          const segmentButton: HTMLInoSegmentButtonElement = e.target;
-          const group: HTMLInoSegmentGroupElement = segmentButton.closest(
-            'ino-segment-group'
-          );
-          group.value = segmentButton.value;
-        };
-
-        document.addEventListener('checkedChange', checkedChangeHandler);
-
         return () => {
           btns.forEach(btn => btn.removeEventListener('click', handleOpen));
           dialogs.forEach(dialog => dialog.removeEventListener('close', handleClose));
-          document.removeEventListener('checkedChange', checkedChangeHandler);
         };
       });
 
@@ -63,7 +52,7 @@ export default {
     attachTo: '.story-dialog',
     open: false,
     fullwidth: false,
-    dismissible: false,
+    dismissible: true,
     headline: 'Hi, I am a headline',
     description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, conset',
     actiontext: 'submit',
@@ -124,7 +113,6 @@ ConfirmationDialog.args = {
   headline: 'Discard draft?',
   canceltext: 'Cancel',
   actiontext: 'Discard',
-  dismissible: true,
   dataDialogId: 'demo-conformation-dialog',
   buttonText: ' Open confirmation Dialog'
 };
@@ -155,7 +143,6 @@ ConfirmationDialogWithText.args = {
   description: 'After deleting your account, you will no longer be able to access your saved items.',
   canceltext: 'Cancel',
   actiontext: 'Delete account',
-  dismissible: true,
   dataDialogId: 'demo-conformation-with-text-dialog',
   buttonText: 'Open confirmation Dialog with text'
 };
@@ -178,14 +165,13 @@ const templateConfirmationWithIcon = new TemplateGenerator<InoDialogExtended>(
 `);
 
 /**
- * Confirmation Dialog with Hero Icon requires an acknowledgement of the information from the user. User has to confirm with one klick on the primary button. Contains a hero icon, header, text, cancel and primary action button.
+ * A Confirmation Dialog with Hero Icon requires an acknowledgement of the information from the user. User has to confirm with one klick on the primary button. Contains a hero icon, header, text, cancel and primary action button.
  */
 export const ConfirmationDialogWithIcon = templateConfirmationWithIcon.generatePlaygroundStory();
 ConfirmationDialogWithIcon.args = {
   headline: 'Perfect! You almost made it...',
   description: 'A confirmation email is on the way. Please click on the link in the email to complete the registration and secure your discount code.',
   actiontext: 'Confirm',
-  dismissible: true,
   dataDialogId: 'demo-conformation-with-icon-dialog',
   buttonText: 'Open confirmation Dialog with icon',
   icon: 'message',
@@ -227,12 +213,55 @@ additionalBodyContent.args = {
   description: 'Inviting users is the easiest way to get your team collaborating. Enter an email address and assign a role below.',
   actiontext: 'Send invite',
   canceltext: 'Cancel',
-  dismissible: true,
   dataDialogId: 'demo-additional-body-content-dialog',
   buttonText: 'Open Dialog with additional body content',
 };
 
+const templateCustomContent = new TemplateGenerator<InoDialogExtended>(
+'ino-dialog',
+args => html`
+  <ino-button class="open-dialog-btn" data-dialog-id="${args.dataDialogId}">${args.buttonText}</ino-button>
+  <ino-dialog id="${args.dataDialogId}"
+    open="${args.open}"
+    fullwidth="${args.fullwidth}"
+    dismissible="${args.dismissible}"
+    attach-to="${args.attachTo}"
+    headline="${args.headline}"
+    actiontext="${args.actiontext}"
+    canceltext="${args.canceltext}"
+    description="${args.description}"
+  >
+    <ino-carousel slot="content"
+      class="customizable-carousel"
+      value="0"
+      autoplay="true"
+      hide-buttons="true"
+      infinite="true"
+      intermission="2000"
+      reverse="false"
+      @valueChange="${onSlideChanged}"
+    >
+      <ino-carousel-slide src=${lightningImg} value="0"></ino-carousel-slide>
+      <ino-carousel-slide src=${mountainsImg} value="1"></ino-carousel-slide>
+      <ino-carousel-slide src=${nidarosImg} value="2"></ino-carousel-slide>
+    </ino-carousel>
+  </ino-dialog>
+`);
 
+const onSlideChanged = (ev: CustomEvent<string>) => {
+  const carouselEl = ev.target as HTMLInoCarouselElement;
+  carouselEl.value = ev.detail;
+}
+
+/**
+ * Use `slot="content"` on the child-element of `ino-dialog` to remove the given dialog elements (`headline`, `description`,  `cancelButton` & `actionButton`) and instead display your own custom content.
+ */
+export const customContent = templateCustomContent.generatePlaygroundStory();
+customContent.args = {
+  dataDialogId: 'demo-custom-content-dialog',
+  buttonText: 'Open Dialog with custom content',
+};
+  
 
 export const FullWidth = template.generateStoryForProp('fullwidth', true);
 FullWidth.args = {
@@ -242,7 +271,7 @@ FullWidth.args = {
 }
 
 export const Dismissible = template.generateStoryForProp('dismissible', true);
-Dismissible .args = {
+Dismissible.args = {
   buttonText: 'Open dismissible Dialog',
   dataDialogId: 'demo-dismissible-dialog',
 }
@@ -254,7 +283,6 @@ Dismissible .args = {
  */
 export const ClosingAction = template.generatePlaygroundStory();
 ClosingAction.args = {
-  dismissible: true,
   dataDialogId: 'demo-action-dialog',
   buttonText: 'open closing action Dialog'
 };
