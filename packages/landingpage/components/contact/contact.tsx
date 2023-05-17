0882f43d9da33fe7638e62ec5aa01f373436898c
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-05-17 15:33:52 +0200

    refactor: replace old landingpage

---
+++ packages/landingpage/components/contact/contact.tsx
@@ -0,0 +1,75 @@
+import styles from './contact.module.scss';
+import Image from 'next/image';
+import { InoButton, InoIcon, InoInput, InoTextarea } from '@elements';
+import { FormEventHandler, useState } from 'react';
+import useTranslation from 'utils/hooks/useTranslation';
+
+function Contact() {
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const inovexMail = 'jan-niklas.voss@inovex.de';
+
+  const { t } = useTranslation();
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    message.replace(/ /g, '%20');
+    const mailtoLink =
+      'mailto:' + inovexMail + '?subject=' + subject + '&body=' + message;
+    window.open(mailtoLink, '_blank');
+  };
+
+  return (
+    <div className={styles.contact_section}>
+      <div className={styles.image1}>
+        <Image
+          src={`/contact-image1.png`}
+          alt="contact image1"
+          fill
+          style={{ objectFit: 'contain' }}
+        />
+      </div>
+      <div className={styles.wrapper}>
+        <h1 className="header-d3">
+          <b>{t('contact.title_1')}</b> {t('contact.title_2')}
+        </h1>
+        <p className="body-l">{t('contact.description')}</p>
+        <form className={styles.form_container} onSubmit={onSubmit}>
+          <InoInput
+            className={styles.subject}
+            value={subject}
+            onValueChange={(e) => setSubject(e.detail)}
+            type="text"
+            label={t('contact.label_subject')}
+            outline
+            required
+          ></InoInput>
+          <InoTextarea
+            className={styles.message}
+            value={message}
+            onValueChange={(e) => setMessage(e.detail)}
+            label={t('contact.label_message')}
+            outline
+            required
+          ></InoTextarea>
+          <InoButton className={styles.submit_button} type="submit">
+            <span>{t('contact.label_submit')}</span>
+            <InoIcon slot="icon-trailing" icon="mail"></InoIcon>
+          </InoButton>
+        </form>
+      </div>
+      <div className={styles.image2}>
+        <Image
+          src={`/contact-image2.png`}
+          alt="contact image2"
+          fill
+          style={{ objectFit: 'contain' }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ packages/landingpage/components/layout/header/desktop/header.desktop.tsx
@@ -0,0 +1,68 @@
+import styles from './header.desktop.module.scss';
+import Navbar from './navbar';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import ElementsLogo from '@assets/elements.svg';
+import LocaleSwitcher from './localeSwitcher';
+
+export default function HeaderDesktop() {
+  const [yOffset, setYOffset] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const offset = window.pageYOffset;
+
+      if (offset !== 0 && Math.abs(yOffset - offset) < 5) return; // prevent jumping
+
+      setYOffset(offset);
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [yOffset]);
+
+  return (
+    <header
+      className={styles.header}
+      style={{ '--factor': yOffset } as React.CSSProperties}
+    >
+      <div className={styles.logo}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="/"
+        >
+          <Image
+            src={ElementsLogo}
+            alt="inovex-elements Logo"
+            width={68}
+            height={68}
+          />
+        </a>
+      </div>
+      <div className={styles.language}>
+        <LocaleSwitcher />
+      </div>
+      <div className={styles.navbar}>
+        <Navbar />
+      </div>
+      <a
+        className={styles.github}
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/inovex/elements"
+      >
+        <Image
+          className={styles.githubImage}
+          src={`github-logo.png`}
+          alt="Github Logo"
+          width={30}
+          height={30}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+        />
+      </a>
+    </header>
+  );
+}
