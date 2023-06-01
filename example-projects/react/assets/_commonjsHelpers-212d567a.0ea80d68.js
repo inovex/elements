0e729c0f4fa542a18ce543deccfd39648e82f1e3
janivo <github@jnvoss.de>
2023-06-01 15:45:43 +0000

    Deploy landingpage

--- example-projects/react/assets/_commonjsHelpers-212d567a.0ea80d68.js
+++
@@ -1,3 +0,0 @@
-/*!
- * Crafted with ❤ by inovex GmbH
- */var l=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{};function s(e){return e&&e.__esModule&&Object.prototype.hasOwnProperty.call(e,"default")?e.default:e}function u(e,n,o){return o={path:n,exports:{},require:function(t,a){return r()}},e(o,o.exports),o.exports}function r(){throw new Error("Dynamic requires are not currently supported by @rollup/plugin-commonjs")}export{l as a,u as c,s as g};
