79f8fa8848d7bd60476c3719084c980d0f015e88
janivo <github@jnvoss.de>
2023-06-19 15:58:47 +0000

    Deploy landingpage

--- example-projects/react/assets/ino-carousel-slide.entry.9a5e88ed.js
+++
@@ -1,3 +0,0 @@
-import{r as i,h as s,H as o,g as r}from"./index.68c728be.js";import{c as a}from"./index-796d4780.ad696562.js";import"./_commonjsHelpers-212d567a.0ea80d68.js";/*!
- * Crafted with ❤ by inovex GmbH
- */const t=".ino-carousel-slide{grid-row:1;grid-column:1;opacity:0;transition:opacity var(--carousel-animation-duration)}.ino-carousel-slide__image{background-size:cover;background-position:50%;width:100%;height:100%}.ino-carousel-slide.ino-carousel-slide--selected{opacity:1}",l=class{constructor(e){i(this,e)}render(){return s(o,{class:a({"ino-carousel-slide":!0,"ino-carousel-slide--selected":this.selected})},s("div",{class:"ino-carousel-slide__image",style:{"background-image":`url(${this.src})`}}))}get el(){return r(this)}};l.style=t;export{l as ino_carousel_slide};
