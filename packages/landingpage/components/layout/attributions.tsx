453c6660a35ef9a1b7de4bf6f33509fa9caa320c
Patrick Hillert <silentHoo@users.noreply.github.com>
2023-06-27 11:48:13 +0200

    chore(landingpage): add legal links (#993)
    Co-authored-by: Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>

--- packages/landingpage/components/layout/attributions.tsx
+++ packages/landingpage/components/layout/attributions.tsx
@@ -1,21 +1,18 @@
-import classNames from 'classnames';
-import styles from './attributions.module.scss';
-import Image from 'next/image';
 import inovexLogo from '@assets/inovex-logo.svg';
+import classNames from 'classnames';
+import Image from 'next/image';
+import useTranslation from 'utils/hooks/useTranslation';
+import styles from './attributions.module.scss';
 
 export default function Attributions() {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
-      <a
-        className={classNames(styles.attribution, 'body-m')}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.inovex.de/"
-      >
-        <small>provided by</small>
+      <div className={styles.company}>
         <Image
           src={inovexLogo}
-          alt="Github Logo"
+          alt="Inovex Logo"
           width={30}
           height={30}
           style={{
@@ -23,8 +20,39 @@ export default function Attributions() {
             height: 'auto',
           }}
         />
-        <small>inovex GmbH</small>
-      </a>
+        <a
+          className={classNames(styles.attribution, 'body-m')}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.inovex.de/"
+        >
+          <small>inovex GmbH</small>
+        </a>
+        <span className={classNames(styles.attribution, 'body-m')}>&bull;</span>
+        <a
+          className={classNames(styles.attribution, 'body-m')}
+          rel="noopener noreferrer"
+          href={t('common.attributions.legalNotice.url')}
+        >
+          <small>{t('common.attributions.legalNotice.label')}</small>
+        </a>
+        <span className={classNames(styles.attribution, 'body-m')}>&bull;</span>
+        <a
+          className={classNames(styles.attribution, 'body-m')}
+          rel="noopener noreferrer"
+          href={t('common.attributions.exclusionOfLiability.url')}
+        >
+          <small>{t('common.attributions.exclusionOfLiability.label')}</small>
+        </a>
+        <span className={classNames(styles.attribution, 'body-m')}>&bull;</span>
+        <a
+          className={classNames(styles.attribution, 'body-m')}
+          rel="noopener noreferrer"
+          href={t('common.attributions.dataProtection.url')}
+        >
+          <small>{t('common.attributions.dataProtection.label')}</small>
+        </a>
+      </div>
       <a
         className={classNames(styles.attribution, 'body-m')}
         href="https://storyset.com/online"
