40caff4da5886f954d6c10bbd0b1fad691b01374
Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>
2023-06-27 11:19:03 +0200

    fixed typo;

--- packages/landingpage/components/layout/attributions.tsx
+++ packages/landingpage/components/layout/attributions.tsx
@@ -1,25 +1,25 @@
-import classNames from 'classnames';
-import styles from './attributions.module.scss';
-import Image from 'next/image';
 import inovexLogo from '@assets/inovex-logo.svg';
+import classNames from 'classnames';
+import Image from 'next/image';
 import useTranslation from 'utils/hooks/useTranslation';
+import styles from './attributions.module.scss';
 
 export default function Attributions() {
   const { t } = useTranslation();
-  
+
   return (
     <div className={styles.container}>
       <div className={styles.company}>
         <Image
-            src={inovexLogo}
-            alt="Github Logo"
-            width={30}
-            height={30}
-            style={{
-              maxWidth: '100%',
-              height: 'auto',
-            }}
-          />
+          src={inovexLogo}
+          alt="Inovex Logo"
+          width={30}
+          height={30}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+        />
         <a
           className={classNames(styles.attribution, 'body-m')}
           target="_blank"
@@ -28,28 +28,25 @@ export default function Attributions() {
         >
           <small>inovex GmbH</small>
         </a>
-        <span className={classNames(styles.attribution, 'body-m')}>
-          &bull;
-        </span>
-        <a className={classNames(styles.attribution, 'body-m')}
+        <span className={classNames(styles.attribution, 'body-m')}>&bull;</span>
+        <a
+          className={classNames(styles.attribution, 'body-m')}
           rel="noopener noreferrer"
           href={t('common.attributions.legalNotice.url')}
         >
           <small>{t('common.attributions.legalNotice.label')}</small>
         </a>
-        <span className={classNames(styles.attribution, 'body-m')}>
-          &bull;
-        </span>
-        <a className={classNames(styles.attribution, 'body-m')}
+        <span className={classNames(styles.attribution, 'body-m')}>&bull;</span>
+        <a
+          className={classNames(styles.attribution, 'body-m')}
           rel="noopener noreferrer"
           href={t('common.attributions.exclusionOfLiability.url')}
         >
           <small>{t('common.attributions.exclusionOfLiability.label')}</small>
         </a>
-        <span className={classNames(styles.attribution, 'body-m')}>
-          &bull;
-        </span>
-        <a className={classNames(styles.attribution, 'body-m')}
+        <span className={classNames(styles.attribution, 'body-m')}>&bull;</span>
+        <a
+          className={classNames(styles.attribution, 'body-m')}
           rel="noopener noreferrer"
           href={t('common.attributions.dataProtection.url')}
         >
