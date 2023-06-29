7daa831dfb5e1aa6481cb78ada1e687356d9ec51
Tobias Heim Galindo <tobisellfy@gmail.com>
2023-06-29 13:14:30 +0200

    chore: initial bump to storybook 7

--- packages/storybook/config/main.js
+++ packages/storybook/config/main.js
@@ -1,54 +1,53 @@
 // main point of configuration for storybook
 const path = require('path');
-
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   staticDirs: ['../static', '../../elements/src/assets', '../../../assets/logo'],
   stories: ['../src/**/*.stories.ts', '../src/**/*.stories.mdx'],
-  addons: ['@storybook/addon-essentials', '@pxtrn/storybook-addon-docs-stencil'],
+  addons: ['@storybook/addon-essentials', '@pxtrn/storybook-addon-docs-stencil', "@storybook/addon-mdx-gfm"],
   managerEntries: ['./addons/post-current-story'],
   typescript: {
     compilerOptions: {
-      "typeRoots" : ["node_modules/@types", "../src/types"]
-    },
+      "typeRoots": ["node_modules/@types", "../src/types"]
+    }
   },
-  webpackFinal: (config) => {
+  webpackFinal: config => {
     config.devServer = {
       watchContentBase: true,
       contentBase: path.join(__dirname, 'src'),
-      historyApiFallback: true,
+      historyApiFallback: true
     };
 
     // Sass Support
     config.module.rules.push({
       test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: 'sass-loader',
-          options: {
-            implementation: require('sass'),
-            sassOptions: {
-              includePaths: [
-                path.resolve(__dirname, '../src/stories'),
-                path.resolve(__dirname, '../../elements/src/components'),
-                path.resolve(__dirname, '../../../node_modules'),
-              ],
-            },
-          },
-        },
-      ],
+      use: ['style-loader', 'css-loader', {
+        loader: 'sass-loader',
+        options: {
+          implementation: require('sass'),
+          sassOptions: {
+            includePaths: [path.resolve(__dirname, '../src/stories'), path.resolve(__dirname, '../../elements/src/components'), path.resolve(__dirname, '../../../node_modules')]
+          }
+        }
+      }]
     });
 
     // Story Description
     config.module.rules.push({
       test: /\.stories\.ts/,
-      use: [{ loader: "story-description-loader", options: { isTSX: true } }],
+      use: [{
+        loader: "story-description-loader",
+        options: {
+          isTSX: true
+        }
+      }]
     });
-
     return config;
   },
+  framework: {
+    name: "@storybook/web-components-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
