25a4dc33ad8b341b2ae33e3d4cccb5387e50b445
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-09-20 12:42:17 +0200

    chore: fix landingpage format

--- packages/landingpage/utils/elementsWrapper.ts
+++ packages/landingpage/utils/elementsWrapper.ts
@@ -1,9 +1,7 @@
+// @ts-nocheck
 import dynamic from 'next/dynamic';
-// eslint-disable-next-line no-restricted-imports
 import Components from '@inovex.de/elements-react/dist';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 export const importElement = <Component extends keyof typeof Components>(
   element: Component,
 ): (typeof Components)[Component] =>
