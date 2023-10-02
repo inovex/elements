57bf8c69f244dac6ab3516205cf3b38b756dfcd0
Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>
2023-10-02 11:51:40 +0200

    ordered according to enum

--- packages/elements/src/components/ino-markdown-editor/editor-toolbar-helper.ts
+++ packages/elements/src/components/ino-markdown-editor/editor-toolbar-helper.ts
@@ -23,20 +23,26 @@ export function handleToolbarBtnClick(
   url?
 ): void {
   switch (action) {
+    case Actions.ITALIC:
+      focusChain(editor).toggleItalic().run();
+      break;
+    case Actions.BOLD:
+      focusChain(editor).toggleBold().run();
+      break;
     case Actions.H1:
       focusChain(editor).toggleHeading({ level: 1 }).run();
       break;
     case Actions.H2:
       focusChain(editor).toggleHeading({ level: 2 }).run();
       break;
-    case Actions.BOLD:
-      focusChain(editor).toggleBold().run();
-      break;
-    case Actions.ITALIC:
-      focusChain(editor).toggleItalic().run();
-      break;
     case Actions.STRIKE:
       focusChain(editor).toggleStrike().run();
+      break;
+    case Actions.OL:
+      focusChain(editor).toggleOrderedList().run();
+      break;
+    case Actions.UL:
+      focusChain(editor).toggleBulletList().run();
       break;
     case Actions.LINK:
       if (url) {
@@ -46,17 +52,11 @@ export function handleToolbarBtnClick(
     case Actions.UNLINK:
       focusChain(editor).unsetLink().run();
       break;
-    case Actions.OL:
-      focusChain(editor).toggleOrderedList().run();
-      break;
-    case Actions.UL:
-      focusChain(editor).toggleBulletList().run();
+    case Actions.CODE_BLOCK:
+      focusChain(editor).toggleCodeBlock().run();
       break;
     case Actions.BLOCKQUOTE:
       focusChain(editor).toggleBlockquote().run();
-      break;
-    case Actions.CODE_BLOCK:
-      focusChain(editor).toggleCodeBlock().run();
       break;
     case Actions.TASK_LIST:
       focusChain(editor).toggleTaskList().run();
@@ -70,26 +70,26 @@ export function isToolbarBtnActive(editor: Editor, action: Actions): boolean {
   if (!editor) return false;
 
   switch (action) {
+    case Actions.ITALIC:
+      return editor.isActive('italic');
+    case Actions.BOLD:
+      return editor.isActive('bold');
     case Actions.H1:
       return editor.isActive('heading', { level: 1 });
     case Actions.H2:
       return editor.isActive('heading', { level: 2 });
-    case Actions.BOLD:
-      return editor.isActive('bold');
-    case Actions.ITALIC:
-      return editor.isActive('italic');
     case Actions.STRIKE:
       return editor.isActive('strike');
-    case Actions.LINK:
-      return editor.isActive('link');
     case Actions.OL:
       return editor.isActive('orderedList');
     case Actions.UL:
       return editor.isActive('bulletList');
-    case Actions.BLOCKQUOTE:
-      return editor.isActive('blockquote');
+    case Actions.LINK:
+      return editor.isActive('link');
     case Actions.CODE_BLOCK:
       return editor.isActive('code');
+    case Actions.BLOCKQUOTE:
+      return editor.isActive('blockquote');
     case Actions.TASK_LIST:
       return editor.isActive('taskList');
     default:
