5905370c942811c2b6c94b0b8154f467a43941f7
Benedikt Scheffbuch <benedikt.scheffbuch@inovex.de>
2023-10-19 14:39:09 +0200

    WIP

--- packages/storybook/addons/open-playground-automatically.tsx
+++ packages/storybook/addons/open-playground-automatically.tsx
@@ -18,27 +18,38 @@ addons.register('open-playground-automatically', () => {
         if (mutation.type === 'childList') {
           const addedNodes = Array.from(mutation.addedNodes);
 
+          let target = mutation.target as HTMLElement;
+
+          if (
+            Array.from(target.classList).some(className =>
+              className.startsWith('sto-')
+            )
+          ) {
+          }
+
           // Attach click event to added buttons whose text starts with "ino-"
           addedNodes.forEach(node => {
-            if (node instanceof HTMLElement) {
-              const buttons = node.querySelectorAll(
-                'button.sidebar-item'
-              ) as NodeListOf<HTMLElement>;
-              buttons.forEach(button => {
-                if (button.innerText.startsWith('ino-')) {
-                  console.log(button);
+            if (node instanceof HTMLButtonElement) {
+              if (node.innerText.startsWith('ino-')) {
+                console.log('added click listener');
+                console.log('target', mutation.target);
+                console.log('found', mutation.addedNodes);
 
-                  attachClickEvent(button);
-                }
-              });
+                attachClickEvent(node);
+              }
             }
           });
         }
       }
     });
 
-    // Observe only for direct child list changes (added/removed elements)
-    observer.observe(document.body, { childList: true, subtree: true });
+    let nav = document.querySelector('nav.container.sidebar-container');
+
+    if (nav) {
+      console.log(nav);
+      // Observe only for direct child list changes (added/removed elements)
+      observer.observe(nav, { childList: true, subtree: true });
+    }
   };
 
   initializeObserver();
