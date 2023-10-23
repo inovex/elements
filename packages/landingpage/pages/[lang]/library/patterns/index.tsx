3fc91e8da802594e6a2974811603257281a2317c
Tobias Heim Galindo <tobisellfy@gmail.com>
2023-10-23 13:40:47 +0200

    refactor: add preview-box, styling and parent-child component logic

--- packages/landingpage/pages/[lang]/library/patterns/index.tsx
+++ packages/landingpage/pages/[lang]/library/patterns/index.tsx
@@ -8,17 +8,27 @@ import {
 import { Locale_File } from '../../../../translations/types';
 import PreviewBox from './preview-box';
 
+const MockupComponent: React.FC = () => (
+  <div>Hello, this is a mockup component!</div>
+);
+
+const mockupHTMLString = `<div>Hello, this is a mockup HTML!</div>`;
+
 const PatternsPage: NextPage = () => {
   return (
     <div>
-      <h1>Welcome to the Patterns page.</h1>
-      <PreviewBox title="Pattern 1" description="This is a cool pattern." />
-      <PreviewBox title="Pattern 2" description="This is another cool pattern." />
+      <h1>Design Pattern</h1>
+      <PreviewBox
+        title="Login"
+        description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+        previewComponent={<MockupComponent />}
+        codeString={mockupHTMLString}
+      />
+    
     </div>
   );
 };
 
-// Static Generation methods
 export const getStaticProps: GetStaticProps = async (ctx) => {
   return getStaticLanguageProps(ctx as LangContext, Locale_File.LIBRARY);
 };
--- packages/landingpage/pages/[lang]/library/patterns/preview-box.tsx
+++ packages/landingpage/pages/[lang]/library/patterns/preview-box.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { InoSegmentGroup, InoSegmentButton, InoIcon } from '@elements';
+import styles from './preview-box.module.scss';
+
 
 interface PreviewBoxProps {
   title: string;
   description: string;
+  previewComponent: JSX.Element;
+  codeString: string;
 }
 
-const PreviewBox: React.FC<PreviewBoxProps> = ({ title, description }) => {
+export default function PreviewBox({
+  title,
+  description,
+  previewComponent,
+  codeString,
+}: PreviewBoxProps) {
+  const [selectedValue, setSelectedValue] = useState<string>('preview');
+
   return (
-    <div>
+    <div className={styles.patternsWrapper}>
       <h3>{title}</h3>
       <p>{description}</p>
+      <div className={styles.segmentGroup}>
+        <InoSegmentGroup
+          value={selectedValue}
+          onValueChange={(event) => setSelectedValue(event.detail)}
+        >
+          <InoSegmentButton value="preview" className={styles.segmentButton}>
+            <InoIcon style={{ marginRight: '7.5px' }} icon="display"></InoIcon>{' '}
+            Preview
+          </InoSegmentButton>
+          <InoSegmentButton value="Code" className={styles.segmentButton}>
+            <InoIcon style={{ marginRight: '7.5px' }} icon="code"></InoIcon>{' '}
+            Code
+          </InoSegmentButton>
+        </InoSegmentGroup>
+      </div>
+      <div className={styles.previewBox}>
+        {selectedValue === 'preview' && (
+          <div className={styles.pattern}>{previewComponent}</div>
+        )}
+        {selectedValue === 'Code' && (
+          <div className={styles.code}>{codeString}</div>
+
+          // TODO: use shiki highlighter which is closes to CodeHike (https://github.com/shikijs/next-shiki) and (https://github.com/shikijs/shiki)
+        )}
+      </div>
     </div>
   );
-};
-
-export default PreviewBox;
+}
