01a406f1707a60daebbc750eb07977668f49d5ec
Tobias Heim Galindo <tobisellfy@gmail.com>
2023-11-15 11:56:53 +0100

    refactor: clean up previewbox

--- packages/landingpage/pages/[lang]/library/patterns/preview-box.tsx
+++ packages/landingpage/pages/[lang]/library/patterns/preview-box.tsx
@@ -7,41 +7,16 @@ import {
 } from '@elements';
 import styles from './preview-box.module.scss';
 
-interface PreviewBoxProps {
-  title: string;
-  description: string;
-  previewComponent: JSX.Element;
-  highlightedCode: string;
+type ViewMode = 'CodeMode' | 'PreviewMode';
+
+interface CopyButtonProps {
   rawCode: string;
+  copyIconId: string;
 }
 
-export default function PreviewBox({
-  title,
-  description,
-  previewComponent,
-  highlightedCode,
-  rawCode,
-}: PreviewBoxProps) {
-  const [selectedValue, setSelectedValue] = useState<string>('Preview');
-  const [hoveredButton, setHoveredButton] = useState<string | null>(null);
+function CopyButton({ rawCode, copyIconId }: CopyButtonProps) {
   const [copyIcon, setCopyIcon] = useState<string>('copy');
   const [visiblePopover, setVisiblePopover] = useState<boolean>(false);
-
-  const previewRef = useRef<HTMLDivElement>(null);
-
-  const previewBoxHeight = useMemo(() => {
-    if(!previewRef.current) return 0
-
-    return previewRef.current.getBoundingClientRect().height
-  }, [previewRef.current])
-
-  const handleMouseEnter = (value: string) => {
-    setHoveredButton(value);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredButton(null);
-  };
 
   const handleIconClick = () => {
     setCopyIcon('checkmark');
@@ -57,87 +32,140 @@ export default function PreviewBox({
   };
 
   return (
+    <>
+      <InoIcon
+        id={copyIconId}
+        icon={copyIcon}
+        className={styles.copyIcon}
+        onClick={handleIconClick}
+      />
+      <InoPopover
+        controlled={true}
+        visible={visiblePopover}
+        for={copyIconId}
+        trigger="click"
+        placement="top"
+      >
+        <div className={styles.popover}>
+          <span className="body-s">Copied!</span>
+        </div>
+      </InoPopover>
+    </>
+  );
+}
+
+interface ViewModeSelectionProps {
+  selectedValue: ViewMode;
+  setSelectedValue: React.Dispatch<React.SetStateAction<ViewMode>>;
+}
+
+function ViewModeSelection({
+  selectedValue,
+  setSelectedValue,
+}: ViewModeSelectionProps) {
+  const [hoveredButton, setHoveredButton] = useState<string | null>(null);
+
+  const handleMouseEnter = (value: string) => setHoveredButton(value);
+  const handleMouseLeave = () => setHoveredButton(null);
+
+  return (
+    <InoSegmentGroup
+      value={selectedValue}
+      onValueChange={(event) => setSelectedValue(event.detail)}
+    >
+      <InoSegmentButton
+        value="PreviewMode"
+        onMouseEnter={() => handleMouseEnter('PreviewMode')}
+        onMouseLeave={handleMouseLeave}
+      >
+        <span>
+          <InoIcon
+            icon="display"
+            className={
+              selectedValue === 'CodeMode' && hoveredButton !== 'PreviewMode'
+                ? styles.unselectedIcon
+                : ''
+            }
+          />
+          Preview
+        </span>
+      </InoSegmentButton>
+      <InoSegmentButton
+        value="CodeMode"
+        onMouseEnter={() => handleMouseEnter('CodeMode')}
+        onMouseLeave={handleMouseLeave}
+      >
+        <span>
+          <InoIcon
+            icon="code"
+            className={
+              selectedValue === 'PreviewMode' && hoveredButton !== 'CodeMode'
+                ? styles.unselectedIcon
+                : ''
+            }
+          />
+          Code
+        </span>
+      </InoSegmentButton>
+    </InoSegmentGroup>
+  );
+}
+
+interface PreviewBoxProps {
+  id: string;
+  title: string;
+  description: string;
+  previewComponent: JSX.Element;
+  highlightedCode: string;
+  rawCode: string;
+}
+
+export default function PreviewBox({
+  id,
+  title,
+  description,
+  previewComponent,
+  highlightedCode,
+  rawCode,
+}: PreviewBoxProps) {
+  const [selectedValue, setSelectedValue] = useState<ViewMode>('PreviewMode');
+  const previewRef = useRef<HTMLDivElement>(null);
+
+  const previewBoxHeight = useMemo(() => {
+    if (!previewRef.current) return 0;
+    return previewRef.current.getBoundingClientRect().height;
+  }, [previewRef.current]);
+
+  return (
     <div className={styles.patternsWrapper}>
       <h1 className="header-h1">{title}</h1>
       <p className="body-l">{description}</p>
       <div className={styles.segmentGroup}>
-        <InoSegmentGroup
-          value={selectedValue}
-          onValueChange={(event) => setSelectedValue(event.detail)}
-        >
-          <InoSegmentButton
-            value="Preview"
-            onMouseEnter={() => handleMouseEnter('Preview')}
-            onMouseLeave={handleMouseLeave}
-          >
-            <span>
-              <InoIcon
-                icon="display"
-                className={
-                  selectedValue === 'Code' && hoveredButton !== 'Preview'
-                    ? styles.unselectedIcon
-                    : ''
-                }
-              ></InoIcon>
-              Preview
-            </span>
-          </InoSegmentButton>
-          <InoSegmentButton
-            value="Code"
-            onMouseEnter={() => handleMouseEnter('Code')}
-            onMouseLeave={handleMouseLeave}
-          >
-            <span>
-              <InoIcon
-                icon="code"
-                className={
-                  selectedValue === 'Preview' && hoveredButton !== 'Code'
-                    ? styles.unselectedIcon
-                    : ''
-                }
-              ></InoIcon>
-              Code
-            </span>
-          </InoSegmentButton>
-        </InoSegmentGroup>
-
+        <ViewModeSelection
+          selectedValue={selectedValue}
+          setSelectedValue={setSelectedValue}
+        />
         <div className={styles.divider} />
-        <InoIcon
-          id="copy-icon"
-          icon={copyIcon}
-          className={styles.copyIcon}
-          onClick={handleIconClick}
-        ></InoIcon>
-        <InoPopover
-          controlled={true}
-          visible={visiblePopover}
-          for="copy-icon"
-          trigger="click"
-          placement="top"
-        >
-          {' '}
-          <div className={styles.popover}>
-            <span className="body-s">Copied!</span>
-          </div>
-        </InoPopover>
+        <CopyButton rawCode={rawCode} copyIconId={`copy-icon-${id}`} />
       </div>
       <div
         className={`${styles.previewBox} ${
-          selectedValue === 'Preview' ? styles.previewMode : styles.codeMode
+          selectedValue === 'PreviewMode' ? styles.previewMode : styles.codeMode
         }`}
-        style={{
-          background: selectedValue === 'Code' ? '#1E1E1E' : '#ffffff80',
-          padding: selectedValue === 'Code' ? '16px' : '16px',
-        }}
       >
         <div
           ref={previewRef}
-          style={{ display: selectedValue === 'Preview' ? 'block' : 'none' }}
+          style={{
+            display: selectedValue === 'PreviewMode' ? 'block' : 'none',
+          }}
         >
           {previewComponent}
         </div>
         <div
-          style={{ display: selectedValue === 'Code' ? 'block' : 'none', height: previewBoxHeight }}
+          style={{
+            display: selectedValue === 'CodeMode' ? 'block' : 'none',
+            height: previewBoxHeight,
+          }}
           className={styles.code}
           dangerouslySetInnerHTML={{ __html: highlightedCode }}
         ></div>
