b6a185102ac843d6ad09a47cf6aadd560e2bd260
Tobias Heim Galindo <tobisellfy@gmail.com>
2023-11-22 13:13:38 +0100

    docs: Add comments explaining theme-aware config approach

--- packages/landingpage/pages/_app.tsx
+++ packages/landingpage/pages/_app.tsx
@@ -15,6 +15,18 @@ import UiContextProvider from '../utils/context/UiContext';
 import { VersionProvider } from '../utils/context/VersionContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  /*
+    Theme Toggle Implementation:
+    ----------------------------
+    - A theme state should be introduced to manage the current theme (light or dark).
+    - The theme state will be used to toggle a class on a high-level element, like `body`.
+    - This class change will dynamically update the styles of Stencil components used in the application via CSS variables.
+    - The actual theme toggle function (to switch between light and dark) can be implemented and triggered from a UI element like a button.
+    - This setup allows runtime theme switching without causing a full page re-render, as it relies on CSS variable updates.
+
+    NOTE: this probably interfers with "@import '@inovex.de/elements/dist/inovex-elements/inovex-elements.css';" in the landingpage global.css,
+          as we use the same css variables ("--inovex-elements-n-9"). Need to test if this is a problem.
+  */
   useEffect(() => {
     void applyPolyfills().then(() => {
       void defineCustomElements(window);
