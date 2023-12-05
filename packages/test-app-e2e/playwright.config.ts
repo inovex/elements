ded448d4055e43fbe4ccd75f4c8c568533f80439
LuisBuehler <luis.buehler@inovex.de>
2023-12-05 14:23:43 +0100

    setup e2e test setup

--- packages/test-app-e2e/playwright.config.ts
+++ packages/test-app-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn nx serve playwright-e2e',
+    command: 'yarn nx serve test-app',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
--- packages/test-app-e2e/src/example.spec.ts
+++ packages/test-app-e2e/src/example.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/components/ino-segment-button.html');
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  expect(await page.locator('ino-segment-button').isVisible());
 });
