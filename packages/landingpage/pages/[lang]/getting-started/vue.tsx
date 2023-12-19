436b5a571cea516d73c61c3ed3824fcc8833fa8d
Tobias Heim Galindo <tobisellfy@gmail.com>
2023-12-19 10:32:27 +0100

    refactor: scope getting started guides css to avoid patterns styling issues

--- packages/landingpage/pages/[lang]/getting-started/vue.tsx
+++ packages/landingpage/pages/[lang]/getting-started/vue.tsx
@@ -9,14 +9,20 @@ import {
 } from 'utils/context/staticPaths';
 import { Framework } from 'utils/frameworks';
 
+import styles from './guide.module.scss';
+
 const VueGuidePage = () => {
   return (
-    <Layout
-      framework={Framework.VUE}
-      sandboxUrl={'https://codesandbox.io/s/github/inovex/elements-example-vue'}
-    >
-      <VueGuide />
-    </Layout>
+    <div className={styles['mdx-content']}>
+      <Layout
+        framework={Framework.VUE}
+        sandboxUrl={
+          'https://codesandbox.io/s/github/inovex/elements-example-vue'
+        }
+      >
+        <VueGuide />
+      </Layout>
+    </div>
   );
 };
 
