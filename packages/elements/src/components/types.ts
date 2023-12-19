df96dd4d8e7dc41ee955bfb60532d151f25e5aa4
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-12-19 09:39:22 +0100

    fix: formatting stuff

--- packages/elements/src/components/types.ts
+++ packages/elements/src/components/types.ts
@@ -1,7 +1,5 @@
 export type ButtonType = 'button' | 'submit' | 'reset';
 export type ChipSurface = 'solid' | 'outline';
-export type ColorScheme = 'primary' | 'warning' | 'error';
-export type DatepickerType = 'date' | 'datetime' | 'range' | 'time';
 export type HorizontalLocation = 'left' | 'right';
 export type ImageDecodingTypes = 'async' | 'auto' | 'sync';
 export type Locations = HorizontalLocation | VerticalLocation;
@@ -9,7 +7,6 @@ export type NavDrawerAnchor = 'left' | 'right';
 export type NavDrawerVariant = 'docked' | 'dismissible' | 'modal' | 'mobile';
 export type SnackbarType = 'info' | 'success' | 'error';
 export type SpinnerType = 'tile' | 'bounce' | 'circle';
-export type SurfaceType = 'filled' | 'outlined' | 'text';
 export type TooltipTrigger =
   | 'mouseenter'
   | 'mouseenter focus'
@@ -30,7 +27,7 @@ export enum ViewMode {
   MARKDOWN = 'markdown',
   PREVIEW = 'preview',
   READONLY = 'readonly',
-};
+}
 export type ViewModeUnion = `${ViewMode}`;
 export type VerticalLocation = 'top' | 'bottom';
 export type InputType =
@@ -54,11 +51,10 @@ export type KeyValue = {
   key: string;
   value: string;
 };
-export type HeaderSize = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 export type NavDrawerLabels = {
   content: string;
   footer: string;
   toggleBtn: string;
 };
 export type TippyThemes = 'light' | 'dark' | 'primary';
-export type Variants = 'filled' | 'outlined' | 'text';
+export type ButtonVariants = 'filled' | 'outlined' | 'text';
