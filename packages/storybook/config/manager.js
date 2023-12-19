4139d50ba77c9763b3d32c778d1578ec5ada1818
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2023-12-19 11:02:36 +0100

    refactor(storybook): update manager

--- packages/storybook/config/manager.js
+++ packages/storybook/config/manager.js
@@ -1,9 +1,13 @@
 // configures Storybook's "manager" UI that wraps the preview, and also configures addons panel
-import { addons } from '@storybook/addons';
+import { addons } from '@storybook/manager-api';
 import theme from './theme';
 
 addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
   panelPosition: 'bottom',
+  showToolbar: true,
   theme,
   sidebar: {
     showRoots: true,
