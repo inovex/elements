cc0fc4f6fa4722949be2312d74ab3e34120a56ac
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2023-12-21 15:14:34 +0100

    fix: fix linting & format errors

--- packages/elements/output-targets/angular.ts
+++ packages/elements/output-targets/angular.ts
@@ -10,5 +10,5 @@ export default angularOutputTarget({
   componentCorePackage: '@inovex.de/elements',
   outputType: 'component',
   directivesProxyFile: `${angularTargetPath}/proxies.ts`,
-  directivesArrayFile: `${angularTargetPath}/proxies-list.ts`
+  directivesArrayFile: `${angularTargetPath}/proxies-list.ts`,
 });
