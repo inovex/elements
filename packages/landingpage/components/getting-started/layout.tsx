813721f7fe02028f118db4623652782a847bbfbf
André Ehlert <andre.ehlert@inovex.de>
2023-12-22 13:14:10 +0100

    feat(landingpage|ino-nav-menu) integrated ino-nav-menu in landing page

--- packages/landingpage/components/getting-started/layout.tsx
+++ packages/landingpage/components/getting-started/layout.tsx
@@ -1,12 +1,13 @@
-import { InoSegmentButton, InoSegmentGroup, InoButton } from '@elements';
+import { InoButton, InoSegmentButton, InoSegmentGroup } from '@elements';
+import { InoNavMenu } from '@inovex.de/elements-react';
 import Page from 'components/layout/page';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { Supported_Locales } from 'translations/config';
 import { Framework, NameByFramework } from 'utils/frameworks';
 import { MainRoutes } from 'utils/routes';
 import styles from './layout.module.scss';
-import NavigationMenu from './navigationMenu';
 
 interface Props {
   children: React.ReactNode;
@@ -23,6 +24,8 @@ const SANDBOX_MAP: { [key in Framework]?: string } = {
 const Layout = ({ children, framework, sandboxUrl }: Props) => {
   const { push } = useRouter();
   const frameworkName = NameByFramework[framework];
+
+  const [activeSection, setActiveSection] = useState<string | null>(null);
 
   return (
     <Page title={['Getting Started', frameworkName]}>
@@ -45,7 +48,13 @@ const Layout = ({ children, framework, sandboxUrl }: Props) => {
       </div>
       <div className={styles.container}>
         <article>{children}</article>
-        <NavigationMenu title={frameworkName.toUpperCase() + ' GUIDE'} />
+        <aside>
+          <InoNavMenu
+            menuTitle={(frameworkName + ' guide').toUpperCase()}
+            activeSection={activeSection}
+            onActiveSectionChanged={(e) => setActiveSection(e.detail)}
+          />
+        </aside>
         <div className={styles.sandbox}>
           <h2>Looking for more resources?</h2>
           <p className="title-s">
