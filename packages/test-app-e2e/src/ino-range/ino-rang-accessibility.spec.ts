171daef02761715116fd8272dc40998a70d5b4f0
LuisBuehler <luis.buehler@inovex.de>
2024-01-01 14:37:00 +0100

    accessibility beispiele

---
+++ packages/test-app-e2e/src/ino-range/ino-rang-accessibility.spec.ts
@@ -0,0 +1,40 @@
+import AxeBuilder from '@axe-core/playwright';
+import { test, expect } from '@playwright/test';
+
+test('should not have any accessibility violations outside of rules with known issues', async ({
+  page,
+}) => {
+  await page.goto(
+    '/components/ino-segment-button/ino-segment-button-visual.html',
+  );
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .disableRules([
+      'document-title',
+      'html-has-lang',
+      'landmark-one-main',
+      'page-has-heading-one',
+      'landmark-no-duplicate-banner',
+      'landmark-no-duplicate-contentinfo', //there are more landmark tags
+      'landmark-no-duplicate-main',
+      'meta-viewport',
+      'region',
+    ])
+    .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+test('should not have any accessibility violations for AA rules', async ({
+  page,
+}) => {
+  await page.goto(
+    '/components/ino-segment-button/ino-segment-button-visual.html',
+  );
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .withTags(['wcag2aa'])
+    .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
