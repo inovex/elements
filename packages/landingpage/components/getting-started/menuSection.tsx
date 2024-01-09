c8fd9bef601b136fe8e8a3638c83ac60ac53ac4c
Tobias Heim Galindo <tobisellfy@gmail.com>
2024-01-09 12:33:36 +0100

    feat: add clickable menu sections with smooth scrolling

--- packages/landingpage/components/getting-started/menuSection.tsx
+++ packages/landingpage/components/getting-started/menuSection.tsx
@@ -4,6 +4,7 @@ import 'utils/stringExtensions';
 type MenuSectionProps = {
   title: string;
   children: ReactNode;
+  includeInNavigationMenu?: boolean;
 };
 
 /**
@@ -13,9 +14,33 @@ type MenuSectionProps = {
  * @param children The content to render within the section.
  */
 function MenuSection({ title, children }: MenuSectionProps) {
+  const id = title.toCamelCase();
+
+  const handleMenuSectionClick = (event: React.MouseEvent<HTMLHeadingElement>) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(id);
+    if (!targetElement) return;
+
+    const headerOffset = 80;
+    const elementPosition = targetElement.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+    // Change the URL (because we're preventing the default anchor click behavior)
+    const newUrl = `${window.location.origin}${window.location.pathname}#${id}`;
+    window.history.pushState(null, '', newUrl);
+
+    // Using window.scrollTo() instead of element.scrollIntoView() because the latter doesn't support offsets
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <section data-menu-section id={title.toCamelCase()}>
-      <h2>{title}</h2>
+    <section data-menu-section id={id}>
+      <h2 onClick={handleMenuSectionClick} style={{ cursor: 'pointer' }}>
+        {title}
+      </h2>
       {children}
     </section>
   );
