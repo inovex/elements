6e3e1abe197c1c06d25f73c0af04763471b314b6
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2024-01-10 13:37:12 +0100

    refactor(storybook|ino-avatar): update stories

--- packages/storybook/src/stories/ino-avatar/ino-avatar.stories.ts
+++ packages/storybook/src/stories/ino-avatar/ino-avatar.stories.ts
@@ -2,24 +2,22 @@ import { Meta } from '@storybook/web-components';
 import { Components } from '@inovex.de/elements';
 import { useEffect } from '@storybook/preview-api';
 import { html } from 'lit-html';
-import { TemplateGenerator } from '../template-generator';
-import { decorateStoryWithClass } from '../utils';
+import Story from '../StoryWrapper';
 import './ino-avatar.scss';
 
 import avatarImg from '../../assets/images/avatar.jpg';
 
-export default {
+const Playground = {
   title: 'Graphic/<ino-avatar>',
   component: 'ino-avatar',
   decorators: [
-    (story) => decorateStoryWithClass(story, 'story-avatar'),
     (story) => {
       useEffect(() => {
         const handleCheckedChange = (e) => {
           const checkbox: HTMLInoCheckboxElement = e.target;
           const parentDiv = checkbox.closest('div');
           const avatar: HTMLInoAvatarElement =
-            parentDiv.querySelector('ino-avatar');
+            parentDiv?.querySelector('ino-avatar');
           checkbox.checked = e.detail;
           avatar.setAttribute('loading', checkbox.checked);
           if (checkbox.indeterminate) {
@@ -39,6 +37,19 @@ export default {
       return story();
     },
   ],
+  render: (args) => html`
+    <ino-avatar
+      initials="${args.initials}"
+      interactive="${args.interactive}"
+      variant="${args.variant}"
+      src="${args.src}"
+      color-secondary="${args.colorSecondary}"
+      a11y-label="${args.a11yLabel}"
+      alt="${args.alt}"
+      .loading="${args.loading}"
+    >
+  </ino-avatar>
+`,
   args: {
     initials: 'JD',
     interactive: false,
@@ -51,57 +62,66 @@ export default {
   },
 } as Meta<Components.InoAvatar>;
 
-// The basic template for the avatar component
-const template = new TemplateGenerator<Components.InoAvatar>(
-  'ino-avatar',
-  (args) => html`
-    <ino-avatar
-      initials="${args.initials}"
-      interactive="${args.interactive}"
-      variant="${args.variant}"
-      src="${args.src}"
-      color-secondary="${args.colorSecondary}"
-      a11y-label="${args.a11yLabel}"
-      alt="${args.alt}"
-      .loading="${args.loading}"
-    >
-    </ino-avatar>
-  `,
-);
+export const Interactive = Story({
+  ...Playground,
+  docsFromProperty: 'interactive',
+  args: {
+    interactive: true
+  }
+});
 
-export const Playground = template.generatePlaygroundStory();
+export const ColorSecondary = Story({
+  ...Playground,
+  docsFromProperty: 'colorSecondary',
+  args: {
+    colorSecondary: true
+  }
+});
 
-export const Interactive = template.generateStoryForProp('interactive', true);
-export const ColorSecondary = template.generateStoryForProp(
-  'colorSecondary',
-  true,
-);
-export const VariantDashed = template.generateStoryForProp('variant', 'dashed');
-export const VariantDashedInteractive = template.generateStoryForProp(
-  'variant',
-  'dashed',
-  { interactive: true },
-);
+export const VariantDashed = Story({
+  ...Playground,
+  docsFromProperty: 'variant',
+  args: {
+    variant: 'dashed'
+  }
+});
+
+export const VariantDashedInteractive = Story({
+  ...Playground,
+  docsFromProperty: 'variant',
+  args: {
+    variant: 'dashed',
+    interactive: true
+  }
+});
+
 /*
  The initials of the avatar if the `src` is not defined or `img` fails to load.
  */
-export const Initials = () => html`
-  <ino-avatar initials="JD" interactive="false" variant="solid" src="">
-  </ino-avatar>
-`;
+export const Initials = Story({
+  ...Playground,
+  render: () => html`
+    <ino-avatar 
+      initials="JD" 
+      interactive="false" 
+      variant="solid" 
+      src="">
+    </ino-avatar>
+  `
+});
 
-/*
+/**
  Add icons to the ino-avatar element by adding a `slot` and an `icon`.
  */
-export const WithIcon = () => {
-  const args = {
+export const WithIcon = Story({
+  ...Playground,
+  args: {
     initials: 'JD',
     interactive: false,
     variant: 'solid',
     src: avatarImg,
-  };
-
-  return html`
+  },
+  render: (args) => html`
     <ino-avatar
       initials="${args.initials}"
       interactive="${args.interactive}"
@@ -114,11 +134,13 @@ export const WithIcon = () => {
         icon="checkmark"
       ></ino-icon>
     </ino-avatar>
-  `;
-};
+  `,
+})
 
-export const LoadingAvatar = (args: Components.InoAvatar) => {
-  return html`
+export const LoadingAvatar = Story({
+  ...Playground,
+  docsFromProperty: 'loading',
+  render: (args) => html`
     <div class="avatar-loading-container">
       <ino-avatar
         initials="${args.initials}"
@@ -135,5 +157,7 @@ export const LoadingAvatar = (args: Components.InoAvatar) => {
         >Loading</ino-checkbox
       >
     </div>
-  `;
-};
+  `
+})
+
+export default Playground;
