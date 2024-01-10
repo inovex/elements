f6e0ccb86a8b7330a6d718b2d22cd6b26c653e94
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2024-01-10 13:27:04 +0100

    refactor(storybook|ino-fab): update stories

--- packages/storybook/src/stories/ino-fab/ino-fab.stories.ts
+++ packages/storybook/src/stories/ino-fab/ino-fab.stories.ts
@@ -1,15 +1,25 @@
-import { Components } from '@inovex.de/elements';
 import { Meta } from '@storybook/web-components';
+import { Components } from '@inovex.de/elements';
 import { html } from 'lit-html';
-import { TemplateGenerator } from '../template-generator';
-import { decorateStoryWithClass, withIconControl } from '../utils';
-
+import Story from '../StoryWrapper';
 import './ino-fab.scss';
 
-export default {
+const Playground = {
   title: 'Buttons/<ino-fab>',
   component: 'ino-fab',
-  decorators: [(story) => decorateStoryWithClass(story, 'story-fab')],
+  render: (args) => html`
+    <ino-fab
+      class="customizable-fab"
+      disabled="${args.disabled}"
+      edge-position="${args.edgePosition}"
+      extended="${args.extended}"
+      label="${args.label}"
+      mini="${args.mini}"
+      tooltip-placement="${args.tooltipPlacement}"
+    >
+      <ino-icon slot="icon-leading" icon="${args.icon}" />
+    </ino-fab>
+  `,
   args: {
     disabled: false,
     edgePosition: 'top-left',
@@ -17,6 +27,7 @@ export default {
     label: 'Label',
     mini: false,
     tooltipPlacement: 'right',
+    icon: 'add',
   },
   argTypes: {
     edgePosition: {
@@ -51,29 +62,16 @@ export default {
   },
 } as Meta<Components.InoFab>;
 
-const template = new TemplateGenerator<Components.InoFab>(
-  'ino-fab',
-  (args) => html`
-    <ino-fab
-      class="customizable-fab"
-      disabled="${args.disabled}"
-      edge-position="${args.edgePosition}"
-      extended="${args.extended}"
-      label="${args.label}"
-      mini="${args.mini}"
-      tooltip-placement="${args.tooltipPlacement}"
-    >
-      <ino-icon slot="icon-leading" icon="${args.icon}" />
-    </ino-fab>
-  `,
-);
-
-export const Playground = template.generatePlaygroundStory();
-withIconControl(Playground, 'icon', 'add');
-
-const templateVariants = new TemplateGenerator<Components.InoFab>(
-  'ino-fab',
-  (args) => html`
+/**
+ * In order to achive diffrent variations, use this properties:
+ *
+ * - `mini`: Optional, modifies the FAB to a smaller size
+ * - `extended`: Optional, modifies the FAB to wider size which includes a text label.
+ * - `disabled`: Disables the button
+ */
+export const Variants = Story({
+  ...Playground,
+  render: () => html`
     <div class="ino-fab-variants">
       <ino-fab tooltip-placement="none" label="Default Fab">
         <ino-icon slot="icon-leading" icon="add"></ino-icon>
@@ -89,19 +87,22 @@ const templateVariants = new TemplateGenerator<Components.InoFab>(
       </ino-fab>
     </div>
   `,
-);
-/**
- * In order to achive diffrent variations, use this properties:
- *
- * - `mini`: Optional, modifies the FAB to a smaller size
- * - `extended`: Optional, modifies the FAB to wider size which includes a text label.
- * - `disabled`: Disables the button
- */
-export const Variants = templateVariants.generatePlaygroundStory();
+})
 
-const templateEdgePositions = new TemplateGenerator<Components.InoFab>(
-  'ino-fab',
-  (args) => html`
+/**
+ * In order to achive edges on the fab-button, add `edge-position`.
+ *
+ * Possible values are:
+ *
+ * - `top-right`
+ * - `top-left`
+ * - `bottom-right`
+ * - `bottom-left`
+ * - `none` = `top-left`
+ */
+export const EdgePositions = Story({
+  ...Playground,
+  render: () => html`
     <div class="ino-fab-variants">
       <ino-fab label="top-left" edge-position="top-left">
         <ino-icon slot="icon-leading" icon="add"></ino-icon>
@@ -119,17 +120,7 @@ const templateEdgePositions = new TemplateGenerator<Components.InoFab>(
         <ino-icon slot="icon-leading" icon="add"></ino-icon>
       </ino-fab>
     </div>
-  `,
-);
-/**
- * In order to achive edges on the fab-button, add `edge-position`.
- *
- * Possible values are:
- *
- * - `top-right`
- * - `top-left`
- * - `bottom-right`
- * - `bottom-left`
- * - `none` = `top-left`
- */
-export const EdgePositions = templateEdgePositions.generatePlaygroundStory();
+  `
+})
+
+export default Playground;
