dbf736b1cd6e8d512208c40393c661db6c072542
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2024-01-15 19:21:10 +0100

    refactor(storybook|ino-segment-group): update stories

--- packages/storybook/src/stories/ino-segment-group/ino-segment-group.stories.ts
+++ packages/storybook/src/stories/ino-segment-group/ino-segment-group.stories.ts
@@ -1,19 +1,33 @@
-import { Components } from '@inovex.de/elements';
 import { Meta } from '@storybook/web-components';
+import { Components } from '@inovex.de/elements';
 import { html } from 'lit-html';
-import { TemplateGenerator } from '../template-generator';
-import { decorateStoryWithClass } from '../utils';
+import Story from '../StoryWrapper';
 import './ino-segment-group.scss';
 
-export default {
+const checkedChangeHandler = ({ detail, target }: CustomEvent<string>) =>
+  ((target as HTMLInoSegmentGroupElement).value = detail);
+
+const InoSegmentGroupMeta = {
   title: 'Buttons/ino-segment-group',
   component: 'ino-segment-group',
-  decorators: [(story) => decorateStoryWithClass(story, 'story-segment-group')],
   parameters: {
     actions: {
       handles: ['checkedChange'],
     },
   },
+  render: (args) => html`
+    <ino-segment-group
+      id="segment-grp"
+      name="${args.name}"
+      value="${args.value}"
+      @valueChange="${checkedChangeHandler}"
+    >
+      <ino-segment-button value="opt-1">Option 1</ino-segment-button>
+      <ino-segment-button value="opt-2">Option 2</ino-segment-button>
+      <ino-segment-button value="opt-3">Option 3</ino-segment-button>
+      <ino-segment-button value="opt-4">Option 4</ino-segment-button>
+    </ino-segment-group>
+  `,
   argTypes: {
     value: {
       control: {
@@ -28,24 +42,8 @@ export default {
   },
 } as Meta<Components.InoSegmentGroup>;
 
-const checkedChangeHandler = ({ detail, target }: CustomEvent<string>) =>
-  ((target as HTMLInoSegmentGroupElement).value = detail);
+export default InoSegmentGroupMeta;
 
-const template = new TemplateGenerator<Components.InoSegmentGroup>(
-  'ino-segment-group',
-  (args) => html`
-    <ino-segment-group
-      id="segment-grp"
-      name="${args.name}"
-      value="${args.value}"
-      @valueChange="${checkedChangeHandler}"
-    >
-      <ino-segment-button value="opt-1">Option 1</ino-segment-button>
-      <ino-segment-button value="opt-2">Option 2</ino-segment-button>
-      <ino-segment-button value="opt-3">Option 3</ino-segment-button>
-      <ino-segment-button value="opt-4">Option 4</ino-segment-button>
-    </ino-segment-group>
-  `,
-);
-
-export const Playground = template.generatePlaygroundStory();
+export const Default = Story({
+  ...InoSegmentGroupMeta,
+});
