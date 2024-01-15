9f93888b35dd3d8fcdebcdb097d40b591ae2bfb3
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2024-01-15 11:46:21 +0100

    refactor(storybook|ino-list-divider): update stories

--- packages/storybook/src/stories/ino-list-divider/ino-list-divider.stories.ts
+++ packages/storybook/src/stories/ino-list-divider/ino-list-divider.stories.ts
@@ -1,22 +1,12 @@
 import { Meta } from '@storybook/web-components';
-import { html } from 'lit-html';
-import { TemplateGenerator } from '../template-generator';
 import { Components } from '@inovex.de/elements';
+import { html } from 'lit-html';
+import Story from '../StoryWrapper';
 
-export default {
+const InoListDividerMeta = {
   title: 'Structure/ino-list-divider',
   component: 'ino-list-divider',
-  args: {
-    inset: false,
-    betweenLists: false,
-    padded: false,
-    slot: false,
-  },
-} as Meta<Components.InoListDivider>;
-
-const template = new TemplateGenerator<Components.InoListDivider>(
-  'ino-list-divider',
-  (args) => html`
+  render: (args) => html`
     <ino-list>
       <ino-list-item text="Item 2"></ino-list-item>
       <ino-list-divider
@@ -27,13 +17,32 @@ const template = new TemplateGenerator<Components.InoListDivider>(
       <ino-list-item text="Item 3"></ino-list-item>
     </ino-list>
   `,
-);
-export const Playground = template.generatePlaygroundStory();
-export const Padded = template.generateStoryForProp('padded', true);
+  args: {
+    inset: false,
+    betweenLists: false,
+    padded: false,
+    slot: false,
+  },
+} as Meta<Components.InoListDivider>;
 
-const templateInsert = new TemplateGenerator<Components.InoListDivider>(
-  'ino-list-divider',
-  (args) => html`
+export default InoListDividerMeta;
+
+export const Default = Story({
+  ...InoListDividerMeta,
+});
+
+export const Padded = Story({
+  ...Default,
+  docsFromProperty: 'padded',
+  args: {
+    padded: true,
+  }
+});
+
+export const Inset = Story({
+  ...Default,
+  docsFromProperty: 'inset',
+  render: (args) => html`
     <ino-list>
       <ino-list-item text="Add to favorites">
         <ino-icon slot="leading" icon="favorite"></ino-icon>
@@ -47,26 +56,29 @@ const templateInsert = new TemplateGenerator<Components.InoListDivider>(
       </ino-list-item>
     </ino-list>
   `,
-);
-export const Insert = templateInsert.generateStoryForProp('inset', true);
+  args: {
+    inset: true,
+  }
+});
 
-const templateBetweenLists = new TemplateGenerator<Components.InoListDivider>(
-  'ino-list-divider',
-  (args) => html`
+export const BetweenLists = Story({
+  ...Default,
+  docsFromProperty: 'betweenLists',
+  render: (args) => html`
     <ino-list>
-      <ino-list-item text="List 1 - item 1"></ino-list-item>
-      <ino-list-item text="List 1 - item 2"></ino-list-item>
-      <ino-list-item text="List 1 - item 3"></ino-list-item>
-    </ino-list>
-    <ino-list-divider between-lists="${args.betweenLists}"></ino-list-divider>
-    <ino-list>
-      <ino-list-item text="List 2 - item 1"></ino-list-item>
-      <ino-list-item text="List 2 - item 2"></ino-list-item>
-      <ino-list-item text="List 2 - item 3"></ino-list-item>
+      <ino-list-item text="Add to favorites">
+        <ino-icon slot="leading" icon="favorite"></ino-icon>
+      </ino-list-item>
+      <ino-list-divider
+        inset="${args.inset}"
+        between-lists="${args.betweenLists}"
+      ></ino-list-divider>
+      <ino-list-item text="Star it!">
+        <ino-icon slot="leading" icon="star"></ino-icon>
+      </ino-list-item>
     </ino-list>
   `,
-);
-export const BetweenLists = templateBetweenLists.generateStoryForProp(
-  'betweenLists',
-  true,
-);
+  args: {
+    betweenLists: true,
+  }
+});
