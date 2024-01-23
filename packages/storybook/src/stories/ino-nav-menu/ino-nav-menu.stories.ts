cff1c4d6da01d1b0eee3a95cf478c274134d834f
MariaLStefan <103122411+MariaLStefan@users.noreply.github.com>
2024-01-23 18:36:24 +0100

    refactor(storybook|ino-nav-menu): add stories for props - add max-height to every story to see stickyness of nav

--- packages/storybook/src/stories/ino-nav-menu/ino-nav-menu.stories.ts
+++ packages/storybook/src/stories/ino-nav-menu/ino-nav-menu.stories.ts
@@ -5,12 +5,14 @@ import { TemplateGenerator } from '../template-generator';
 import { decorateStoryWithClass } from '../utils';
 import './ino-nav-menu.scss';
 
+const sections = ['Section 1', 'Section 2', 'Section 3'];
+
 export default {
   title: `Structure/ino-nav-menu`,
   component: 'ino-nav-menu',
   decorators: [(story) => decorateStoryWithClass(story, 'story-ino-nav-menu')],
   args: {
-    sections: ['Section 1', 'Section 2', 'Section 3'],
+    sections: sections,
     activeSection: 'section-1',
     menuTitle: 'Sections',
   },
@@ -19,59 +21,40 @@ export default {
 const activeSectionChanged = (x: CustomEvent<string>) =>
   console.log('active section changed: ' + x.detail);
 
+const renderSection = (name: string) => {
+  return html`<ino-nav-menu-section section-name="${name}">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </ino-nav-menu-section>`
+}
+
 const template = new TemplateGenerator<Components.InoNavMenu>(
   'ino-nav-menu',
   (args) => html`
-    <div class="sections">
-      <ino-nav-menu-section section-name="Section 1">
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
-      </ino-nav-menu-section>
-      <ino-nav-menu-section section-name="Section 2">
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
-      </ino-nav-menu-section>
-      <ino-nav-menu-section section-name="Section 3">
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
-      </ino-nav-menu-section>
-    </div>
-    <aside>
-      <ino-nav-menu
-        menu-title="${args.menuTitle}"
-        .sections="${args.sections}"
-        active-section="${args.activeSection}"
-        @activeSectionChanged="${activeSectionChanged}"
-      />
-    </aside>
-  `,
+  <div class="sections">
+    ${args.sections?.map((sectionName) => renderSection(sectionName))}
+  </div>
+  <aside>
+    <ino-nav-menu
+      menu-title="${args.menuTitle}"
+      .sections="${args.sections}"
+      active-section="${args.activeSection}"
+      @activeSectionChanged="${activeSectionChanged}"
+    />
+  </aside>
+`,
 );
 
 export const Playground = template.generatePlaygroundStory();
+export const Sections = template.generateStoryForProp('sections', sections);
+export const ActiveSection = template.generateStoryForProp('activeSection', 'section-2');
+export const MenuTitle = template.generateStoryForProp('menuTitle', 'Sticky navigation');
