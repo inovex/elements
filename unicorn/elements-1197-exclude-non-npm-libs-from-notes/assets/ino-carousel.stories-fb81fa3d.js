66b50b6336fe3cb5058c5fbeaedcda2003141cca
BenPag <BenPag@users.noreply.github.com>
2024-01-30 07:34:59 +0000

    Deploying unicorn elements-1197-exclude-non-npm-libs-from-notes 🦄

---
+++ unicorn/elements-1197-exclude-non-npm-libs-from-notes/assets/ino-carousel.stories-fb81fa3d.js
@@ -0,0 +1,32 @@
+import{x as f}from"./lit-html-b5e645d2.js";import{S as a}from"./StoryWrapper-3f0c1834.js";import{l as g,m as h,n as y}from"./nidaros-83a5e61c.js";import"./elements-stencil-docs-997710dd.js";const $=e=>{const v=e.target;v.value=e.detail},S={title:"Graphic/ino-carousel",component:"ino-carousel",render:e=>f`
+    <div class="ino-carousel-example">
+      <ino-carousel
+        class="customizable-carousel"
+        value="${e.value}"
+        autoplay="${e.autoplay}"
+        hide-buttons="${e.hideButtons}"
+        infinite="${e.infinite}"
+        intermission="${e.intermission}"
+        reverse="${e.reverse}"
+        @valueChange="${$}"
+      >
+        <ino-carousel-slide src=${g} value="0"></ino-carousel-slide>
+        <ino-carousel-slide src=${h} value="1"></ino-carousel-slide>
+        <ino-carousel-slide src=${y} value="2"></ino-carousel-slide>
+      </ino-carousel>
+    </div>
+  `,args:{value:"0",autoplay:!0,hideButtons:!1,infinite:!0,intermission:2e3,reverse:!1}},r=a({...S}),o=a({...r,docsFromProperty:"hideButtons",args:{hideButtons:!0}}),s=a({...r,docsFromProperty:"reverse",args:{reverse:!0}});var t,n,i;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`Story({
+  ...InoCarouselMeta
+})`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var u,l,c;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'hideButtons',
+  args: {
+    hideButtons: true
+  }
+})`,...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var d,m,p;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'reverse',
+  args: {
+    reverse: true
+  }
+})`,...(p=(m=s.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const D=["Default","HideButtons","Reverse"];export{r as Default,o as HideButtons,s as Reverse,D as __namedExportsOrder,S as default};
---
+++ unicorn/elements-1197-exclude-non-npm-libs-from-notes/assets/ino-tab.stories-8a5c36eb.js
@@ -0,0 +1,24 @@
+import{x as y}from"./lit-html-b5e645d2.js";import{g as f}from"./utils-784729f7.js";import{S as a}from"./StoryWrapper-3f0c1834.js";import"./elements-stencil-docs-997710dd.js";const S={title:"Structure/ino-tab",component:"ino-tab",render:o=>y`
+    <ino-tab
+      icon="${o.icon}"
+      indicator-content-width="${o.indicatorContentWidth}"
+      stacked="${o.stacked}"
+      a11y-controls="${o.a11yControls}"
+    >
+      Label
+    </ino-tab>
+  `,argTypes:{icon:{control:{type:"select"},options:f()}},args:{indicatorContentWidth:!1,stacked:!1,icon:"info",a11yControls:"panel-id"}},t=a({...S}),r=a({...t,docsFromProperty:"stacked",args:{stacked:!0}}),e=a({...t,docsFromProperty:"indicatorContentWidth",args:{indicatorContentWidth:!0}});var n,s,c;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`Story({
+  ...InoTabMeta
+})`,...(c=(s=t.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var i,d,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'stacked',
+  args: {
+    stacked: true
+  }
+})`,...(p=(d=r.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var m,u,l;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'indicatorContentWidth',
+  args: {
+    indicatorContentWidth: true
+  }
+})`,...(l=(u=e.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};const W=["Default","Stacked","IndicatorContentWidth"];export{t as Default,e as IndicatorContentWidth,r as Stacked,W as __namedExportsOrder,S as default};
