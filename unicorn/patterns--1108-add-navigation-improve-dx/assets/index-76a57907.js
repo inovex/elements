5460bda6c9684e2d641fe04641b64ea14554edcf
TobiasHeimGalindo <TobiasHeimGalindo@users.noreply.github.com>
2024-01-31 11:14:37 +0000

    Deploying unicorn patterns--1108-add-navigation-improve-dx 🦄

---
+++ unicorn/patterns--1108-add-navigation-improve-dx/assets/index-76a57907.js
@@ -0,0 +1,1 @@
+import{M as a,a as n,u as M,w as D}from"./index-1f0487e7.js";import"./index-cdfeeac4.js";import"./iframe-d46f4931.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-de833af9.js";import"./index-d2c24ff6.js";import"./index-356e4a49.js";export{a as MDXContext,n as MDXProvider,M as useMDXComponents,D as withMDXComponents};
