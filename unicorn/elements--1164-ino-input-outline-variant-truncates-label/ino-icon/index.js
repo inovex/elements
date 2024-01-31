b10ccfb08d615348543b905db54cf1176308a38d
MariaLStefan <MariaLStefan@users.noreply.github.com>
2024-01-31 09:22:11 +0000

    Deploying unicorn elements--1164-ino-input-outline-variant-truncates-label 🦄

--- unicorn/elements--1164-ino-input-outline-variant-truncates-label/ino-icon/index.js
+++ unicorn/elements--1164-ino-input-outline-variant-truncates-label/ino-icon/index.js
@@ -1,5 +1,1 @@
-require.context(
-  '!!file-loader?name=[name].[ext]&outputPath=svg!./',
-  false,
-  /.svg$/,
-);
+require.context('!!file-loader?name=[name].[ext]&outputPath=svg!./', false, /.svg$/);
