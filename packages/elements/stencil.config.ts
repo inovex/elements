df1319c826c558bf873ab5ef83df352bcee88518
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-01-31 10:19:38 +0100

    wip

--- packages/elements/stencil.config.ts
+++ packages/elements/stencil.config.ts
@@ -43,6 +43,5 @@ export const config: Config = {
       includePaths: ['../../node_modules'].map((d) => join(__dirname, d)),
     }),
   ],
-  maxConcurrentWorkers: 4,
   preamble: 'Crafted with ❤ by inovex GmbH',
 };
