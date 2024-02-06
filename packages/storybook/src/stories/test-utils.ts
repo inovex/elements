03e19d4d405d38603493d00ec5e114e7c3c3aaa6
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2024-02-06 10:37:06 +0100

    chore: fix format

--- packages/storybook/src/stories/test-utils.ts
+++ packages/storybook/src/stories/test-utils.ts
@@ -6,7 +6,6 @@ export async function goToStory(
   page: Page,
   storyDescription: StoryDescription,
 ) {
-
   const [category, name, story] = storyDescription;
 
   await page.goto(`/iframe.html?id=${category}-${name}--${story}`);
