b9671ed2d224e11b6a57206373d51acc5792da1e
Jan-Niklas <jan-niklas.voss@inovex.de>
2024-02-06 11:36:49 +0100

    test(storybook): init setup for e2e test (#1239)
    * test(storybook): init setup for e2e test
    
    * test(storybook): wrap goToStory
    
    * test: implement test for ino-checkbox
    
    * chore: formatter
    
    * chore: update configurations
    
    * chore: update configurations
    
    * ci: cache playwright binaries
    
    * ci: cache playwright binaries
    
    * chore: fix review findings
    
    * chore: fix review findings
    
    * chore: fix format

---
+++ packages/storybook/src/stories/ino-checkbox/ino-checkbox.spec.ts
@@ -0,0 +1,30 @@
+import { expect, Locator, test } from '@playwright/test';
+import { goToStory } from '../test-utils';
+
+test.describe('ino-checkbox', () => {
+  let checkbox: Locator;
+
+  test.beforeEach(({ page }) => {
+    checkbox = page.getByRole('checkbox');
+  });
+
+  test('can be toggled properly', async ({ page }) => {
+    await goToStory(page, ['input', 'ino-checkbox', 'default']);
+
+    await expect(checkbox).toBeChecked();
+
+    await checkbox.click();
+
+    await expect(checkbox).not.toBeChecked();
+  });
+
+  test('disabling prevents a change of checked', async ({ page }) => {
+    await goToStory(page, ['input', 'ino-checkbox', 'disabled']);
+
+    await expect(checkbox).toBeChecked();
+    await expect(checkbox).toBeDisabled();
+    await checkbox.click({ force: true });
+
+    await expect(checkbox).toBeChecked();
+  });
+});
