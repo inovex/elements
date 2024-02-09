dcbf7c87a0801d48b0710ad993362f21876d9ccc
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-02-09 09:14:44 +0100

    format file

--- commit-scope-enum.js
+++ commit-scope-enum.js
@@ -6,18 +6,15 @@ const packagesPath = path.join(__dirname, 'packages');
 const packages = fs.readdirSync(packagesPath);
 
 const componentsPath = path.join(packagesPath, 'elements', 'src', 'components');
-const components = fs.readdirSync(componentsPath).filter((p) => p.startsWith('ino-'));
+const components = fs
+  .readdirSync(componentsPath)
+  .filter((p) => p.startsWith('ino-'));
 
 const packageComponentList = packages
   .filter((pkg) => pkg.match(/storybook$|elements(-[a-z]*)?$/))
   .map((pkg) => components.map((cmp) => `${pkg}${separator}${cmp}`))
   .flat();
 
-const commitScopes = [
-  ...packages,
-  ...packageComponentList,
-  "'*'",
-  'deps',
-];
+const commitScopes = [...packages, ...packageComponentList, "'*'", 'deps'];
 
 fs.writeFileSync('commit-scopes.txt', commitScopes.join('\n') + '\n');
