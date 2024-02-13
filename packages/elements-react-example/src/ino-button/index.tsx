ab53f5b055498782aa26e2f106a07a85afa9aa8d
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-02-13 09:32:12 +0100

    revert changes in react example

--- packages/elements-react-example/src/ino-button/index.tsx
+++ packages/elements-react-example/src/ino-button/index.tsx
@@ -1,27 +1,12 @@
 import * as React from 'react';
-import { InoButton, InoIcon, InoTooltip, InoCheckbox } from '../shared/InovexElements';
-import { useState } from 'react';
+import { InoButton, InoIcon } from '../shared/InovexElements';
 
-export const Button = () => {
-  const [check, setCheck] = useState(false);
-
-  return (
-    <div className="App">
-      <InoCheckbox
-        onCheckedChange={(event) => setCheck(event.detail)}
-        checked={check}
-      >MyLabel</InoCheckbox>
-      <br />
-      <p id="sss">Lorem ipsum text...</p>
-      {check && (
-        <InoTooltip for="sss">Goo</InoTooltip>
-      )}
-      <br />
-      <InoButton id="sss" dense={false} fill="solid" loading={false}>
-        <InoIcon slot="icon-leading" icon="info" />
-        My Button
-        <InoIcon slot="icon-trailing" icon="info" />
-      </InoButton>
-    </div>
-  )
-};
+export const Button = () => (
+  <div className="App">
+    <InoButton dense={false} fill="solid" loading={false}>
+      <InoIcon slot="icon-leading" icon="info" />
+      My Button
+      <InoIcon slot="icon-trailing" icon="info" />
+    </InoButton>
+  </div>
+);
