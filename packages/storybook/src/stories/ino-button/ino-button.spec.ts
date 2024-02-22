bc54faa5ed1421f7e06513a3d9c176a7cb991a52
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2024-02-22 17:04:20 +0100

    refactor: use bounding box function

--- packages/storybook/src/stories/ino-button/ino-button.spec.ts
+++ packages/storybook/src/stories/ino-button/ino-button.spec.ts
@@ -4,17 +4,6 @@ import { goToStory, setAttribute } from '../test-utils';
 test.describe('ino-button', () => {
   let inoButton: Locator;
 
-  async function getButtonDimensions() {
-    const { height, width } = await inoButton
-      .getByRole('button')
-      .evaluate((btn) => window.getComputedStyle(btn));
-
-    return {
-      height: parseInt(height, 10),
-      width: parseInt(width, 10),
-    };
-  }
-
   test.beforeEach(async ({ page }) => {
     await goToStory(page, ['buttons', 'ino-button', 'default']);
     inoButton = page.locator('ino-button');
@@ -23,7 +12,7 @@ test.describe('ino-button', () => {
   test('should keep dimensions if loading state is set', async () => {
     const spinnerEl = inoButton.locator('ino-spinner');
 
-    const { height, width } = await getButtonDimensions();
+    const { height, width } = await inoButton.boundingBox();
 
     // set loading and wait for spinner to show
     await setAttribute(inoButton, 'loading', 'true');
@@ -33,7 +22,7 @@ test.describe('ino-button', () => {
     await setAttribute(inoButton, 'loading', 'false');
     await expect(spinnerEl).toBeHidden();
 
-    const { height: newHeight, width: newWidth } = await getButtonDimensions();
+    const { height: newHeight, width: newWidth } = await inoButton.boundingBox();
 
     expect(Math.abs(newHeight - height)).toBeLessThanOrEqual(1);
     expect(Math.abs(newWidth - width)).toBeLessThanOrEqual(1);
