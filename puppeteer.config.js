7f723ad397d4f98bb83e65c7676c997526a45e8e
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-02-22 13:03:35 +0100

    format fix

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -7,5 +7,5 @@ module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, 'node_modules', '.cache', 'puppeteer'),
   skipChromeHeadlessShellDownload: true,
-  downloadBaseUrl: 'https://storage.googleapis.com/chrome-for-testing-public'
+  downloadBaseUrl: 'https://storage.googleapis.com/chrome-for-testing-public',
 };
