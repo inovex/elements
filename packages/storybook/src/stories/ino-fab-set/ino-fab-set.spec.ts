497c8a9bfbda9a97bd34ccccc0844e8abecd3891
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-02-26 10:42:50 +0100

    add playwright tests

---
+++ packages/storybook/src/stories/ino-fab-set/ino-fab-set.spec.ts
@@ -0,0 +1,21 @@
+import { expect, Locator, test } from '@playwright/test';
+import { goToStory } from '../test-utils';
+
+test.describe('ino-fab-set', () => {
+  let inoFabSet: Locator;
+
+  test.beforeEach(async ({ page }) => {
+    await goToStory(page, ['buttons', 'ino-fab-set', 'default']);
+    inoFabSet = page.locator('ino-fab-set');
+  });
+
+  test('should not open the dial by click', async () => {
+    const { width, height } = await inoFabSet.boundingBox();
+    await inoFabSet.click();
+    const { width: openWidth, height: openHeight } =
+      await inoFabSet.boundingBox();
+
+    expect(openWidth).toEqual(width);
+    expect(openHeight).toBeGreaterThan(height * 2);
+  });
+});
