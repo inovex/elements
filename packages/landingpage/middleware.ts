de3291cfd06c016d5508c4ab43008c73d72bae63
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-02-27 11:11:12 +0100

    fix public assets

--- packages/landingpage/middleware.ts
+++ packages/landingpage/middleware.ts
@@ -23,16 +23,17 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
+  // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
+  if (
+    [
+      '/manifest.json',
+      '/favicon.ico',
+      '/site.webmanifest',
+      //Your other files in `public`
+    ].includes(pathname) ||
+    pathname.match(/.*\.(png|svg)$/i) !== null
+  )
+    return;
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
