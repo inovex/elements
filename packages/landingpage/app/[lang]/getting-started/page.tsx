df23ec1aedf0b1c56cf3b59164bc19a60cb025cd
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-02-27 14:03:18 +0100

    format:fix

--- packages/landingpage/app/[lang]/getting-started/page.tsx
+++ packages/landingpage/app/[lang]/getting-started/page.tsx
@@ -1,9 +1,11 @@
-import { permanentRedirect  } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 import { i18n } from 'i18n-config';
 import { ParamsWithLang } from 'types/langParam';
 import { MainRoutes, SubRoutes } from 'utils/routes';
 
 export default function Page({ params }: ParamsWithLang) {
   const locale = params.lang ?? i18n.defaultLocale;
-  permanentRedirect(`/${locale}${MainRoutes.GETTING_STARTED}/${SubRoutes.GETTING_STARTED_REACT}`);
+  permanentRedirect(
+    `/${locale}${MainRoutes.GETTING_STARTED}/${SubRoutes.GETTING_STARTED_REACT}`,
+  );
 }
--- packages/landingpage/app/page.tsx
+++ packages/landingpage/app/page.tsx
@@ -1,4 +1,4 @@
-import { permanentRedirect  } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 import { i18n } from 'i18n-config';
 
 export default function Page() {
