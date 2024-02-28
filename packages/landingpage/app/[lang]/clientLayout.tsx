eb1f403308c51979b73e30930a2d944bbf21330a
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-02-28 08:15:51 +0100

    fix elements hydration issue

--- packages/landingpage/app/[lang]/clientLayout.tsx
+++ packages/landingpage/app/[lang]/clientLayout.tsx
@@ -3,7 +3,7 @@
 import '../../styles/globals.css';
 import '@code-hike/mdx/dist/index.css';
 import '../../styles/custom-ch.css';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import UiContextProvider from '../../utils/context/UiContext';
 import { VersionProvider } from '../../utils/context/VersionContext';
 import I18nContextProvider from '../../utils/context/i18nContext';
@@ -23,17 +23,24 @@ export default function ClientLayout({
   lang: Locale;
   translations: any;
 }) {
+  /**
+   * Fix elements hydration error issue
+   * https://nextjs.org/docs/messages/react-hydration-error#solution-1-using-useeffect-to-run-on-the-client-only
+   */
+  const [isClient, setIsClient] = useState(false);
   useEffect(() => {
-    void applyPolyfills().then(() => {
-      void defineCustomElements(window);
-    });
+    (async () => {
+      await applyPolyfills();
+      defineCustomElements(window);
+      setIsClient(true);
+    })();
   }, []);
 
   return (
     <UiContextProvider>
       <VersionProvider>
         <I18nContextProvider lang={lang} translations={translations}>
-          <Layout>{children}</Layout>
+          { isClient && <Layout>{children}</Layout>}
         </I18nContextProvider>
       </VersionProvider>
     </UiContextProvider>
