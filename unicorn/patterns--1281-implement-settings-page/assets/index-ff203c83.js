d68b385f676c3d4c2e1814399f377c64ca0557f4
TobiasHeimGalindo <TobiasHeimGalindo@users.noreply.github.com>
2024-03-08 13:58:38 +0000

    Deploying unicorn patterns--1281-implement-settings-page 🦄

---
+++ unicorn/patterns--1281-implement-settings-page/assets/index-ff203c83.js
@@ -0,0 +1,1 @@
+import{M as a,a as n,u as M,w as D}from"./index-df9d80d4.js";import"./index-911df398.js";import"./iframe-f2134ba0.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-de833af9.js";import"./index-d2c24ff6.js";import"./index-356e4a49.js";export{a as MDXContext,n as MDXProvider,M as useMDXComponents,D as withMDXComponents};
