79caad6c9be8c3367c2075e3f01f5ddfebff30da
janivo <github@jnvoss.de>
2024-03-11 15:04:10 +0000

    Deploy landingpage

---
+++ _next/static/DriLOW-gLXb677YbM7MDZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5ec3602094a37ee4.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
