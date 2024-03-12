0c8862008903dc2a89143ab9ed4a23d57a9a221e
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-03-12 14:17:43 +0100

    refactor(elements/ino-select): migrate stencil e2e tests to spec test (#1307)
    * begin migrating tests
    
    * finish migration

---
+++ packages/storybook/src/stories/ino-select/ino-select.spec.ts
@@ -0,0 +1,56 @@
+import { expect, Locator, test } from '@playwright/test';
+import { goToStory, setAttribute } from '../test-utils';
+
+test.describe('ino-select - Properties', () => {
+  let inoSelect: Locator;
+
+  test.beforeEach(async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-select', 'default']);
+    inoSelect = page.locator('ino-select');
+  });
+
+  test('should render with the disabled property set to true', async () => {
+    await setAttribute(inoSelect, 'disabled', 'true');
+    await inoSelect.click();
+    await expect(inoSelect.locator('li').first()).toBeHidden();
+  });
+
+  test('should render with the required property set to true', async () => {
+    await setAttribute(inoSelect, 'required', 'true');
+    await expect(inoSelect.locator('input[required]')).toBeAttached();
+  });
+
+  test('should render as an outlined element if inoOutlined is true', async () => {
+    const bBoxDefault = await inoSelect.boundingBox();
+    await setAttribute(inoSelect, 'outline', 'true');
+    const bBoxOutline = await inoSelect.boundingBox();
+
+    expect(bBoxOutline.height).toBe(bBoxDefault.height);
+    expect(bBoxOutline.width).toBeGreaterThan(bBoxDefault.width);
+  });
+});
+
+test.describe('ino-select - Form integration', () => {
+  let inoSelect: Locator;
+  let submitBtn: Locator;
+  let notification: Locator;
+
+  test.beforeEach(async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-select', 'form']);
+    inoSelect = page.locator('ino-select');
+    submitBtn = page.locator('ino-button');
+    notification = page.getByText(/Form submitted/i);
+  });
+
+  test('should submit if select is required and value is set', async () => {
+    await inoSelect.click();
+    await inoSelect.locator('ino-option').first().click();
+    await submitBtn.click();
+    await expect(notification).toBeVisible();
+  });
+
+  test('should not submit if select is required and no value is set', async () => {
+    await submitBtn.click();
+    await expect(notification).toBeHidden();
+  });
+});
