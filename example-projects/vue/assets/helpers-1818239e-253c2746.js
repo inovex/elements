0e7dce6151eeb9d752b536e5a490c864f10e035e
janivo <github@jnvoss.de>
2024-03-13 10:56:47 +0000

    Deploy landingpage

--- example-projects/vue/assets/helpers-1818239e-253c2746.js
+++
@@ -1,3 +0,0 @@
-/*!
- * Crafted with ❤ by inovex GmbH
- */function i(n){return n!=null&&n.trim()!==""}const a=(n,u,t,d)=>{let e=n.querySelector("input.aux-input");return e||(e=n.ownerDocument.createElement("input"),e.type="hidden",e.classList.add("aux-input"),n.appendChild(e)),e.disabled=d,e.name=u,e.value=t||"",e};export{i as h,a as r};
