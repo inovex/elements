8dce352c4f8a83c858cdb6fa3882b573882957af
BenPag <BenPag@users.noreply.github.com>
2024-03-14 08:56:14 +0000

    Deploying unicorn elements-migrate-ino-textarea-tests 🦄

--- unicorn/elements-migrate-ino-textarea-tests/assets/DocsRenderer-NNNQARDV-1fbc6bd9.js
+++
@@ -1,1 +0,0 @@
-import{D as a,d}from"./chunk-HLWAVYOI-226a5f36.js";import"./iframe-99d9cc60.js";import"../sb-preview/runtime.js";import"./index-bc7dadb1.js";import"./_commonjsHelpers-de833af9.js";import"./index-d2c24ff6.js";import"./index-356e4a49.js";export{a as DocsRenderer,d as defaultComponents};
---
+++ unicorn/elements-migrate-ino-textarea-tests/assets/ino-menu.stories-b9735ac6.js
@@ -0,0 +1,21 @@
+import{x as l}from"./lit-html-b5e645d2.js";import{S as m}from"./StoryWrapper-48043eab.js";import"./elements-stencil-docs-75fc22e4.js";const p={title:"Structure/ino-menu",component:"ino-menu",render:c=>l`
+    <ino-card disable-elevation>
+      <div class="row" slot="header">
+        <p>Open Menu</p>
+        <ino-icon-button icon="menu">
+          <ino-menu placement="${c.placement}">
+            <ino-list-item tabindex="0" text="Item 1"></ino-list-item>
+            <ino-list-item tabindex="0" text="Item 2"></ino-list-item>
+          </ino-menu>
+        </ino-icon-button>
+      </div>
+    </ino-card>
+  `,args:{placement:"auto"}},e=m({...p}),t=m({...e,docsFromProperty:"placement",args:{placement:"left"}});var n,o,r;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`Story({
+  ...InoMenuMeta
+})`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};var a,s,i;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'placement',
+  args: {
+    placement: 'left'
+  }
+})`,...(i=(s=t.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const x=["Default","Placement"];export{e as Default,t as Placement,x as __namedExportsOrder,p as default};
