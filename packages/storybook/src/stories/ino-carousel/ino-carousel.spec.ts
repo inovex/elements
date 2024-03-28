764145978238f8977fcab4b8717a069a3b0cd500
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2024-03-28 12:31:28 +0100

    chore(ino-carousel): fix flaky test

--- packages/storybook/src/stories/ino-carousel/ino-carousel.spec.ts
+++ packages/storybook/src/stories/ino-carousel/ino-carousel.spec.ts
@@ -3,37 +3,57 @@ import { goToStory, setAttribute } from '../test-utils';
 
 test.describe('ino-carousel', () => {
   let inoCarousel: Locator;
-  let selectedSlide: Locator;
+  let firstSlide: Locator;
+  let secondSlide: Locator;
+  let thirdSlide: Locator;
+  let leftButton: Locator;
+  let rightButton: Locator;
+
+  async function assertSelected(slide: Locator) {
+    await expect(slide).toHaveClass(/ino-carousel-slide--selected/);
+  }
+
+  async function assertUnselected(slide: Locator) {
+    await expect(slide).not.toHaveClass(/ino-carousel-slide--selected/);
+  }
 
   test.beforeEach(async ({ page }) => {
     await goToStory(page, ['Graphic', 'ino-carousel', 'default']);
     inoCarousel = page.locator('ino-carousel');
-    selectedSlide = inoCarousel.locator('.ino-carousel-slide--selected');
+
+    [firstSlide, secondSlide, thirdSlide] = await page
+      .locator('ino-carousel-slide')
+      .all();
+    [leftButton, rightButton] = await page.getByRole('button').all();
+
     await setAttribute(inoCarousel, 'autoplay', 'false');
   });
 
   test('should render with default values', async () => {
-    await expect(
-      inoCarousel.locator('.ino-carousel-slide--selected'),
-    ).toBeVisible();
+    await expect(firstSlide).toBeVisible();
   });
 
   test('should hide the buttons if inoHideButtons is set to true', async () => {
-    await expect(inoCarousel.getByRole('button')).toHaveCount(2);
+    await expect(leftButton).toBeVisible();
+    await expect(rightButton).toBeVisible();
     await setAttribute(inoCarousel, 'hide-buttons', 'true');
-    await expect(inoCarousel.getByRole('button')).toHaveCount(0);
+    await expect(leftButton).toBeHidden();
+    await expect(rightButton).toBeHidden();
   });
 
   test('should change image on arrow click', async () => {
-    const imageBefore = await selectedSlide.innerHTML();
-    const arrowButtons = inoCarousel.getByRole('button');
+    await assertSelected(firstSlide);
+    await assertUnselected(secondSlide);
+    await assertUnselected(thirdSlide);
 
-    await arrowButtons.first().click(); // click left
-    await expect(selectedSlide).toBeVisible();
-    expect(imageBefore).not.toEqual(await selectedSlide.innerHTML());
+    await leftButton.click();
+    await assertUnselected(firstSlide);
+    await assertUnselected(secondSlide);
+    await assertSelected(thirdSlide);
 
-    await arrowButtons.last().click(); // click right
-    await expect(selectedSlide).toBeVisible();
-    expect(imageBefore).toEqual(await selectedSlide.innerHTML());
+    await leftButton.click();
+    await assertUnselected(firstSlide);
+    await assertSelected(secondSlide);
+    await assertUnselected(thirdSlide);
   });
 });
