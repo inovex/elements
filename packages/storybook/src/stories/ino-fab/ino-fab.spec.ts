bd7bce31b5012ebe0b7e65d475d9ab5319d9c2eb
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-04-03 12:13:10 +0200

    reduce flaky tests

--- packages/storybook/src/stories/ino-fab/ino-fab.spec.ts
+++ packages/storybook/src/stories/ino-fab/ino-fab.spec.ts
@@ -1,15 +1,13 @@
-import { expect, Locator, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { goToStory, setAttribute } from '../test-utils';
 
 test.describe('ino-fab', () => {
-  let inoFab: Locator;
-
   test.beforeEach(async ({ page }) => {
     await goToStory(page, ['Buttons', 'ino-fab', 'default']);
-    inoFab = page.locator('ino-fab');
   });
 
-  test('should render with property mini', async () => {
+  test('should render with property mini', async ({ page }) => {
+    const inoFab = page.locator('ino-fab');
     const { width, height } = await inoFab.boundingBox();
     await setAttribute(inoFab, 'mini', 'true');
     const { width: miniWidth, height: miniHeight } = await inoFab.boundingBox();
@@ -18,7 +16,8 @@ test.describe('ino-fab', () => {
     expect(height).toBeGreaterThan(miniHeight);
   });
 
-  test('should render with property extended', async () => {
+  test('should render with property extended', async ({ page }) => {
+    const inoFab = page.locator('ino-fab');
     const { width } = await inoFab.boundingBox();
     await setAttribute(inoFab, 'extended', 'true');
     await inoFab.click();
--- packages/storybook/src/stories/ino-nav-drawer/ino-nav-drawer.spec.ts
+++ packages/storybook/src/stories/ino-nav-drawer/ino-nav-drawer.spec.ts
@@ -1,22 +1,14 @@
-import { expect, Locator, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { goToStory } from '../test-utils';
 
 test.describe('ino-nav-drawer', () => {
-  let expandButton: Locator;
-
-  test.beforeEach(({ page }) => {
-    expandButton = page.locator('button:has-text("Toggle Navigation")');
-  });
-
   test('can be opened properly', async ({ page }) => {
     await goToStory(page, ['Structure', 'ino-nav-drawer', 'default']);
-
     const navDrawer = page.locator('ino-nav-drawer');
+    const expandButton = page.locator('button:has-text("Toggle Navigation")');
 
     await expect(navDrawer).toHaveAttribute('open', '');
-
     await expandButton.click();
-
     await expect(navDrawer).toHaveAttribute('open', 'false');
   });
 });
