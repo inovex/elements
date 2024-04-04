b49299f25d382981aeacd406f47eaac65f0d01eb
BenPag <BenPag@users.noreply.github.com>
2024-04-04 09:26:50 +0000

    Deploying unicorn elements-1258-migrate-ino-switch-tests 🦄

---
+++ unicorn/elements-1258-migrate-ino-switch-tests/assets/10-updating-dependencies.stories-eeeb8ca5.js
@@ -0,0 +1,57 @@
+import"./chunk-HLWAVYOI-6faac1a1.js";import{M as i}from"./index-92b7c7ff.js";import{j as e}from"./jsx-runtime-2746887a.js";import{u as o}from"./index-84431831.js";import"./iframe-03aad2e3.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-de833af9.js";import"./index-d2c24ff6.js";import"./index-356e4a49.js";function r(t){const n=Object.assign({h1:"h1",p:"p",h2:"h2",ol:"ol",li:"li",a:"a",code:"code",ul:"ul",strong:"strong"},o(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Docs/Contributing/Updating Dependencies"}),`
+`,e.jsx(n.h1,{id:"updating-dependencies",children:"Updating Dependencies"}),`
+`,e.jsx(n.p,{children:"In order to get the most mature and stable version, we have to update the dependencies regularly."}),`
+`,e.jsx(n.h2,{id:"how-to-update-stencil-in-the-core-package",children:"How to Update Stencil in the Core Package"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Go to ",e.jsx(n.a,{href:"https://github.com/ionic-team/stencil",target:"_blank",rel:"nofollow noopener noreferrer",children:"https://github.com/ionic-team/stencil"})," and open the ",e.jsx(n.code,{children:"CHANGELOG"}),"."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Get the current installed ",e.jsx(n.code,{children:"@stencil/core"})," version from ",e.jsx(n.code,{children:"packages/elements/package.json"}),"."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Search this version in the CHANGELOG, for example ",e.jsx(n.code,{children:"1.5.0"}),"."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Go to the next minor version, in our example ",e.jsx(n.code,{children:"1.6.0"}),"."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Replace ",e.jsx(n.code,{children:"1.5.0"})," with ",e.jsx(n.code,{children:"1.6.0"})," in the ",e.jsx(n.code,{children:"package.json"})," file."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:"Check for any note about a TypeScript or Jest upgrade between the current and last version and update these packages to the given version if necessary."}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Run ",e.jsx(n.code,{children:"yarn install"})," from within the root of the repo."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Run ",e.jsx(n.code,{children:"yarn start"}),` to check if the build succeeds, Storybook opens in the browser and
+all components are working as expected. Make sure that all tests pass before you continue.`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[`If the build fails with some compiler errors of TypeScript or any type mismatchs, you may
+need to update the packages `,e.jsx(n.code,{children:"typescript"}),", ",e.jsx(n.code,{children:"@types/jest"}),", ",e.jsx(n.code,{children:"jest"}),", and ",e.jsx(n.code,{children:"jest-cli"}),` as well.
+This IS the case when there are some hints in the Stencil `,e.jsx(n.code,{children:"CHANGELOG"})," like:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"typescript: update to typescript 3.7.2"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"jest: bump jest versions to fix types"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["Repeat step 2) - 7) until you reached the latest version of ",e.jsx(n.code,{children:"@stencil/core"}),"."]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"how-to-update-the-material-web-components",children:"How to update the material web components"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:["Visit the ",e.jsx(n.a,{href:"https://github.com/material-components/material-components-web",target:"_blank",rel:"nofollow noopener noreferrer",children:"material-components-web github page"}),`
+and open the `,e.jsx(n.a,{href:"https://github.com/material-components/material-components-web/releases",target:"_blank",rel:"nofollow noopener noreferrer",children:"CHANGELOG"})]}),`
+`,e.jsxs(n.li,{children:["Search for the currently used material-components-web version in the ",e.jsx(n.code,{children:"CHANGELOG"}),", e.g. ",e.jsx(n.code,{children:"5.0.0"})]}),`
+`,e.jsxs(n.li,{children:["If a new version is available, open the ",e.jsx(n.code,{children:"package.json"}),` located in the inovex Elements package directory and replace
+the current version of `,e.jsx(n.strong,{children:"every"})," material component with the next minor version, e.g. ",e.jsx(n.code,{children:"5.1.0"}),"."]}),`
+`,e.jsxs(n.li,{children:["Run ",e.jsx(n.code,{children:"yarn install"})," in the root directory"]}),`
+`,e.jsxs(n.li,{children:["Run ",e.jsx(n.code,{children:"yarn start"})," to check if the build succeeds and the Storybook opens in the browser."]}),`
+`,e.jsxs(n.li,{children:["Go through every component and check the ",e.jsx(n.code,{children:"CHANGELOG"}),` for any changes. Adjust the component accordingly and
+make sure that the component works as intended, the styles have not been altered by the upgrade, and that
+all component tests pass.`]}),`
+`,e.jsx(n.li,{children:"Repeat steps 2) - 6) until you have reached the latest version."}),`
+`]})]})}function c(t={}){const{wrapper:n}=Object.assign({},o(),t.components);return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}const l=()=>{throw new Error("Docs-only story")};l.parameters={docsOnly:!0};const s={title:"Docs/Contributing/Updating Dependencies",tags:["stories-mdx"],includeStories:["__page"]};s.parameters=s.parameters||{};s.parameters.docs={...s.parameters.docs||{},page:c};const y=["__page"];export{y as __namedExportsOrder,l as __page,s as default};
---
+++ unicorn/elements-1258-migrate-ino-switch-tests/assets/ino-tooltip.stories-ead62e14.js
@@ -0,0 +1,126 @@
+import{x as I}from"./lit-html-f27292d4.js";import{S as e}from"./StoryWrapper-9a52780a.js";import"./elements-stencil-docs-a1cadc86.js";const M={title:"Notification/ino-tooltip",component:"ino-tooltip",argTypes:{for:{type:"string"},placement:{control:{type:"select"},options:["top","right","bottom","left"]},trigger:{control:{type:"select"},options:["mouseenter","focus","click","mouseenter focus","mouseenter click","focus click","mouseenter focus click"]},colorScheme:{control:{type:"select"},options:["light","dark","primary"]},delay:{control:{type:"object"}},message:{control:"string"}},render:r=>I`
+    <ino-button id="${r.for}">Trigger Element</ino-button>
+    <ino-tooltip
+      header-text="${r.headerText}"
+      for="${r.for}"
+      placement="${r.placement}"
+      trigger="${r.trigger}"
+      color-scheme="${r.colorScheme}"
+      delay=${typeof r.delay=="number"?r.delay:[r.delay]}
+      arrow="${r.arrow}"
+      >This is a tooltip</ino-tooltip
+    >
+  `,args:{headerText:"",for:"tooltip-target",placement:"top",trigger:"mouseenter focus",colorScheme:"primary",delay:0,message:"",arrow:!1}},o=e({...M}),t=e({...o,docsFromProperty:"headerText",args:{headerText:"HeaderText",for:"tooltip-target-header"}}),a=e({...o,docsFromProperty:"placement",args:{placement:"right",for:"tooltip-target-placement"}}),n=e({...o,docsFromProperty:"arrow",args:{arrow:!0,for:"tooltip-target-arrow"}}),i=e({...o,docsFromProperty:"trigger",args:{trigger:"click",for:"tooltip-target-trigger"}}),l=e({...o,render:r=>{const s="tooltip-light",p="tooltip-dark",d="tooltip-primary";return I`
+      <ino-button id="${s}">Light</ino-button>
+      <ino-tooltip
+        color-scheme="light"
+        for="${s}"
+        placement="${r.placement}"
+        trigger="${r.trigger}"
+        delay=${typeof r.delay=="number"?r.delay:[r.delay]}
+        arrow="${r.arrow}"
+      >
+        This is a tooltip
+      </ino-tooltip>
+      <ino-button id="${p}">Dark</ino-button>
+      <ino-tooltip
+        color-scheme="dark"
+        for="${p}"
+        placement="${r.placement}"
+        trigger="${r.trigger}"
+        delay=${typeof r.delay=="number"?r.delay:[r.delay]}
+        arrow="${r.arrow}"
+      >
+        This is a tooltip
+      </ino-tooltip>
+      <ino-button id="${d}">Primary</ino-button>
+      <ino-tooltip
+        color-scheme="primary"
+        for="${d}"
+        placement="${r.placement}"
+        trigger="${r.trigger}"
+        delay=${typeof r.delay=="number"?r.delay:[r.delay]}
+        arrow="${r.arrow}"
+      >
+        This is a tooltip
+      </ino-tooltip>
+    `}}),c=e({...o,docsFromProperty:"delay",args:{delay:[500,200],for:"tooltip-target-delay"}});var m,g,y;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`Story({
+  ...InoTooltipMeta
+})`,...(y=(g=o.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var u,f,$;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'headerText',
+  args: {
+    headerText: 'HeaderText',
+    for: 'tooltip-target-header'
+  }
+})`,...($=(f=t.parameters)==null?void 0:f.docs)==null?void 0:$.source}}};var h,T,b;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'placement',
+  args: {
+    placement: 'right',
+    for: 'tooltip-target-placement'
+  }
+})`,...(b=(T=a.parameters)==null?void 0:T.docs)==null?void 0:b.source}}};var w,S,P;n.parameters={...n.parameters,docs:{...(w=n.parameters)==null?void 0:w.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'arrow',
+  args: {
+    arrow: true,
+    for: 'tooltip-target-arrow'
+  }
+})`,...(P=(S=n.parameters)==null?void 0:S.docs)==null?void 0:P.source}}};var k,D,x;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'trigger',
+  args: {
+    trigger: 'click',
+    for: 'tooltip-target-trigger'
+  }
+})`,...(x=(D=i.parameters)==null?void 0:D.docs)==null?void 0:x.source}}};var F,L,H;l.parameters={...l.parameters,docs:{...(F=l.parameters)==null?void 0:F.docs,source:{originalSource:`Story({
+  ...Default,
+  render: args => {
+    const idLight = 'tooltip-light';
+    const idDark = 'tooltip-dark';
+    const idPrimary = 'tooltip-primary';
+    return html\`
+      <ino-button id="\${idLight}">Light</ino-button>
+      <ino-tooltip
+        color-scheme="light"
+        for="\${idLight}"
+        placement="\${args.placement}"
+        trigger="\${args.trigger}"
+        delay=\${typeof args.delay === 'number' ? args.delay : [args.delay]}
+        arrow="\${args.arrow}"
+      >
+        This is a tooltip
+      </ino-tooltip>
+      <ino-button id="\${idDark}">Dark</ino-button>
+      <ino-tooltip
+        color-scheme="dark"
+        for="\${idDark}"
+        placement="\${args.placement}"
+        trigger="\${args.trigger}"
+        delay=\${typeof args.delay === 'number' ? args.delay : [args.delay]}
+        arrow="\${args.arrow}"
+      >
+        This is a tooltip
+      </ino-tooltip>
+      <ino-button id="\${idPrimary}">Primary</ino-button>
+      <ino-tooltip
+        color-scheme="primary"
+        for="\${idPrimary}"
+        placement="\${args.placement}"
+        trigger="\${args.trigger}"
+        delay=\${typeof args.delay === 'number' ? args.delay : [args.delay]}
+        arrow="\${args.arrow}"
+      >
+        This is a tooltip
+      </ino-tooltip>
+    \`;
+  }
+})`,...(H=(L=l.parameters)==null?void 0:L.docs)==null?void 0:H.source}}};var A,C,E;c.parameters={...c.parameters,docs:{...(A=c.parameters)==null?void 0:A.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'delay',
+  args: {
+    delay: [500, 200],
+    for: 'tooltip-target-delay'
+  }
+})`,...(E=(C=c.parameters)==null?void 0:C.docs)==null?void 0:E.source}}};const O=["Default","HeaderText","Placement","Arrow","Trigger","ColorScheme","Delay"];export{n as Arrow,l as ColorScheme,o as Default,c as Delay,t as HeaderText,a as Placement,i as Trigger,O as __namedExportsOrder,M as default};
