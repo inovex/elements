470e2374502e0e6839c6ca88778975245635e6f5
Tobias Heim Galindo <tobisellfy@gmail.com>
2024-04-04 14:30:33 +0200

    test: migrate ino-tab-bar tests to playwright spec.ts

---
+++ packages/storybook/src/stories/ino-tab-bar/ino-tab-bar.spec.ts
@@ -0,0 +1,44 @@
+import { expect, Locator, test } from '@playwright/test';
+import { goToStory } from '../test-utils';
+
+test.describe('ino-tab-bar', () => {
+  let inoTab: Locator;
+  let inoTabBar: Locator;
+
+  test.beforeEach(async ({ page }) => {
+    inoTabBar = page.locator('ino-tab-bar');
+    inoTab = inoTabBar.locator('ino-tab');
+  });
+
+  test('should render with the inoActiveTab property set correctly', async ({
+    page,
+  }) => {
+    await goToStory(page, ['Structure', 'ino-tab-bar', 'active-tab']);
+    await expect(inoTab.nth(1)).toHaveAttribute('a-1-1y-selected');
+  });
+
+  test('should be focused automatically with the autoFocus property', async ({
+    page,
+  }) => {
+    await goToStory(page, ['Structure', 'ino-tab-bar', 'auto-focus']);
+
+    const firstTabHTML = await page.evaluate(
+      () => document.querySelector('ino-tab').outerHTML,
+    );
+
+    const focusedElementHTML = await page.evaluate(
+      () => document.activeElement.closest('ino-tab').outerHTML,
+    );
+
+    // Check if the first ino-tab is the currently focused element
+    expect(focusedElementHTML).toBe(firstTabHTML);
+  });
+
+  test('should set the clicked ino-tab as the active tab in the ino-tab-bar', async ({
+    page,
+  }) => {
+    await goToStory(page, ['Structure', 'ino-tab-bar', 'default']);
+    await inoTab.nth(1).click();
+    await expect(inoTabBar).toHaveAttribute('active-tab', '1');
+  });
+});
