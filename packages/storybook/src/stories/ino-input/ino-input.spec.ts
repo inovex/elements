edecbac6cf2dd33f847affbbdff488447a9b4fd9
Benjamin Pagelsdorf <bpagelsdorf@inovex.de>
2024-04-04 15:07:06 +0200

    migrate ino-input tests to playwright

--- packages/storybook/src/stories/ino-input/ino-input.spec.ts
+++ packages/storybook/src/stories/ino-input/ino-input.spec.ts
@@ -21,6 +21,83 @@ test.describe('ino-input', () => {
     await expect(disabledInput).toBeDisabled();
   });
 
+  test('should display helper text helper-persistent', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const input = inoInput.getByRole('textbox');
+    const helper = inoInput.getByText('Helper message');
+
+    await expect(helper).toBeHidden();
+    await input.click();
+    await expect(helper).toBeVisible();
+    await input.blur();
+    await expect(helper).toBeHidden();
+  });
+
+  test('should display persistent helper text', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const helper = inoInput.getByText('Helper message');
+
+    await expect(helper).toBeHidden();
+    await setAttribute(inoInput, 'helper-persistent', 'true');
+    await expect(helper).toBeVisible();
+  });
+
+  test('should display optional label hint', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const label = inoInput.getByText('Input label');
+    const getHintContent = () =>
+      label.evaluate((el) => window.getComputedStyle(el, ':after').content);
+
+    expect(await getHintContent()).toEqual('none');
+    await setAttribute(inoInput, 'show-label-hint', 'true');
+    expect(await getHintContent()).toContain('- Optional');
+  });
+
+  test('should display required label hint', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const label = inoInput.getByText('Input label');
+    const getHintContent = () =>
+      label.evaluate((el) => window.getComputedStyle(el, ':after').content);
+
+    await setAttribute(inoInput, 'required', 'true');
+    expect(await getHintContent()).toEqual('none');
+    await setAttribute(inoInput, 'show-label-hint', 'true');
+    expect(await getHintContent()).toContain('*');
+  });
+
+  test('should display unit', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const nativeInput = page.locator('input');
+    const unitText = inoInput.getByText('€');
+    const getUnitOpacity = () =>
+      unitText.evaluate((el) => Number(window.getComputedStyle(el).opacity));
+
+    await setAttribute(inoInput, 'unit', '€');
+    expect(await getUnitOpacity()).toBe(0);
+    await nativeInput.click();
+    expect(await getUnitOpacity()).toBeGreaterThan(0.5);
+    await nativeInput.fill('100');
+    await nativeInput.blur();
+    expect(await getUnitOpacity()).toBe(1);
+  });
+
+  test('should have correct input type', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const nativeInput = inoInput.locator('input');
+
+    await expect(nativeInput).toHaveAttribute('type', 'text');
+    await setAttribute(inoInput, 'type', 'email');
+    await expect(nativeInput).toHaveAttribute('type', 'email');
+    await setAttribute(inoInput, 'type', 'number');
+    await expect(nativeInput).toHaveAttribute('type', 'number');
+  });
+
   test('should not be able to input anything if disabled', async ({ page }) => {
     await goToStory(page, ['Input', 'ino-input', 'states']);
 
@@ -91,5 +168,38 @@ test.describe('ino-input', () => {
     await arrowDown.click();
     await arrowDown.click();
     await expect(input).toHaveValue('0');
+  });
+});
+
+test.describe('ino-input - Error', () => {
+  test('should display error when input is required', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const nativeInput = page.locator('input');
+    const invalidInput = page.locator('input:invalid');
+    await nativeInput.fill('ABC');
+
+    await setAttribute(inoInput, 'required', 'true');
+    await expect(invalidInput).toBeHidden();
+
+    await nativeInput.fill('');
+    await nativeInput.blur();
+    await expect(invalidInput).toBeVisible();
+  });
+
+  test('should display validation helper on error', async ({ page }) => {
+    await goToStory(page, ['Input', 'ino-input', 'default']);
+    const inoInput = page.locator('ino-input');
+    const nativeInput = page.locator('input');
+    const helper = inoInput.getByText('Helper message');
+
+    await setAttribute(inoInput, 'required', 'true');
+    await setAttribute(inoInput, 'helper-validation', 'true');
+    await expect(helper).toBeHidden();
+
+    await nativeInput.fill('');
+    await expect(helper).toBeHidden();
+    await nativeInput.blur();
+    await expect(helper).toBeVisible();
   });
 });
