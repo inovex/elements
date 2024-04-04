b38fd7b8b0f1a48788a2bccba80cb45d75c12a22
janivo <janivo@users.noreply.github.com>
2024-04-04 15:37:16 +0000

    Deploying example projects

---
+++ example-projects/vue/assets/index-38c18b20-01ad82eb.js
@@ -0,0 +1,7 @@
+/*!
+ * Crafted with ❤ by inovex GmbH
+ */var i={exports:{}};/*!
+	Copyright (c) 2018 Jed Watson.
+	Licensed under the MIT License (MIT), see
+	http://jedwatson.github.io/classnames
+*/(function(s){(function(){var f={}.hasOwnProperty;function e(){for(var t="",n=0;n<arguments.length;n++){var r=arguments[n];r&&(t=o(t,c(r)))}return t}function c(t){if(typeof t=="string"||typeof t=="number")return t;if(typeof t!="object")return"";if(Array.isArray(t))return e.apply(null,t);if(t.toString!==Object.prototype.toString&&!t.toString.toString().includes("[native code]"))return t.toString();var n="";for(var r in t)f.call(t,r)&&t[r]&&(n=o(n,r));return n}function o(t,n){return n?t?t+" "+n:t+n:t}s.exports?(e.default=e,s.exports=e):window.classNames=e})()})(i);const u=i.exports;export{u as c};
