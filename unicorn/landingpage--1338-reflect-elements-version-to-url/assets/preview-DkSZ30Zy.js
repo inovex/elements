d4e4f3b88ba76b8ec969624da2c9d1b5c64e1c39
TobiasHeimGalindo <TobiasHeimGalindo@users.noreply.github.com>
2024-04-11 13:58:52 +0000

    Deploying unicorn landingpage--1338-reflect-elements-version-to-url 🦄

---
+++ unicorn/landingpage--1338-reflect-elements-version-to-url/assets/preview-DkSZ30Zy.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-C9ju3nPv.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CRNpvopb.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CRNpvopb.js","./chunk-HLWAVYOI-75JPl46b.js","./iframe-C9ju3nPv.js","./index-Ddb8kuSy.js","./_commonjsHelpers-BosuxZz1.js","./index-BHQg7lv3.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
