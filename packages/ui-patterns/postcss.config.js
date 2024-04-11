143308feefdeaedc19186caddc1d0b00b79de012
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2024-04-11 11:50:49 +0200

    fix(ui-patterns): tailwind styles not being integrated

--- packages/ui-patterns/postcss.config.js
+++ packages/ui-patterns/postcss.config.js
@@ -1,6 +1,14 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.js'),
+    },
     autoprefixer: {},
   },
 };
