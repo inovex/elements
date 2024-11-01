0cdf273ccdaa20f16670bedec9aa5464b246b645
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2024-11-01 16:51:32 +0100

    feat: add example in storybook for complex content

--- packages/storybook/src/stories/ino-meetup/ino-meetup.stories.ts
+++ packages/storybook/src/stories/ino-meetup/ino-meetup.stories.ts
@@ -4,23 +4,19 @@ import { Components } from '@inovex.de/elements';
 import './ino-meetup.scss';
 
 const eventHandler = (e: CustomEvent<boolean>) => {
-  
+  console.log(e);
 };
 
 const inoAccordionMeta = {
   title: 'Structure/ino-meetup-accordion',
   component: 'ino-meetup-accordion',
   render: args => html`
-    <ino-meetup-accordion
-      accordion-title="${args.accordionTitle}"
-      @expandedChange="${eventHandler}"
-    >
+    <ino-meetup-accordion accordion-title="${args.accordionTitle}" @expandedChange="${eventHandler}">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mi enim, venenatis non facilisis sed, finibus in
       enim. Sed auctor enim nisl, sit amet feugiat risus blandit vitae.
     </ino-meetup-accordion>
   `,
   args: {
-    expanded: false,
     accordionTitle: 'Accordion Title',
   },
 } as Meta<Components.InoMeetupAccordion>;
@@ -31,16 +27,25 @@ export const Default = {
   ...inoAccordionMeta,
 };
 
-export const Expanded = {
-  ...Default,
-  args: {
-    expanded: true,
-  },
-};
-
 export const CustomTitle = {
   ...Default,
   args: {
     accordionTitle: 'Custom Title',
   },
+};
+
+export const ComplexContent = {
+  ...Default,
+  render: args => html`
+    <ino-meetup-accordion accordion-title="Notification Settings" @expandedChange="${eventHandler}">
+      <div style="display: flex; flex-direction: column; gap: 1.5rem;">
+        <ino-radio-group alignment="vertical">
+          <ino-radio>I want to receive all notification</ino-radio>
+          <ino-radio>Only those where I'm mentioned</ino-radio>
+          <ino-radio>None</ino-radio>
+        </ino-radio-group>
+        <ino-button>Save</ino-button>
+      </div>
+    </ino-meetup-accordion>
+  `,
 };
