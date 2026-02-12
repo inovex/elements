acfa777127fd347ed0f55aafee96207e1108f6ac
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2026-02-12 17:51:24 +0100

    chore: remove ui-patterns references from project files

--- packages/landingpage/translations/locales/en/index.ts
+++ packages/landingpage/translations/locales/en/index.ts
@@ -2,12 +2,10 @@ import about from './about.json';
 import common from './common.json';
 import explore from './explore.json';
 import home from './home.json';
-import library from './library.json';
 
 export default {
   home,
   about,
   common,
-  library,
   explore,
 };
--- packages/ui-patterns/src/patterns/my-pattern-a/MyPatternA.tsx
+++
@@ -1,8 +0,0 @@
-import htmlContent from './my-pattern-a.html?raw';
-import PatternWrapper from '../PatternWrapper';
-
-function MyPatternA() {
-  return <PatternWrapper htmlContent={htmlContent} />;
-}
-
-export default MyPatternA;
