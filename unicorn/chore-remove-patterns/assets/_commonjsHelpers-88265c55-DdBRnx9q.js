18336a3d62bb6c4efb8b19a13ad53e8cf732e380
janivo <janivo@users.noreply.github.com>
2026-03-11 14:21:26 +0000

    Deploying unicorn chore-remove-patterns 🦄

---
+++ unicorn/chore-remove-patterns/assets/_commonjsHelpers-88265c55-DdBRnx9q.js
@@ -0,0 +1,3 @@
+/*!
+ * Crafted with ❤ by inovex GmbH
+ */var n=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{};function f(e){if(e.__esModule)return e;var o=Object.defineProperty({},"__esModule",{value:!0});return Object.keys(e).forEach(function(t){var r=Object.getOwnPropertyDescriptor(e,t);Object.defineProperty(o,t,r.get?r:{enumerable:!0,get:function(){return e[t]}})}),o}export{n as c,f as g};
---
+++ unicorn/chore-remove-patterns/assets/ino-img.stories-CDGSiqod.js
@@ -0,0 +1,32 @@
+import{b as g}from"./lit-html-odaN0r1L.js";import{S as a}from"./StoryWrapper-CB660Xf5.js";import{g as h}from"./utils-ebStcoTl.js";import"./elements-stencil-docs-CE9fhhj4.js";const f={title:"Graphic/ino-img",component:"ino-img",render:o=>g`
+    <ino-img
+      class="customizable-img"
+      alt="${o.alt}"
+      decoding="${o.decoding}"
+      height="${o.height}"
+      sizes="${o.sizes}"
+      src="${o.src}"
+      srcset="${o.srcset}"
+      width="${o.width}"
+      usemap="${o.usemap}"
+      ratio-width="${o.ratioWidth}"
+      ratio-height="${o.ratioHeight}"
+      rounded="${o.rounded}"
+      fallback-icon="${o.fallbackIcon}"
+    >
+    </ino-img>
+  `,argTypes:{fallbackIcon:{control:{type:"select"},options:h()}},args:{alt:"Alternative Text",decoding:"async",height:100,src:"https://cdn-images-1.medium.com/max/1600/1*HP8l7LMMt7Sh5UoO1T-yLQ.png",width:100,ratioWidth:1,ratioHeight:1,rounded:!1,srcset:"",usemap:"",fallbackIcon:"image_not_available"}},e=a({...f}),r=a({...e,docsFromProperty:"rounded",args:{rounded:!0}}),t=a({...e,docsFromProperty:"fallbackIcon",args:{src:"https://cdn-images-1/max/1600/1*HP8l7LMMt7Sh5UoO1T-yLQ.png"}});var s,c,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`Story({
+  ...InoImgMeta
+})`,...(n=(c=e.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var i,d,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'rounded',
+  args: {
+    rounded: true
+  }
+})`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var l,p,u;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`Story({
+  ...Default,
+  docsFromProperty: 'fallbackIcon',
+  args: {
+    src: 'https://cdn-images-1/max/1600/1*HP8l7LMMt7Sh5UoO1T-yLQ.png'
+  }
+})`,...(u=(p=t.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};const S=["Default","Rounded","FallbackIcon"];export{e as Default,t as FallbackIcon,r as Rounded,S as __namedExportsOrder,f as default};
