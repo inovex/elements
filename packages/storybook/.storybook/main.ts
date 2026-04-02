23efe09b520d9d8e7aea6512b410b2f1942de4d2
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2026-04-02 11:29:44 +0200

    chore: wip

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -16,9 +16,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.ts', '../src/**/*.mdx'],
 
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     {
       name: '@storybook/addon-docs',
       options: {
