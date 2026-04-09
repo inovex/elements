b6694d33cef9ffa50f2cd3d1cca08af6d3976ca8
Jan-Niklas Voß <jan-niklas.voss@inovex.de>
2026-04-09 16:49:43 +0200

    feat(landingpage): improve SEO by adding sitemaps

--- packages/landingpage/astro.config.ts
+++ packages/landingpage/astro.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elements.inovex.de',
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'de',
+        locales: {
+          de: 'de',
+          en: 'en',
+        },
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
